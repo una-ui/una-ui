6bd5d4c734a311da232c409bb8787c6c14b3f53c
phojie rengel <phojrengel@gmail.com>
2023-05-01 20:10:39 +0800

    chore: init

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,4 +1,4 @@
-import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { createResolver, defineNuxtModule } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions { }
@@ -14,8 +14,5 @@ export default defineNuxtModule<ModuleOptions>({
     // options, nuxt
 
     const resolver = createResolver(import.meta.url)
-
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
   },
 })
--- packages/nuxt/src/runtime/plugin.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-
-export default defineNuxtPlugin(() => {
-  // nuxtApp
-  // console.log('Plugin injected by @nexvelt/ui-nuxt!')
-})
