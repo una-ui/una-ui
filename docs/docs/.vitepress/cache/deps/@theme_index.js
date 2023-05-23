c0aec4af6f883e341ad159d392e1b10f63230b06
phojie rengel <phojrengel@gmail.com>
2023-05-23 18:29:12 +0800

    chore(docs): init

---
+++ docs/docs/.vitepress/cache/deps/@theme_index.js
@@ -0,0 +1,41 @@
+// ../node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/index.js
+import "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+
+// ../node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/without-fonts.js
+import "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "/Users/einstein/_Future/Projects/Nexvelt/ui/node_modules/.pnpm/vitepress@1.0.0-beta.1_@algolia+client-search@4.17.0_@types+node@18.16.3_sass@1.62.1/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+var theme = {
+  Layout,
+  enhanceApp: ({ app }) => {
+    app.component("Badge", VPBadge);
+  }
+};
+var without_fonts_default = theme;
+export {
+  default5 as VPDocAsideSponsors,
+  default3 as VPHomeFeatures,
+  default2 as VPHomeHero,
+  default4 as VPHomeSponsors,
+  default9 as VPTeamMembers,
+  default6 as VPTeamPage,
+  default8 as VPTeamPageSection,
+  default7 as VPTeamPageTitle,
+  without_fonts_default as default
+};
+//# sourceMappingURL=@theme_index.js.map
