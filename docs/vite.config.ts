731476fd86580b6c6fcb627963a453efcce88bed
phojie rengel <phojrengel@gmail.com>
2023-05-30 23:36:36 +0800

    chore: init

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
-import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +10,5 @@ export default defineConfig({
   },
   plugins: [
     UnoCSS(),
-    AutoImport({
-      dirs: [
-        './vitepress/theme/components',
-      ],
-    }),
   ],
 })
