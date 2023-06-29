4af03610d83d5e9a7956296ae47c8f29efe178ba
phojie rengel <phojrengel@gmail.com>
2023-06-29 17:51:29 +0800

    chore: lint

--- packages/preset/src/_shortcuts/input.ts
+++ packages/preset/src/_shortcuts/input.ts
@@ -1,5 +1,6 @@
 export const staticInput = {
   /* default */
+  'input-base': 'input-md input-disabled ring-base sm:(text-sm leading-6) placeholder:(font-light text-gray-400) dark:placeholder:text-gray-500 block outline-none rounded-md w-full border-0 shadow-sm bg-transparent',
   'input-disabled': '!disabled:(cursor-not-allowed pointer-events-none bg-muted text-muted opacity-75)',
   // 'input-base-error': 'input-base-error ring-error-500 dark:ring-error-400',
 
@@ -12,7 +13,6 @@ export const staticInput = {
 
 export const dynamicInput = [
   [/^input-focus(-(\S+))?$/, ([, , c = 'primary']) => `focus:ring-2 focus:ring-${c}-500 dark:focus:ring-${c}-400`],
-  [/^input-base(-(\S+))?$/, ([, , c = 'primary']) => 'input-md input-disabled ring-base sm:(text-sm leading-6) placeholder:(font-light text-gray-400) dark:placeholder:text-gray-500 block outline-none rounded-md w-full border-0 shadow-sm bg-transparent'],
-  [/^input-outline(-(\S+))?$/, ([, , c = 'primary']) => `input-base-${c} input-focus-${c} ring-1 ring-inset ring-base`],
-  [/^input-solid(-(\S+))?$/, ([, , c = 'primary']) => `input-base-${c} input-focus-${c} ring-1 ring-${c}-500 dark:ring-${c}-400`],
+  [/^input-outline(-(\S+))?$/, ([, , c = 'primary']) => `input-base input-focus-${c} ring-1 ring-inset ring-base`],
+  [/^input-solid(-(\S+))?$/, ([, , c = 'primary']) => `input-base input-focus-${c}  ring-1 ring-${c}-500 dark:ring-${c}-400`],
 ]
