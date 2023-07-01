06b3685cfe98890faaadd79ddbea8f64ac43871b
phojie rengel <phojrengel@gmail.com>
2023-07-02 05:28:40 +0800

    chore: config file creator

--- docs/scripts/file-creator.ts
+++ docs/scripts/file-creator.ts
@@ -5,13 +5,17 @@ import type { DefaultTheme } from 'vitepress/types'
 function createFile(name: string, content: string) {
   fs.writeFileSync(name, content, { encoding: 'utf-8' })
 }
+// create a directory with the given name
+function createDirectory(name: string) {
+  fs.mkdirSync(name)
+}
 
-// just copy a list sidebar items from the sidebar
+// just copy a list sidebar items from the config file
 const listOfDirectoriesWithFiles: DefaultTheme.SidebarItem[] = [
-  {
-    // text: 'Accordion (WIP)',
-    // link: '/elements/accordion',
-  },
+  // {
+  //   text: 'Checkbox (WIP)',
+  //   link: '/forms/checkbox',
+  // },
 ]
 
 // create a file for each sidebar item base on the link and text from the sidebar
@@ -19,11 +23,16 @@ for (const directory of listOfDirectoriesWithFiles) {
   if (!directory.link || !directory.text)
     continue
 
+  // create a directory for each sidebar item
+  const directoryName = directory.link.split('/')[1]
+  if (!fs.existsSync(directoryName))
+    createDirectory(directoryName)
+
   const fileName = directory.link.split('/')[2]
-  const path = `./forms/${fileName}.md`
+  const path = `./${directoryName}/${fileName}.md`
   const content = `---
 title: ${directory.text}
-description: ${directory.text}
+description: 
 ---
 
 # ${directory.text.charAt(0).toUpperCase() + directory.text.slice(1)}
