9763f23fe69b25541bb2b3d52a21e6a0bdc3488b
phojie rengel <phojrengel@gmail.com>
2023-07-01 22:20:40 +0800

    docs: fix route value

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       { text: 'Home', link: '/' },
       {
         text: 'Guide',
-        link: '/guide',
+        link: '/guide/',
         activeMatch: '^/guide/',
       },
       {
