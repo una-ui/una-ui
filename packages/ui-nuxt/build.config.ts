4e5c888e12b1f7aa62b706415dad1c256841d2c6
phojie rengel <phojrengel@gmail.com>
2023-07-02 23:38:41 +0800

    chore: lint

---
+++ packages/ui-nuxt/build.config.ts
@@ -0,0 +1,19 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  declaration: true,
+  entries: [
+    { input: 'src/module', format: 'esm' },
+    { input: 'src/unocss', format: 'esm' },
+    { input: 'src/components/', outDir: 'dist/components' },
+    { input: 'src/runtime/', outDir: 'dist/runtime' },
+    { input: 'src/assets/', outDir: 'dist/assets' },
+  ],
+  externals: [
+    'nuxt',
+    'nuxt/schema',
+    '@nuxt/schema',
+    '@nuxt/kit',
+    'unocss.mjs',
+  ],
+})
---
+++ packages/ui-nuxt/src/module.ts
@@ -0,0 +1,19 @@
+import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
+
+// Module options TypeScript interface definition
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'my-module',
+    configKey: 'myModule',
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url)
+
+    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    addPlugin(resolver.resolve('./runtime/plugin'))
+  },
+})
