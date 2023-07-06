981abeb6e1dda4b20411582d3fdee9370dea1162
phojie rengel <phojrengel@gmail.com>
2023-07-07 05:35:14 +0800

    perf: remove playground

--- playground/vue/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'virtual:uno.css'
-import '@unocss/reset/tailwind-compat.css'
-import '@nexvelt/ui-preset/style.css'
-
-createApp(App).mount('#app')
