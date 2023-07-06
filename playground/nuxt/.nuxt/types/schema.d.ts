97d588aca6ba2f6bd3c7c965433cfe5164535654
phojie rengel <phojrengel@gmail.com>
2023-07-07 04:33:30 +0800

    perf: remove nuxt playground

---
+++ playground/nuxt/.nuxt/types/schema.d.ts
@@ -0,0 +1,24 @@
+import { NuxtModule, RuntimeConfig } from 'nuxt/schema'
+declare module 'nuxt/schema' {
+  interface NuxtConfig {
+    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    modules?: (undefined | null | false | NuxtModule | string | [NuxtModule | string, Record<string, any>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+  }
+  interface RuntimeConfig {
+   app: {
+      baseURL: string,
+
+      buildAssetsDir: string,
+
+      cdnURL: string,
+   },
+  }
+  interface PublicRuntimeConfig {
+
+  }
+}
+declare module 'vue' {
+        interface ComponentCustomProperties {
+          $config: RuntimeConfig
+        }
+      }
