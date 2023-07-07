4eaf89698946ce85f21d1a97482093dda1bed5a5
phojie rengel <phojrengel@gmail.com>
2023-07-07 10:10:01 +0800

    style: update theme switcher icon

--- docs/.vitepress/theme/components/ThemeSwitcher.vue
+++ docs/.vitepress/theme/components/ThemeSwitcher.vue
@@ -56,7 +56,7 @@ function shuffleTheme() {
   <div class="sm:ml-7">
     <Popover class="relative inline-block">
       <PopoverButton btn="solid" class="rounded-lg !text-white">
-        <span i-carbon-color-palette />
+        <span i-ion-color-palette-sharp />
       </PopoverButton>
 
       <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
