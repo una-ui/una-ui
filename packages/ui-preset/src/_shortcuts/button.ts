1acd72518d597e8ffedf6556b836488a8aea78d8
phojie rengel <phojrengel@gmail.com>
2023-07-07 08:51:33 +0800

    style: update button solid text color default

--- packages/ui-preset/src/_shortcuts/button.ts
+++ packages/ui-preset/src/_shortcuts/button.ts
@@ -32,7 +32,7 @@ export const staticButton = {
 export const dynamicButton = [
   // variants
   [/^btn-focus(-(\S+))?$/, ([, , c = 'primary']) => `focus-visible:outline-${c}-500 dark:focus-visible:outline-${c}-400  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`],
-  [/^btn-solid(-(\S+))?$/, ([, , c = 'primary']) => `btn-base btn-focus-${c} text-white shadow-sm bg-${c}-600 hover:bg-${c}-500 dark:bg-${c}-500 dark:hover:bg-${c}-400`],
+  [/^btn-solid(-(\S+))?$/, ([, , c = 'primary']) => `btn-base btn-focus-${c} text-inverted shadow-sm bg-${c}-600 hover:bg-${c}-500 dark:bg-${c}-500 dark:hover:bg-${c}-400`],
   [/^btn-outline(-(\S+))?$/, ([, , c = 'primary']) => `btn-base bg-transparent btn-focus-${c} text-${c}-500 dark:text-${c}-400 ring-1 ring-current hover:bg-${c}-50 dark:hover:bg-${c}-950`],
   [/^btn-soft(-(\S+))?$/, ([, , c = 'primary']) => `btn-base btn-focus-${c} text-${c}-500 dark:text-${c}-400 bg-${c}-50 dark:bg-${c}-950 hover:bg-${c}-100 dark:hover:bg-${c}-900`],
   [/^btn-ghost(-(\S+))?$/, ([, , c = 'primary']) => `btn-base btn-focus-${c} text-${c}-500 dark:text-${c}-400 hover:bg-${c}-50 dark:hover:bg-${c}-900 bg-transparent`],
