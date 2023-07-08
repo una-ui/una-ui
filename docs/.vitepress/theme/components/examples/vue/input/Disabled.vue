8787f5d1ae27b715c6795d9c1c47f146d417b6eb
phojie rengel <phojrengel@gmail.com>
2023-07-08 10:07:54 +0800

    docs: add loading example to input

---
+++ docs/.vitepress/theme/components/examples/vue/input/Disabled.vue
@@ -0,0 +1,15 @@
+<template>
+  <div flex="~ col" space-y-4>
+    <NVInput
+      input="outline"
+      disabled
+      placeholder="You can't type here (disabled))"
+    />
+
+    <NVInput
+      input="outline"
+      readonly
+      placeholder="You can't type here either (readonly))"
+    />
+  </div>
+</template>
---
+++ docs/.vitepress/theme/components/examples/vue/input/Loading.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const value = ref('')
+const loading = ref(false)
+let typingTimer = null
+const typingDelay = 1000
+
+// Function to stop loading after typing is done
+function stopLoading() {
+  loading.value = false
+}
+
+watch(value, () => {
+  loading.value = true
+  clearTimeout(typingTimer)
+  typingTimer = setTimeout(stopLoading, typingDelay)
+})
+</script>
+
+<template>
+  <NVInput
+    v-model="value"
+    input="outline"
+    leading="i-carbon-search"
+    :loading="loading"
+    placeholder="Start typing."
+  />
+</template>
