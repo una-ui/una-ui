fd47408e15b6da2c11331cbc026c8f39fbd63d62
phojie rengel <phojrengel@gmail.com>
2023-07-09 07:58:40 +0800

    docs: remove email attrs to input color example

--- docs/.vitepress/theme/components/examples/vue/input/Color.vue
+++ docs/.vitepress/theme/components/examples/vue/input/Color.vue
@@ -4,17 +4,14 @@
       placeholder="This is the default color (primary)"
     />
     <NVInput
-      type="email"
       input="outline-indigo"
       placeholder="This is the indigo color"
     />
     <NVInput
-      type="email"
       input="outline-rose"
       placeholder="This is the rose color"
     />
     <NVInput
-      type="email"
       input="outline-lime"
       placeholder="This is the lime color"
     />
