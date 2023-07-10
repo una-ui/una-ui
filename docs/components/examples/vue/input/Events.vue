425afeb4de39d560953b72656a16cf24d1c7b73e
phojie rengel <phojrengel@gmail.com>
2023-07-10 12:26:37 +0800

    docs: update input events example component

--- docs/components/examples/vue/input/Events.vue
+++ docs/components/examples/vue/input/Events.vue
@@ -5,17 +5,12 @@ function click(description: string) {
 </script>
 
 <template>
-  <div grid="~ cols-2" gap-4>
-    <NVInput
-      input="outline-purple"
-      leading="i-heroicons-hand-thumb-up-20-solid text-blue pointer-events-auto cursor-pointer active:text-green"
-      trailing="i-heroicons-arrow-down-tray-20-solid text-yellow pointer-events-auto cursor-pointer active:text-lime"
-      :nv="{
-        inputWrapper: 'col-span-2',
-      }"
-      placeholder="Leading and trailing icons are clickable"
-      @leading="click('leading icon is clicked')"
-      @trailing="click('trailing icon is clicked')"
-    />
-  </div>
+  <NVInput
+    input="outline-purple"
+    leading="i-heroicons-hand-thumb-up-20-solid text-blue pointer-events-auto cursor-pointer active:text-green"
+    trailing="i-heroicons-arrow-down-tray-20-solid text-yellow pointer-events-auto cursor-pointer active:text-lime"
+    placeholder="Leading and trailing icons are clickable"
+    @leading="click('leading icon is clicked')"
+    @trailing="click('trailing icon is clicked')"
+  />
 </template>
