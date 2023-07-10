96e25e764595aa45bc411db7d7eb7dbadcd0134c
phojie rengel <phojrengel@gmail.com>
2023-07-10 12:31:10 +0800

    docs: update input slots example

--- docs/components/examples/vue/input/TrailingSlot.vue
+++ docs/components/examples/vue/input/TrailingSlot.vue
@@ -1,9 +1,13 @@
 <template>
   <NVInput
+    leading="i-heroicons-currency-dollar-20-solid"
     placeholder="Search"
+    :nv="{
+      inputBase: 'pe-12',
+    }"
   >
     <template #trailing>
-      <span class="text-xs">
+      <span class="text-xs font-bold">
         USD
       </span>
     </template>
