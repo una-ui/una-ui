2df96f0e11e950893247951292d95845f13a04d4
phojie rengel <phojrengel@gmail.com>
2023-07-14 20:20:34 +0800

    chore: config app logo

---
+++ docs/components/Logo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+const isDark = computed(() => colorMode.value === 'dark')
+</script>
+
+<template>
+  <span flex items-center space-x-1 class="drop-shadow-[0_0_0.5mm_rgb(var(--c-primary))]">
+    <span class="h-13 w-13 -rotate-1">
+      <ClientOnly>
+        <img class="-mt-0.4" :src="isDark ? '/logo-solid-dark.svg' : '/logo-solid.svg'" alt="NexveltUI Logo">
+      </ClientOnly>
+    </span>
+
+    <span class="hidden font-bold text-base drop-shadow-lg md:block">
+      NexveltUI
+    </span>
+  </span>
+</template>
