4a83fec161e82d26f961ad0ef0006e834d570c1c
phojie rengel <phojrengel@gmail.com>
2023-07-15 20:07:12 +0800

    chore: config

---
+++ docs/components/content/Exemplar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div border="~ base" flex="~ col" rounded-lg bg-muted p-4 class="font-sans">
+    <!-- TODO: Add header soon for interactive component -->
+    <slot />
+  </div>
+</template>
