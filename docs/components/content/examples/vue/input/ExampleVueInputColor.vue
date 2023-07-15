d05cfc295050cb30905d523bd7cc514bed6b03c7
phojie rengel <phojrengel@gmail.com>
2023-07-15 18:19:38 +0800

    docs: add initial input examples

---
+++ docs/components/content/examples/vue/input/ExampleVueInputColor.vue
@@ -0,0 +1,19 @@
+<template>
+  <div grid="~ sm:cols-2" gap-4>
+    <NInput
+      placeholder="This is the default color (primary)"
+    />
+    <NInput
+      input="outline-indigo"
+      placeholder="This is the indigo color"
+    />
+    <NInput
+      input="outline-rose"
+      placeholder="This is the rose color"
+    />
+    <NInput
+      input="outline-lime"
+      placeholder="This is the lime color"
+    />
+  </div>
+</template>
---
+++ docs/components/content/examples/vue/input/ExampleVueInputStatus.vue
@@ -0,0 +1,23 @@
+<template>
+  <div grid="~ sm:cols-2" gap-4>
+    <NInput
+      status="error"
+      placeholder="This is the outline variant with error status"
+    />
+    <NInput
+      type="email"
+      status="success"
+      placeholder="This is the outline variant with success status"
+    />
+    <NInput
+      type="email"
+      status="warning"
+      placeholder="This is the outline variant with warning status"
+    />
+    <NInput
+      type="email"
+      status="info"
+      placeholder="This is the outline variant with info status"
+    />
+  </div>
+</template>
