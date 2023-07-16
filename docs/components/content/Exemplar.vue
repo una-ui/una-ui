b63b20eeb6921a878cc35d55b0639ae64855d902
phojie rengel <phojrengel@gmail.com>
2023-07-16 12:03:28 +0800

    chore: init

--- docs/components/content/Exemplar.vue
+++ docs/components/content/Exemplar.vue
@@ -3,7 +3,6 @@
 
 <template>
   <div border="~ base" flex="~ col" rounded-lg bg-muted p-4 class="font-sans">
-    <!-- TODO: Add header soon for interactive component -->
     <slot />
   </div>
 </template>
