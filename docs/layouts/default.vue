fac3b5db8e7f715f73dd74e040d64631b4929d51
phojie rengel <phojrengel@gmail.com>
2023-07-17 23:19:12 +0800

    style(docs): update ellipsis configuration

--- docs/layouts/default.vue
+++ docs/layouts/default.vue
@@ -1,20 +1,16 @@
 <script setup lang="ts">
 // generate top from 10 to 90
-const top = Math.floor(Math.random() * 80) + 10
-// generate left from 0 to 15 or 85 to 100
-const left = Math.floor(Math.random() * 2) * 85 + Math.floor(Math.random() * 15)
+const location = Math.floor(Math.random() * 80) + 10
 </script>
 
 <template>
-  <div>
-    <DocsPageLayout>
-      <slot />
-    </DocsPageLayout>
+  <DocsPageLayout>
+    <slot />
 
     <Ellipsis
       class="absolute opacity-50"
-      :colors="['rgba(var(--c-primary), 1)', 'rgba(var(--c-primary), 1)', 'var(--nv-c-brand-next)']"
-      height="15rem" width="15rem" :top="`${top}%`" :left="`${left}%`" blur="15rem"
+      :colors="['rgba(var(--c-primary), 1)', 'rgba(var(--c-primary), 1)', 'rgba(var(--c-primary), 1)']"
+      height="15rem" width="15rem" :top="`${location}%`" :left="`${location}%`" blur="15rem"
     />
-  </div>
+  </DocsPageLayout>
 </template>
