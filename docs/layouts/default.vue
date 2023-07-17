839470308529b1eb33beff218ba50e845815472e
phojie rengel <phojrengel@gmail.com>
2023-07-18 00:08:15 +0800

    style: update ellipsis config

--- docs/layouts/default.vue
+++ docs/layouts/default.vue
@@ -4,13 +4,15 @@ const location = Math.floor(Math.random() * 80) + 10
 </script>
 
 <template>
-  <DocsPageLayout>
-    <slot />
+  <div>
+    <DocsPageLayout>
+      <slot />
+    </DocsPageLayout>
 
     <Ellipsis
-      class="absolute opacity-30"
-      :colors="['rgba(var(--c-primary), 1)', 'rgba(var(--c-primary), 1)', 'rgba(var(--c-primary), 1)']"
+      class="absolute opacity-50"
+      :colors="['rgba(var(--c-primary), .9)', 'rgba(var(--c-primary), .5)', 'rgba(var(--c-primary), .9)']"
       height="15rem" width="15rem" :top="`${location}%`" :left="`${location}%`" blur="15rem"
     />
-  </DocsPageLayout>
+  </div>
 </template>
