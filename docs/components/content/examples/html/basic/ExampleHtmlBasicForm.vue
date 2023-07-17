0fb547736ad9afc5eb29fead988995705af2c41c
phojie rengel <phojrengel@gmail.com>
2023-07-17 16:34:35 +0800

    docs: update example usage of preset

--- docs/components/content/examples/html/basic/ExampleHtmlBasicForm.vue
+++ docs/components/content/examples/html/basic/ExampleHtmlBasicForm.vue
@@ -2,14 +2,12 @@
   <form class="flex flex-col gap-y-4">
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
       <div form-group="wrapper">
-        <label for="firstname" from-group="label-base">Firstname </label>
-
+        <label for="firstname" form-group="label-base">Firstname </label>
         <input id="firstname" input="outline" placeholder="Firstname">
       </div>
 
       <div form-group="wrapper">
-        <label for="lastname" from-group="label-base">Lastname </label>
-
+        <label for="lastname" form-group="label-base">Lastname </label>
         <input id="lastname" input="outline" placeholder="Lastname">
       </div>
     </div>
@@ -18,7 +16,6 @@
       <button type="button" btn="solid-white">
         Cancel
       </button>
-
       <button type="button" btn="solid">
         Submit
       </button>
