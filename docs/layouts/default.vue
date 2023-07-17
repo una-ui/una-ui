d253363666b2a7dd6a9e27d5c604fada0fe24023
phojie rengel <phojrengel@gmail.com>
2023-07-17 19:20:49 +0800

    style(docs): prevent ellipsis to display in the center of the page

--- docs/layouts/default.vue
+++ docs/layouts/default.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const top = Math.floor(Math.random() * 80) + 10
-const left = Math.floor(Math.random() * 80) + 10
+// return left position between 10-20 or 70-90
+const left = Math.floor(Math.random() * 2) ? Math.floor(Math.random() * 10) + 10 : Math.floor(Math.random() * 20) + 70
 </script>
 
 <template>
@@ -10,7 +11,7 @@ const left = Math.floor(Math.random() * 80) + 10
     </DocsPageLayout>
 
     <Ellipsis
-      class="opacity-70"
+      class="opacity-50"
       :colors="['rgba(var(--c-primary), 1)', 'rgba(var(--c-primary), 1)', 'var(--nv-c-brand-next)']"
       height="15rem" width="15rem" :top="`${top}%`" :left="`${left}%`" blur="15rem"
     />
