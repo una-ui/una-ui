aebdf872cff5deeb7b09c36bbc46c14c551bcf55
phojie rengel <phojrengel@gmail.com>
2023-07-17 16:25:16 +0800

    docs: add example usage of preset mode

--- docs/components/content/examples/html/basic/ExampleHtmlBasicForm.vue
+++ docs/components/content/examples/html/basic/ExampleHtmlBasicForm.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>
-    <button btn="solid">
-      Submit
-    </button>
-    <button btn="outline-gray">
-      Cancel
-    </button>
-  </div>
+  <form class="flex flex-col gap-y-4">
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+      <div form-group="wrapper">
+        <label for="firstname" from-group="label-base">Firstname </label>
+
+        <input id="firstname" input="outline" placeholder="Firstname">
+      </div>
+
+      <div form-group="wrapper">
+        <label for="lastname" from-group="label-base">Lastname </label>
+
+        <input id="lastname" input="outline" placeholder="Lastname">
+      </div>
+    </div>
+
+    <div class="flex flex-col justify-end gap-4 sm:flex-row">
+      <button type="button" btn="solid-white">
+        Cancel
+      </button>
+
+      <button type="button" btn="solid">
+        Submit
+      </button>
+    </div>
+  </form>
 </template>
