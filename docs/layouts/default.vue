848841b59a53eae23f1c6d07b0f3d07f6fa3fecd
phojie rengel <phojrengel@gmail.com>
2023-07-17 18:56:10 +0800

    docs: update and add ellipsis to layouts

---
+++ docs/layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// generate a random position for the ellipsis
+const top = Math.floor(Math.random() * 80)
+const left = Math.floor(Math.random() * 80)
+</script>
+
+<template>
+  <div>
+    <DocsPageLayout>
+      <slot />
+    </DocsPageLayout>
+
+    <Ellipsis
+      :colors="['rgba(var(--c-primary), 1)', 'rgba(var(--c-primary), 1)', 'var(--nv-c-brand-next)']"
+      height="15rem" width="15rem" :top="`${top}%`" :left="`${left}%`" blur="15rem"
+      style="opacity:.4"
+    />
+  </div>
+</template>
--- docs/layouts/page.vue
+++ docs/layouts/page.vue
@@ -3,5 +3,10 @@
     <article>
       <slot />
     </article>
+
+    <Ellipsis
+      :colors="['rgba(var(--c-primary), 1)', 'rgba(var(--c-primary), 1)', 'var(--nv-c-brand-next)']"
+      height="10rem" width="10rem" top="60rem" left="-3rem" blur="10rem"
+    />
   </div>
 </template>
