717086de0a991fdb16424f008ebf694124ecdd38
phojie rengel <phojrengel@gmail.com>
2023-07-17 15:20:17 +0800

    docs: add introduction info

---
+++ docs/components/content/app/AppTitle.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <span class="from-primary to-$nv-c-brand-next bg-gradient-to-r bg-clip-text text-5xl font-bold text-transparent">
+    <ContentSlot :use="$slots.default" unwrap="p" />
+  </span>
+</template>
---
+++ docs/components/content/docs/DocsTitle.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <span class="from-primary to-$nv-c-brand-next bg-gradient-to-l bg-clip-text text-transparent">
+    <ContentSlot :use="$slots.default" unwrap="p" />
+  </span>
+</template>
