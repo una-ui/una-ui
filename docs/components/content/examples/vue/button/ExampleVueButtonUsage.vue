1fdfdb4d6e24b25bcae859a44aafbc4d9a0effa9
phojie rengel <phojrengel@gmail.com>
2023-07-18 18:00:51 +0800

    docs: add example button usage

---
+++ docs/components/content/examples/vue/button/ExampleVueButtonUsage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div flex gap-4>
+    <!-- label syntax -->
+    <NButton label="Label Button " />
+
+    <!-- slot syntax -->
+    <NButton> Slot Button </NButton>
+  </div>
+</template>
