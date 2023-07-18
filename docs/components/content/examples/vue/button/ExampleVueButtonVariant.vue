478620cb32a5a4ef04943bbb9ed43687775118c4
phojie rengel <phojrengel@gmail.com>
2023-07-18 18:01:03 +0800

    docs: add example button variant

---
+++ docs/components/content/examples/vue/button/ExampleVueButtonVariant.vue
@@ -0,0 +1,28 @@
+<template>
+  <div grid="~ cols-2 sm:cols-3" gap-4>
+    <NButton
+      label="Button Solid"
+      btn="solid"
+    />
+    <NButton
+      label="Button Outline"
+      btn="outline"
+    />
+    <NButton
+      label="Button Soft"
+      btn="soft"
+    />
+    <NButton
+      label="Button Ghost"
+      btn="ghost"
+    />
+    <NButton
+      label="Button Link"
+      btn="link"
+    />
+    <NButton
+      label="Button Base"
+      btn="base"
+    />
+  </div>
+</template>
