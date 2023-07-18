03b629c280ce7845106d3968509f3e66848bbde7
phojie rengel <phojrengel@gmail.com>
2023-07-18 20:30:41 +0800

    chore: relocate unocss config file

--- docs/uno.config.ts
+++
@@ -1,9 +0,0 @@
-import { extendUnocssOptions } from '../packages/ui-nuxt/src/unocss'
-
-// for IDE support
-export default {
-  ...extendUnocssOptions(),
-  configDeps: [
-    '../packages/ui-nuxt/src/unocss',
-  ],
-}
