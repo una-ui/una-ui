eea1775d26da3fbfaff6fa4d5e244116fd0e01d1
phojie rengel <phojrengel@gmail.com>
2023-07-20 02:43:01 +0800

    docs: update button icon example

--- docs/components/content/examples/vue/button/ExampleVueButtonIcon.vue
+++ docs/components/content/examples/vue/button/ExampleVueButtonIcon.vue
@@ -1,29 +1,41 @@
 <template>
   <div flex="~ col" gap-4>
     <span class="text-sm font-medium">
-      Icon button
+      Icon buttons with and without square class
     </span>
 
     <div flex="~ row" gap-2>
       <NButton
-        label="i-heroicons-envelope-solid group-focus:i-heroicons-envelope-open-solid"
+        label="i-heroicons-arrow-down-tray-20-solid"
         icon
-        btn="rectangle"
-        class="group"
       />
 
       <NButton
-        label="i-heroicons-envelope-solid group-focus:i-heroicons-envelope-open-solid"
+        label="i-heroicons-arrow-down-tray-20-solid"
         icon
-        btn="square"
-        class="group"
+        btn="soft square"
+      />
+    </div>
+
+    <hr border="base">
+
+    <span class="text-sm font-medium">
+      Icon with states
+    </span>
+
+    <div flex="~ row" gap-2>
+      <NButton
+        label="i-heroicons-bell-20-solid group-hover:i-heroicons-bell-alert-20-solid"
+        icon
+        btn="square solid-black"
+        class="group rounded-none"
       />
 
       <NButton
-        label="i-heroicons-envelope-solid group-focus:i-heroicons-envelope-open-solid"
+        label="i-heroicons-envelope-20-solid group-focus:i-heroicons-envelope-open-20-solid"
         icon
-        btn="circle"
-        class="group"
+        btn="outline-green square focus:outline-yellow"
+        class="group rounded-full"
       />
     </div>
 
@@ -35,14 +47,19 @@
 
     <div flex="~ col sm:row" gap-2>
       <NButton
-        leading="i-logos-google-icon text-lg"
-        btn="solid-white"
+        leading="i-logos-google-icon"
+        btn="solid-gray"
         label="Sign in with Google"
       />
       <NButton
         leading="i-logos-facebook text-lg"
-        btn="solid-black"
+        btn="solid-white"
         label="Sign in with Facebook"
+      />
+      <NButton
+        leading="i-bi-github text-lg"
+        btn="solid-black"
+        label="Sign in with Github"
       />
     </div>
 
@@ -54,13 +71,14 @@
 
     <div flex="~ col sm:row" gap-2>
       <NButton
+        trailing="i-heroicons-at-symbol-20-solid"
+        btn="outline"
+        label="Subscribe now"
+      />
+
+      <NButton
         trailing="i-heroicons-arrow-right-on-rectangle-20-solid"
         label="Login"
-      />
-      <NButton
-        trailing="i-heroicons-envelope-20-solid"
-        btn="solid-lime"
-        label="Subscribe now"
       />
     </div>
   </div>
