acd3237cc4379b0ef174b2b95fad9fe4317b8870
phojie rengel <phojrengel@gmail.com>
2023-07-20 02:42:53 +0800

    docs: add button shape example

---
+++ docs/components/content/examples/vue/button/ExampleVueButtonShape.vue
@@ -0,0 +1,42 @@
+<template>
+  <div flex gap-4>
+    <div>
+      <NButton
+        label="PR"
+        btn="square solid-orange"
+      />
+    </div>
+
+    <div>
+      <NButton
+        label="PR"
+        btn="square solid-lime"
+        class="rounded-full"
+      />
+    </div>
+
+    <div>
+      <NButton
+        label="square + rounded-none"
+        btn="square solid-pink"
+        class="rounded-none"
+      />
+    </div>
+
+    <div>
+      <NButton
+        label="square + rounded-full"
+        btn="square solid-green"
+        class="rounded-full"
+      />
+    </div>
+
+    <div>
+      <NButton
+        label="rectangle + custom-rounded"
+        btn="solid-indigo"
+        class="rounded-b-none rounded-t-2xl text-white"
+      />
+    </div>
+  </div>
+</template>
