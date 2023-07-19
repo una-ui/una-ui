73faaacb20e1cc3fa8e5610730e198a12a493819
phojie rengel <phojrengel@gmail.com>
2023-07-19 21:57:02 +0800

    docs: fix unocss config

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,52 @@
-import { extendUnocssOptions } from './packages/ui-nuxt/src/unocss'
+import {
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
 
-// for IDE support
+import presetNexvelt from './packages/ui-preset/src/index'
+
 export default {
-  ...extendUnocssOptions(),
+  preflight: false,
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
+    presetNexvelt(),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
+  preflights: [
+    {
+      getCSS: () => `
+        *:focus-visible {
+          outline: 2px solid rgb(var(--c-primary-active)); /* 2 */
+          border-radius: 0.25rem; /* 1 */
+          outline-offset: 0.10rem; /* 1 */
+        }
+
+        input:focus, textarea:focus, select:focus{
+          /* outline: none; !* 1 *! */
+          outline: 2px solid transparent; /* 1 */
+        }
+      `,
+    },
+  ],
   configDeps: [
-    './packages/ui-nuxt/src/unocss.ts',
-    './packages/ui-preset/src/index.ts',
     './packages/ui-preset/src/_shortcuts/button.ts',
-    './packages/ui-preset/src/_shortcuts/input.ts',
+    '../packages/ui-preset/src/_shortcuts/button.ts',
+    // './packages/ui-preset/src/_shortcuts/general.ts',
+    // './packages/ui-preset/src/_shortcuts/index.ts',
+    // './packages/ui-preset/src/_shortcuts/input.ts',
   ],
 }
