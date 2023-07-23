496137bd0dd0963739d512020166ec7242d0c0d2
phojie rengel <phojrengel@gmail.com>
2023-07-24 06:07:42 +0800

    docs: add NIcon component

---
+++ docs/components/content/examples/vue/icon/ExampleVueIconUsage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div flex="~ items-center" gap-4>
+    <NIcon name="i-heroicons-bell-20-solid" class="text-primary" />
+
+    <NIcon name="i-logos-adobe-illustrator" class="text-2xl text-primary" />
+
+    <NIcon name="i-logos-adobe-xd" class="text-2xl text-primary" />
+
+    <NIcon name="i-logos-linux-tux" class="text-6xl" />
+
+    <NIcon name="i-logos-airbnb" class="text-6xl" />
+
+    <NIcon name="i-heroicons-sun-20-solid dark:i-heroicons-moon-20-solid" />
+
+    <NIcon name="i-twemoji-black-heart hover:i-twemoji-beating-heart" class="hover:text-xl" />
+
+    <NIcon name="i-twemoji-backhand-index-pointing-left" />
+    <span>
+      Hover
+    </span>
+  </div>
+</template>
