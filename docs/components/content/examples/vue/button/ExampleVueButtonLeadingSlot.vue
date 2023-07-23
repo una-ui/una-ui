689f2142c51a9a064578c56d05098c1db6f37539
phojie rengel <phojrengel@gmail.com>
2023-07-24 05:29:33 +0800

    docs: update button leading slot example

--- docs/components/content/examples/vue/button/ExampleVueButtonLeadingSlot.vue
+++ docs/components/content/examples/vue/button/ExampleVueButtonLeadingSlot.vue
@@ -1,7 +1,13 @@
 <template>
-  <NButton btn="outline" label="Signout">
+  <NButton btn="outline" class="rounded-full" label="View">
     <template #leading>
-      <NIcon name="i-heroicons-arrow-left-on-rectangle-20-solid" />
+      <!-- TODO convert to NAvatar soon -->
+      <div class="rounded-full bg-base">
+        <img
+          class="h-5"
+          src="https://avatars.githubusercontent.com/u/33350692?s=400&u=49395c835e8197ae2ee42ca02c95e828d8f64239&v=4"
+        >
+      </div>
     </template>
   </NButton>
 </template>
