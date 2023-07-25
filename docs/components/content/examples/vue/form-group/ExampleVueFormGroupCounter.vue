aee41f7eb5aaf31c21e8f2f2745626d64259037d
phojie rengel <phojrengel@gmail.com>
2023-07-26 01:33:31 +0800

    docs: add form-group counter example

---
+++ docs/components/content/examples/vue/form-group/ExampleVueFormGroupCounter.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const username = ref('')
+</script>
+
+<template>
+  <NFormGroup
+    label="Username"
+    :counter="{
+      value: username.length,
+      max: 10,
+    }"
+    :status="username.length > 10 ? 'error' : undefined"
+    :message="username.length > 10 ? 'Username must be less than 10 characters' : undefined"
+  >
+    <!-- TODO: update to NTextArea later -->
+    <NInput
+      v-model="username"
+      leading="i-heroicons-user-20-solid"
+    />
+  </NFormGroup>
+</template>
