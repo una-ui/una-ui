66d952cb184614b045b13b966f6b2cb9be7ed776
phojie rengel <phojrengel@gmail.com>
2023-07-25 23:52:12 +0800

    docs: update input events example

--- docs/components/content/examples/vue/input/ExampleVueInputEvents.vue
+++ docs/components/content/examples/vue/input/ExampleVueInputEvents.vue
@@ -21,8 +21,12 @@ const isPasswordVisible = ref(false)
 
     <NInput
       input="outline-purple"
-      leading="i-heroicons-hand-thumb-up-20-solid active:scale-120 text-blue pointer-events-auto cursor-pointer active:text-green"
-      trailing="i-heroicons-arrow-down-tray-20-solid active:scale-90 text-yellow pointer-events-auto cursor-pointer active:text-lime"
+      leading="i-heroicons-hand-thumb-up-20-solid"
+      trailing="i-heroicons-arrow-down-tray-20-solid "
+      :nv="{
+        inputLeadingBase: 'active:scale-120 text-blue pointer-events-auto cursor-pointer active:text-green',
+        inputTrailingBase: 'active:scale-90 text-yellow pointer-events-auto cursor-pointer active:text-lime',
+      }"
       placeholder="Leading and trailing icons are clickable"
       @leading="click('leading icon is clicked')"
       @trailing="click('trailing icon is clicked')"
