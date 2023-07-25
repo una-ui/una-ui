dbcb01282664aff133744be8ec86ba6480a9ea73
phojie rengel <phojrengel@gmail.com>
2023-07-26 00:12:42 +0800

    docs: add form-group required example

---
+++ docs/components/content/examples/vue/form-group/ExampleVueFormGroupRequired.vue
@@ -0,0 +1,13 @@
+<template>
+  <div flex>
+    <NFormGroup
+      label="Email"
+      required
+    >
+      <NInput
+        placeholder="phojrengel@gmail.com"
+        leading="i-heroicons-envelope"
+      />
+    </NFormGroup>
+  </div>
+</template>
