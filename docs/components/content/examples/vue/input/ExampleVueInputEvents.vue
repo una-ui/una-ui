c625890262091470a5829c147d3512940329696d
phojie rengel <phojrengel@gmail.com>
2023-07-25 23:48:09 +0800

    docs: update input events example

--- docs/components/content/examples/vue/input/ExampleVueInputEvents.vue
+++ docs/components/content/examples/vue/input/ExampleVueInputEvents.vue
@@ -3,15 +3,29 @@ function click(description: string) {
   // eslint-disable-next-line no-alert
   alert(description)
 }
+
+const isPasswordVisible = ref(false)
 </script>
 
 <template>
-  <NInput
-    input="outline-purple"
-    leading="i-heroicons-hand-thumb-up-20-solid active:scale-120 text-blue pointer-events-auto cursor-pointer active:text-green"
-    trailing="i-heroicons-arrow-down-tray-20-solid active:scale-90 text-yellow pointer-events-auto cursor-pointer active:text-lime"
-    placeholder="Leading and trailing icons are clickable"
-    @leading="click('leading icon is clicked')"
-    @trailing="click('trailing icon is clicked')"
-  />
+  <div grid="~ cols-1 sm:cols-2" gap-4>
+    <NInput
+      :type="isPasswordVisible ? 'text' : 'password'"
+      :trailing="isPasswordVisible ? 'i-heroicons-eye-20-solid' : 'i-heroicons-eye-slash-20-solid'"
+      :nv="{
+        inputTrailingBase: 'pointer-events-auto cursor-pointer',
+      }"
+      value="Password"
+      @trailing="isPasswordVisible = !isPasswordVisible"
+    />
+
+    <NInput
+      input="outline-purple"
+      leading="i-heroicons-hand-thumb-up-20-solid active:scale-120 text-blue pointer-events-auto cursor-pointer active:text-green"
+      trailing="i-heroicons-arrow-down-tray-20-solid active:scale-90 text-yellow pointer-events-auto cursor-pointer active:text-lime"
+      placeholder="Leading and trailing icons are clickable"
+      @leading="click('leading icon is clicked')"
+      @trailing="click('trailing icon is clicked')"
+    />
+  </div>
 </template>
