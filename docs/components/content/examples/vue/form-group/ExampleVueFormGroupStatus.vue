ce9713f8a5f987ba0f7130cc1f1dcee342ee5a96
phojie rengel <phojrengel@gmail.com>
2023-07-26 00:12:48 +0800

    docs: add form-group status example

---
+++ docs/components/content/examples/vue/form-group/ExampleVueFormGroupStatus.vue
@@ -0,0 +1,44 @@
+<template>
+  <div flex="~ col" gap-4>
+    <NFormGroup
+      label="Username"
+      message="Your username is available."
+      status="success"
+    >
+      <NInput
+        value="Phojie"
+      />
+    </NFormGroup>
+
+    <NFormGroup
+      label="About Me"
+      status="info"
+      message="This information will be visible to other users."
+    >
+      <NInput />
+    </NFormGroup>
+
+    <NFormGroup
+      label="Email"
+      message="Not a valid email address."
+      value="phojrengel@"
+      status="error"
+    >
+      <NInput
+        type="password"
+        value=""
+      />
+    </NFormGroup>
+
+    <NFormGroup
+      label="Password"
+      message="Your password is weak."
+      status="warning"
+    >
+      <NInput
+        type="password"
+        value="Password"
+      />
+    </NFormGroup>
+  </div>
+</template>
