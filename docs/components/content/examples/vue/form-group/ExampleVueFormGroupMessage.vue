9a919597d5455704c10f57a976c732dbc981d944
phojie rengel <phojrengel@gmail.com>
2023-07-26 00:12:35 +0800

    docs: add form-group message example

---
+++ docs/components/content/examples/vue/form-group/ExampleVueFormGroupMessage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div flex>
+    <NFormGroup
+      label="Email"
+      message="We'll never share your email with anyone else."
+    >
+      <NInput
+        placeholder="phojrengel@gmail.com"
+        leading="i-heroicons-envelope"
+      />
+    </NFormGroup>
+  </div>
+</template>
