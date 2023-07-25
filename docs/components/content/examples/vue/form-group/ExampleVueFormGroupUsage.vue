d92084c7a8211747236b882d5b29d093505a95c5
phojie rengel <phojrengel@gmail.com>
2023-07-25 22:30:16 +0800

    docs: update example

--- docs/components/content/examples/vue/form-group/ExampleVueFormGroupUsage.vue
+++ docs/components/content/examples/vue/form-group/ExampleVueFormGroupUsage.vue
@@ -1,11 +1,12 @@
 <template>
-  <NFormGroup
-    label="Email"
-  >
-    <NInput
-      placeholder="phojrengel@gmail.com"
-      leading="i-heroicons-envelope"
-      w-auto
-    />
-  </NFormGroup>
+  <div flex>
+    <NFormGroup
+      label="Email"
+    >
+      <NInput
+        placeholder="phojrengel@gmail.com"
+        leading="i-heroicons-envelope"
+      />
+    </NFormGroup>
+  </div>
 </template>
