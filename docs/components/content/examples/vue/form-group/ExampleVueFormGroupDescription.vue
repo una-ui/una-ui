239194e9bd3cc9ff9161bcab08d28c93a395a40b
phojie rengel <phojrengel@gmail.com>
2023-07-26 00:12:27 +0800

    docs: add form-group description example

---
+++ docs/components/content/examples/vue/form-group/ExampleVueFormGroupDescription.vue
@@ -0,0 +1,14 @@
+<template>
+  <div flex>
+    <NFormGroup
+      label="Email"
+      required
+      description="We'll never share your email with anyone else."
+    >
+      <NInput
+        placeholder="phojrengel@gmail.com"
+        leading="i-heroicons-envelope"
+      />
+    </NFormGroup>
+  </div>
+</template>
