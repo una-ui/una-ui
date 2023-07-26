4b583c62e2cc7f88eba11f4d3d304fbd94cd8c7b
phojie rengel <phojrengel@gmail.com>
2023-07-27 03:55:22 +0800

    chore(preset): config general shortcuts

--- packages/ui-preset/src/_shortcuts/general.ts
+++ packages/ui-preset/src/_shortcuts/general.ts
@@ -1,35 +1,26 @@
-export const text = {
+export const general = {
+  // text-size
   'text-md': 'text-16px',
 
+  // text color
   'text-base': 'text-$c-foreground',
   'text-inverted': 'text-$c-background',
   'text-muted': 'text-$c-muted-foreground',
-
   'text-info': 'text-info-500 dark:text-info-400',
   'text-error': 'text-error-500 dark:text-error-400',
   'text-success': 'text-success-500 dark:text-success-400',
   'text-warning': 'text-warning-500 dark:text-warning-400',
-}
 
-export const ring = {
+  // ring
   'ring-base': 'ring-$c-ring',
   'ring-inverted': 'ring-$c-ring-foreground',
-}
 
-export const border = {
+  // border
   'border-base': 'border-$c-border',
   'border-inverted': 'border-$c-border-foreground',
-}
 
-export const background = {
+  // bg
   'bg-base': 'bg-$c-background',
   'bg-inverted': 'bg-$c-foreground',
   'bg-muted': 'bg-$c-muted',
 }
-
-export const general = [
-  text,
-  ring,
-  border,
-  background,
-]
--- packages/ui-preset/src/_shortcuts/index.ts
+++ packages/ui-preset/src/_shortcuts/index.ts
@@ -1,12 +1,14 @@
 import type { Preset, StaticShortcutMap } from '@unocss/core'
+import { accordion } from './accordion'
 import { button } from './button'
 import { formGroup } from './form-group'
 import { input } from './input'
 import { general } from './general'
 
 export const shortcuts = [
-  ...general,
+  general,
   ...button,
   ...formGroup,
   ...input,
+  ...accordion,
 ] as Exclude<Preset['shortcuts'], undefined | StaticShortcutMap>
