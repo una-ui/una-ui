c3f28b1d1e03d8210dd717a91b01f26f7ce72e21
phojie rengel <phojrengel@gmail.com>
2023-07-28 20:31:02 +0800

    docs: update button leading slot example

--- docs/components/content/examples/vue/button/ExampleVueButtonLeadingSlot.vue
+++ docs/components/content/examples/vue/button/ExampleVueButtonLeadingSlot.vue
@@ -2,12 +2,12 @@
   <NButton btn="outline" class="rounded-full" label="View">
     <template #leading>
       <!-- TODO convert to NAvatar soon -->
-      <div class="rounded-full bg-base">
+      <span class="rounded-full bg-base">
         <img
           class="h-5"
           src="https://avatars.githubusercontent.com/u/33350692?s=400&u=49395c835e8197ae2ee42ca02c95e828d8f64239&v=4"
         >
-      </div>
+      </span>
     </template>
   </NButton>
 </template>
