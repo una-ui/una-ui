492f4a39159f2130ab667958a5b8cf6aa372bf3c
phojie rengel <phojrengel@gmail.com>
2023-07-29 21:20:14 +0800

    docs: add accordion variant example

---
+++ docs/components/content/examples/vue/accordion/ExampleVueAccordionVariant.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const items = [
+  {
+    label: 'Why NexveltUI',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+    leading: 'i-heroicons-book-open',
+  },
+  {
+    label: 'Getting Started',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+    leading: 'i-heroicons-information-circle',
+  },
+  {
+    label: 'Installation',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+    leading: 'i-heroicons-arrow-down-tray',
+  },
+  {
+    label: 'Themes and Colors',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+    leading: 'i-heroicons-beaker',
+    disabled: true,
+  },
+  {
+    label: 'Components',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+    leading: 'i-heroicons-square-3-stack-3d',
+    loading: true,
+  },
+]
+</script>
+
+<template>
+  <NAccordion
+    variant-mode
+    :items="items"
+    :nv="{
+      accordionButton: 'btn-(outline rectangle) rounded-lg',
+      accordionPanel: 'px-0',
+    }"
+  />
+</template>
