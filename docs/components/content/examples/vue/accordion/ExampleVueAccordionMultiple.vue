6225dd00971cf0f4caaab215750cfba699d935d7
phojie rengel <phojrengel@gmail.com>
2023-07-29 20:13:53 +0800

    docs: add accordion multiple prop example

---
+++ docs/components/content/examples/vue/accordion/ExampleVueAccordionMultiple.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const items = [
+  {
+    label: 'How do I get started?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+  {
+    label: 'What is your return policy?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+  {
+    label: 'Can I exchange an item?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+]
+</script>
+
+<template>
+  <NAccordion
+    :items="items"
+    multiple
+  />
+</template>
