2ec2c838ed024f0879edab26f4e792a3de11c6d2
phojie rengel <phojrengel@gmail.com>
2023-07-31 04:30:20 +0800

    docs: add default-open prop to accordion section

---
+++ docs/components/content/examples/vue/accordion/ExampleVueAccordionDefaultOpen.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const items = [
+  {
+    label: 'This is open by default',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+  {
+    label: 'This is open by default',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+  {
+    label: 'This is close by default',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+    defaultOpen: false, // priority over defaultOpen prop
+  },
+]
+</script>
+
+<template>
+  <NAccordion
+    :items="items"
+    multiple
+    default-open
+  />
+</template>
