0c2af9e8718b709b9e9af6933b1dc9461912cf8a
phojie rengel <phojrengel@gmail.com>
2023-07-31 02:29:15 +0800

    docs: add examples to accordion custom section

---
+++ docs/components/content/examples/vue/accordion/ExampleVueAccordionCustom3.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const items = [
+  {
+    label: '1. How do I get started?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+    defaultOpen: true,
+  },
+  {
+    label: '2. What is your return policy?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+  {
+    label: '3. Can I exchange an item?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+]
+</script>
+
+<template>
+  <NAccordion
+    :items="items"
+    variant-mode
+    class="divide-y divide-base"
+    trailing-close="i-heroicons-plus"
+    trailing-open="i-heroicons-minus"
+    :nv="{
+      accordionButton: 'px-0 text-base',
+      accordionPanel: 'px-0 py-2',
+    }"
+  />
+</template>
---
+++ docs/components/content/examples/vue/accordion/ExampleVueAccordionReverse.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const items = [
+  {
+    label: 'How do I get started?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+    defaultOpen: true,
+  },
+  {
+    label: 'What is your return policy?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+  {
+    label: 'Can I exchange an item?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+]
+</script>
+
+<template>
+  <NAccordion
+    :items="items"
+    leading="i-heroicons-question-mark-circle"
+  />
+</template>
