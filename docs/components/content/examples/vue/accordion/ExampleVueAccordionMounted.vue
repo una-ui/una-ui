61cef7bfa50093405471edc2ccb9552dd2b74ee9
phojie rengel <phojrengel@gmail.com>
2023-08-01 06:19:17 +0800

    docs: add accordion mounted prop example

---
+++ docs/components/content/examples/vue/accordion/ExampleVueAccordionMounted.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const items = [
+  {
+    label: 'How do I get started?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+  {
+    label: 'What is your return policy?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+  {
+    label: 'Can I exchange an item?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+  },
+  {
+    label: 'When will I get my refund?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
+    mounted: false,
+  },
+]
+</script>
+
+<template>
+  <NAccordion
+    :items="items"
+    mounted
+  />
+</template>
