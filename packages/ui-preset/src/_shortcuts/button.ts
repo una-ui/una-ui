2da0190ad4b8491d1edcffb1e078ad92abbb689b
phojie rengel <phojrengel@gmail.com>
2023-08-01 13:27:19 +0800

    chore: optimize

--- packages/ui-preset/src/_shortcuts/button.ts
+++ packages/ui-preset/src/_shortcuts/button.ts
@@ -22,20 +22,20 @@ export const staticButton: Record<`${ButtonPrefix}-${string}` | ButtonPrefix, st
   'btn-square': 'p-0.5em',
 
   // variants
-  'btn-solid-white': 'bg-base text-base ring-1 ring-base shadow-sm btn-focus-primary hover:bg-$c-gray-50',
-  'btn-ghost-white': 'text-base btn-focus-primary hover:bg-$c-gray-50',
+  'btn-solid-white': 'bg-base text-base ring-1 ring-base shadow-sm btn-focus hover:bg-$c-gray-50',
+  'btn-ghost-white': 'text-base btn-focus hover:bg-$c-gray-50',
 
-  'btn-solid-gray': 'bg-$c-gray-50 text-$c-gray-800 ring-1 ring-base shadow-sm btn-focus-primary hover:bg-$c-gray-100',
-  'btn-ghost-gray': 'text-$c-gray-500 btn-focus-primary hover:text-$c-gray-950 hover:bg-$c-gray-50',
-  'btn-link-gray': 'text-$c-gray-500 btn-focus-primary hover:text-$c-gray-950 hover:underline underline-offset-4',
+  'btn-solid-gray': 'bg-$c-gray-50 text-$c-gray-800 ring-1 ring-base shadow-sm btn-focus hover:bg-$c-gray-100',
+  'btn-ghost-gray': 'text-$c-gray-500 btn-focus hover:text-$c-gray-950 hover:bg-$c-gray-50',
+  'btn-link-gray': 'text-$c-gray-500 btn-focus hover:text-$c-gray-950 hover:underline underline-offset-4',
 
-  'btn-solid-black': 'bg-$c-gray-950 text-$c-gray-0 ring-1 ring-$c-gray-950 shadow-sm btn-focus-primary hover:bg-$c-gray-800',
-  'btn-link-black': 'text-$c-gray-950 btn-focus-primary hover:underline underline-offset-4',
+  'btn-solid-black': 'bg-$c-gray-950 text-$c-gray-0 ring-1 ring-$c-gray-950 shadow-sm btn-focus hover:bg-$c-gray-800',
+  'btn-link-black': 'text-$c-gray-950 btn-focus hover:underline underline-offset-4',
 }
 
 export const dynamicButton: [RegExp, (params: RegExpExecArray) => string][] = [
   // base
-  [/^btn-focus(-(\S+))?$/, ([, , c = 'primary']) => `focus-visible:outline-${c}-500 dark:focus-visible:outline-${c}-400  focus-visible:outline !focus-visible:outline-2 focus-visible:outline-offset-2`],
+  [/^btn-focus(-(\S+))?$/, ([, , c = 'primary']) => `focus-visible:outline-${c}-500 dark:focus-visible:outline-${c}-400 focus-visible:outline-2 focus-visible:outline-offset-2`],
 
   // variants
   [/^btn-solid(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-inverted shadow-sm bg-${c}-600 hover:bg-${c}-500 dark:bg-${c}-500 dark:hover:bg-${c}-400`],
