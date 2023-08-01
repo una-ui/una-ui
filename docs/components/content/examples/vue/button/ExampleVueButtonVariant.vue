8279558011e3852481d7bca3244b096a2bfdf103
phojie rengel <phojrengel@gmail.com>
2023-08-02 06:16:58 +0800

    docs: update design principles section

--- docs/components/content/examples/vue/button/ExampleVueButtonVariant.vue
+++ docs/components/content/examples/vue/button/ExampleVueButtonVariant.vue
@@ -4,26 +4,32 @@
       label="Button Solid"
       btn="solid"
     />
+
     <NButton
       label="Button Outline"
       btn="outline"
     />
+
     <NButton
       label="Button Soft"
       btn="soft"
     />
+
     <NButton
       label="Button Ghost"
       btn="ghost"
     />
+
     <NButton
       label="Button Link"
       btn="link"
     />
+
     <NButton
       label="Button text"
       btn="text"
     />
+
     <NButton
       label="Button Base"
       btn="~"
