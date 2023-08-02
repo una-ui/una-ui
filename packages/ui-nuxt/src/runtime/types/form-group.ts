20e729ea39ef532ce8c3ccd053e09c2133b34a16
phojie rengel <phojrengel@gmail.com>
2023-08-02 12:25:50 +0800

    chore(nuxt): add tsdocs to form-group

--- packages/ui-nuxt/src/runtime/types/form-group.ts
+++ packages/ui-nuxt/src/runtime/types/form-group.ts
@@ -1,18 +1,74 @@
 export interface NFormGroupProps {
+  /**
+   * Update the form group status.
+   *
+   * @default null
+  */
   status?: 'info' | 'success' | 'warning' | 'error'
-
+  /**
+   * Add a required indicator to the form group.
+   *
+   * @default false
+  */
   required?: boolean
 
+  /**
+   * Manually sets the name attribute for the form group label.
+   * Slot children will inherit the name attribute as both `id` and `name` for accessibility.
+   *
+   * By default, the name attribute is generated randomly.
+   *
+   * @default randomId
+   * @example
+   * name="email"
+  */
   name?: string
+  /**
+   * Label for the form group.
+   *
+   * @example
+   * label="Email"
+  */
   label?: string
+  /**
+   * Display `hint` message for the form group.
+   *
+   * @example
+   * hint="Enter your email address"
+  */
   hint?: string
+  /**
+   * Display `Description` message for the form group.
+   *
+   * @example
+   * description="We will never share your email with anyone else."
+  */
   description?: string
+  /**
+   * Display `Message` for the form group.
+   * Useful for displaying validation errors.
+   *
+   * @example
+   * message="Email is required"
+  */
   message?: string
 
+  /**
+   * Display `counter` for the form group.
+   * Useful for displaying character count.
+   *
+   * @example
+   * counter="{ value: 0, max: 100 }"
+  */
   counter?: {
     value: number
     max?: number
   }
+  /**
+   * `NexveltUI` preset configuration
+   *
+   * @see https://github.com/nexvelt/ui/blob/main/packages/ui-preset/src/_shortcuts/accordion.ts
+  */
   nv?: {
     formGroupTopWrapper?: string
     formGroupTopWrapperInner?: string
