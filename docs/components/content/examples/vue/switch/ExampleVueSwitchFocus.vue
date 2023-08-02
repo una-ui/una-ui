91366fde3ad1e5740a417c63448c32e6b4dde615
phojie rengel <phojrengel@gmail.com>
2023-08-02 09:41:20 +0800

    docs: add switch focus example

---
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchFocus.vue
@@ -0,0 +1,48 @@
+<template>
+  <div flex gap-4>
+    <NSwitch
+      :model-value="true"
+      switch="focus"
+    />
+
+    <NSwitch
+      :model-value="true"
+      switch="focus-error"
+      :nv="{
+        switchSliderChecked: 'switch-checked-error',
+      }"
+    />
+
+    <NSwitch
+      :model-value="true"
+      switch="focus-info"
+      :nv="{
+        switchSliderChecked: 'switch-checked-info',
+      }"
+    />
+
+    <NSwitch
+      :model-value="true"
+      switch="focus-pink"
+      :nv="{
+        switchSliderChecked: 'switch-checked-pink',
+      }"
+    />
+
+    <NSwitch
+      :model-value="true"
+      switch="focus-red"
+      :nv="{
+        switchSliderChecked: 'switch-checked-red',
+      }"
+    />
+
+    <NSwitch
+      :model-value="true"
+      switch="focus-indigo"
+      :nv="{
+        switchSliderChecked: 'switch-checked-indigo',
+      }"
+    />
+  </div>
+</template>
