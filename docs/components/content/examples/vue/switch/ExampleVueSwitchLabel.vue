bfbc0631030439d8189df27f3aa5c0005372b4a0
phojie rengel <phojrengel@gmail.com>
2023-08-02 10:00:57 +0800

    docs: add initial switch label example

---
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchLabel.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const enabled = ref(false)
+</script>
+
+<template>
+  <NFormGroup :label="enabled ? 'Enabled' : 'Disabled'">
+    <NSwitch
+      v-model="enabled"
+    />
+  </NFormGroup>
+</template>
