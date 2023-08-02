a8dceaf67478bdda21324908a6bcd233487b5da3
phojie rengel <phojrengel@gmail.com>
2023-08-02 09:40:01 +0800

    docs: update switch size example

--- docs/components/content/examples/vue/switch/ExampleVueSwitchSize.vue
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchSize.vue
@@ -1,26 +1,8 @@
 <template>
-  <div flex="~ col" gap-4>
+  <div flex gap-4>
     <div>
       <NSwitch
         switch="xs"
-      />
-    </div>
-
-    <div>
-      <NSwitch
-        switch="sm"
-      />
-    </div>
-
-    <div>
-      <NSwitch
-        switch="md"
-      />
-    </div>
-
-    <div>
-      <NSwitch
-        switch="lg"
       />
     </div>
 
