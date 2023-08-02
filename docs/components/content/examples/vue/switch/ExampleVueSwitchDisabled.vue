1c52addf956a1e6044d9d28baf913a288bcb836d
phojie rengel <phojrengel@gmail.com>
2023-08-02 10:47:57 +0800

    docs: add switch disable example

---
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchDisabled.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const enabled = ref(false)
+</script>
+
+<template>
+  <div flex="~ col" gap-4>
+    <NFormGroup :label="!enabled ? 'Clickable' : 'Not clickable'">
+      <NSwitch
+        v-model="enabled"
+        :disabled="enabled"
+      />
+    </NFormGroup>
+
+    <NFormGroup :label="enabled ? 'Clickable' : 'Not clickable'">
+      <NSwitch
+        v-model="enabled"
+        :disabled="!enabled"
+      />
+    </NFormGroup>
+  </div>
+</template>
