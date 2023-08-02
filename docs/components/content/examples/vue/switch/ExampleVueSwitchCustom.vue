262f0170d25249f319a65b8e475c4a0ca05d2bee
phojie rengel <phojrengel@gmail.com>
2023-08-02 10:48:32 +0800

    docs: add switch custom example

---
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchCustom.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const enabled = ref(false)
+</script>
+
+<template>
+  <div flex gap-4 py-4>
+    <NSwitch
+      v-model="enabled"
+      switch="xl"
+      rotate-90
+    />
+
+    <NSwitch
+      v-model="enabled"
+      switch="2xl"
+      rotate-120 rounded-lg
+      :nv="{
+        switchUnchecked: 'switch-unchecked-indigo',
+        switchSlider: 'bg-inverted',
+      }"
+    />
+
+    <NSwitch
+      v-model="enabled"
+      switch="3xl"
+      rotate-45 rounded-none
+      :nv="{
+        switchChecked: 'switch-checked-yellow',
+        switchSlider: 'rounded-none',
+      }"
+    />
+
+    <NSwitch
+      v-model="enabled"
+      switch="4xl"
+      :nv="{
+        switchChecked: 'switch-checked-lime',
+        switchUnchecked: 'switch-unchecked-red',
+        switchSliderChecked: 'rounded-l-lg',
+        switchSliderUnchecked: 'rounded-r-lg',
+      }"
+    />
+  </div>
+</template>
