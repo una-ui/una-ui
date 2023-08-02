e4f7db9219a763ea4cb575d71b961450b5f0eb0c
phojie rengel <phojrengel@gmail.com>
2023-08-02 09:39:37 +0800

    docs: add switch color example

---
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchColor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div flex gap-4>
+    <NSwitch
+      :model-value="true"
+    />
+
+    <NSwitch
+      :model-value="true"
+      :nv="{
+        switchSliderChecked: 'switch-checked-error',
+      }"
+    />
+
+    <NSwitch
+      :model-value="true"
+      :nv="{
+        switchSliderChecked: 'switch-checked-info',
+      }"
+    />
+
+    <NSwitch
+      :model-value="true"
+      :nv="{
+        switchSliderChecked: 'switch-checked-pink',
+      }"
+    />
+
+    <NSwitch
+      :model-value="true"
+      :nv="{
+        switchSliderChecked: 'switch-checked-red',
+      }"
+    />
+
+    <NSwitch
+      :model-value="true"
+      :nv="{
+        switchSliderChecked: 'switch-checked-indigo',
+      }"
+    />
+  </div>
+</template>
