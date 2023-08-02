887351ae5929314e0a4f6f89e6096751b1d2a293
phojie rengel <phojrengel@gmail.com>
2023-08-02 16:02:40 +0800

    docs: update switch example

--- docs/components/content/examples/vue/switch/ExampleVueSwitchVariant.vue
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchVariant.vue
@@ -1,11 +1,9 @@
 <template>
   <div flex gap-4>
     <NSwitch
-      v-model="enabled"
       variant="normal"
     />
     <NSwitch
-      v-model="enabled"
       variant="short"
     />
   </div>
