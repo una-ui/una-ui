ac8704a446e409ddf35852eea183e983b6e4ceae
phojie rengel <phojrengel@gmail.com>
2023-08-02 08:58:05 +0800

    docs: add switch size examples

---
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchSize.vue
@@ -0,0 +1,49 @@
+<template>
+  <div flex="~ col" gap-4>
+    <div>
+      <NSwitch />
+    </div>
+
+    <div>
+      <NSwitch
+        switch="sm"
+      />
+    </div>
+
+    <div>
+      <NSwitch
+        switch="md"
+      />
+    </div>
+
+    <div>
+      <NSwitch
+        switch="lg"
+      />
+    </div>
+
+    <div>
+      <NSwitch
+        switch="xl"
+      />
+    </div>
+
+    <div>
+      <NSwitch
+        switch="3vw"
+      />
+    </div>
+
+    <div>
+      <NSwitch
+        switch="1cm"
+      />
+    </div>
+
+    <div>
+      <NSwitch
+        switch="18px hover:40px"
+      />
+    </div>
+  </div>
+</template>
