cb03c659668e7acf5e3777687aa8100decf4f81b
phojie rengel <phojrengel@gmail.com>
2023-08-02 15:49:42 +0800

    docs: add switch variant section and example

---
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchVariant.vue
@@ -0,0 +1,12 @@
+<template>
+  <div flex gap-4>
+    <NSwitch
+      v-model="enabled"
+      variant="normal"
+    />
+    <NSwitch
+      v-model="enabled"
+      variant="short"
+    />
+  </div>
+</template>
