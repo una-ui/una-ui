d9fdc82f67955f63dcc71a34b19b0d1da13e2698
phojie rengel <phojrengel@gmail.com>
2023-08-03 16:47:43 +0800

    style: init

--- packages/ui-nuxt/playground/app.vue
+++ packages/ui-nuxt/playground/app.vue
@@ -4,89 +4,16 @@ const input = ref('')
 </script>
 
 <template>
-  <div mx-auto mt-4 container>
-    <div class="mx-auto h-screen flex flex-col items-center justify-center gap-4 container">
-      <Tester />
+  <div class="mx-auto h-screen flex flex-col items-center justify-center gap-4 container">
+    <Tester />
 
-      <ColorMode />
-      <div class="grid grid-cols-2 gap-2 sm:grid-cols-4">
-        <NButton
-          label="Button"
-          btn="outline-indigo"
-        />
-        <NThemeSwitcher />
-      </div>
-
-      <form
-        class="grid grid-cols-1 w-lg gap-4 border border-base rounded-md bg-muted px-6 py-8"
-      >
-        <NFormGroup
-          label="Email"
-          description="We'll never share your email with anyone else."
-          :message="input.length > 0 ? `We'll never share your email with anyone else.` : 'You have entered an invalid email'"
-          hint="Optional"
-          :status="input.length > 0 ? undefined : 'error'"
-          required
-        >
-          <NInput
-            v-model="input"
-            placeholder="Email"
-          />
-        </NFormGroup>
-
-        <NFormGroup
-          label="Name"
-          name="firstname"
-          error="Firstname is required"
-          required
-        >
-          <NInput
-            id="firstname"
-            v-model="input"
-            status="success"
-            leading="i-heroicons-archive-box-x-mark"
-            trailing="i-heroicons-adjustments-vertical-solid"
-            placeholder="Firstname"
-          />
-        </NFormGroup>
-
-        <NFormGroup
-          label="Middlename"
-          hint="Optional"
-        >
-          <NInput
-            id="middlename"
-            input="outline-lime"
-            placeholder="Middlename Test"
-          />
-        </NFormGroup>
-
-        <NFormGroup
-          name="lastname"
-          label="Lastname"
-          required
-        >
-          <NInput
-            id="lastname"
-            input="outline-green"
-            placeholder="Lastname"
-          />
-        </NFormGroup>
-
-        <NButton
-          type="submit"
-          btn="solid"
-          :label="!isLoading ? 'Submit' : 'Processing...'"
-          :class="isLoading ? 'animate-pulse' : ''"
-          @click="isLoading = !isLoading"
-        />
-
-        <NButton
-          btn="solid-gray"
-          label="Cancel"
-          @click="isLoading = !isLoading"
-        />
-      </form>
-    </div>
+    <ColorMode />
+    <!-- <div class="grid grid-cols-2 gap-2 sm:grid-cols-4">
+      <NButton
+        label="Button"
+        btn="outline-indigo"
+      />
+      <NThemeSwitcher />
+    </div> -->
   </div>
 </template>
--- packages/ui-nuxt/playground/components/Tester.vue
+++ packages/ui-nuxt/playground/components/Tester.vue
@@ -1,31 +1,56 @@
 <script setup lang="ts">
-const items = [
-  {
-    label: 'How do I get started?',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
-  },
-  {
-    label: 'What is your return policy?',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
-  },
-  {
-    label: 'Can I exchange an item?',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel urna vitae lectus aliquet mollis et eget risus.',
-  },
-]
+const enabled = ref(false)
 </script>
 
 <template>
-  <div>
-    <button
-      type="button"
-      btn
-    >
-      Button
-    </button>
-  </div>
+  <div grid="~ cols-2 sm:cols-3" gap-4>
+    <NButton
+      label="Button Solid"
+      btn="solid"
+    />
 
-  <NAccordion
-    :items="items"
-  />
+    <NButton
+      label="Button Solid"
+    />
+
+    <NButton
+      label="Button Outline"
+      btn="outline"
+    />
+
+    <NButton
+      label="Button Soft"
+      btn="soft"
+    />
+
+    <NButton
+      label="Button Ghost"
+      btn="ghost"
+    />
+
+    <NButton
+      label="Button Link"
+      btn="link"
+    />
+
+    <NButton
+      label="Button text"
+      btn="text"
+    />
+
+    <NButton
+      label="Button Base"
+      btn="soft"
+    />
+
+    <NSwitch
+      v-model="enabled"
+      type="short"
+      switch="orange focus"
+    />
+
+    <NSwitch
+      v-model="enabled"
+    />
+  </div>
 </template>
