5cd96e39db39b187b00b84852a6d0cad28c517b3
phojie rengel <phojrengel@gmail.com>
2023-08-04 19:14:37 +0800

    docs: add switch loading prop section and example

---
+++ docs/components/content/examples/vue/switch/ExampleVueSwitchLoading.vue
@@ -0,0 +1,45 @@
+<template>
+  <div flex items-center gap-4>
+    <NSwitch
+      :model-value="true"
+      outset
+      loading
+      switch="primary"
+    />
+
+    <NSwitch
+      :model-value="false"
+      switch="green"
+      loading
+    />
+
+    <NSwitch
+      :model-value="false"
+      outset
+      size="lg"
+      loading
+      switch="info"
+    />
+
+    <NSwitch
+      :model-value="true"
+      loading
+      size="7"
+      switch="pink"
+    />
+
+    <NSwitch
+      :model-value="true"
+      outset
+      loading
+      size="8"
+      switch="purple"
+    />
+
+    <NSwitch
+      :model-value="true"
+      loading
+      switch="warning"
+    />
+  </div>
+</template>
