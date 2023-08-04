beadb3a7b10e63438b39754647525f24d8cfa62f
phojie rengel <phojrengel@gmail.com>
2023-08-05 06:06:30 +0800

    docs: add button loading slot section and example

---
+++ docs/components/content/examples/vue/button/ExampleVueButtonLoadingSlot.vue
@@ -0,0 +1,15 @@
+<template>
+  <NButton
+    loading
+    label="Button"
+  >
+    <template #loading>
+      <span class="animate-pulse rounded-full bg-base">
+        <img
+          class="h-5"
+          src="https://avatars.githubusercontent.com/u/33350692?s=400&u=49395c835e8197ae2ee42ca02c95e828d8f64239&v=4"
+        >
+      </span>
+    </template>
+  </NButton>
+</template>
