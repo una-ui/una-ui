b0cf169928631bf1647688ea0c7605a0bd8cc431
phojie rengel <phojrengel@gmail.com>
2023-08-06 03:13:01 +0800

    fix(docs): update button(size) outdated example

--- docs/components/content/examples/vue/button/ExampleVueButtonSize.vue
+++ docs/components/content/examples/vue/button/ExampleVueButtonSize.vue
@@ -2,56 +2,57 @@
   <div flex="~ col" gap-4>
     <div>
       <NButton
-        btn="xs"
+        size="xs"
         label="button xs"
       />
     </div>
 
     <div>
       <NButton
-        btn="sm"
+        size="sm"
         label="button sm (default)"
       />
     </div>
 
     <div>
       <NButton
-        btn="md"
+        size="md"
         label="button md"
       />
     </div>
 
     <div>
       <NButton
-        btn="lg"
+        size="lg"
         label="button lg"
       />
     </div>
 
     <div>
       <NButton
-        btn="xl"
+        size="xl"
         label="button xl"
       />
     </div>
 
     <div>
       <NButton
-        btn="3vw"
+        size="3vw"
         label="button 3vw"
       />
     </div>
 
     <div>
       <NButton
-        btn="1cm"
+        size="1cm"
         label="button 1cm"
       />
     </div>
 
     <div>
       <NButton
-        btn="solid hover:outline-lime 18px hover:40px"
+        size="18px hover:40px"
+        btn="solid hover:outline-lime"
         label="hover me 18px->40px"
       />
     </div>
