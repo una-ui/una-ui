a7138a36fc34faff8f833ae2b15d376ba1adcbcd
phojie rengel <phojrengel@gmail.com>
2023-08-06 02:58:32 +0800

    docs: update input leading slot example

--- docs/components/content/examples/vue/input/ExampleVueInputLeadingSlot.vue
+++ docs/components/content/examples/vue/input/ExampleVueInputLeadingSlot.vue
@@ -7,7 +7,7 @@
       <!-- TODO convert to NAvatar soon -->
       <div class="rounded-full bg-base">
         <img
-          class="h-5"
+          class="h-5.5"
           src="https://avatars.githubusercontent.com/u/33350692?s=400&u=49395c835e8197ae2ee42ca02c95e828d8f64239&v=4"
         >
       </div>
