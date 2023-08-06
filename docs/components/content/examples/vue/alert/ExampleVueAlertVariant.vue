7cd895004af9af937a0161b8ae229b396a2d1fc2
phojie rengel <phojrengel@gmail.com>
2023-08-06 18:31:37 +0800

    docs: update alert variant example

---
+++ docs/components/content/examples/vue/alert/ExampleVueAlertVariant.vue
@@ -0,0 +1,21 @@
+<template>
+  <div flex="~ col" space-y-4>
+    <NAlert
+      alert="outline"
+      title="Outline variant (default)"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
+    />
+
+    <NAlert
+      alert="soft"
+      title="Soft variant"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
+    />
+
+    <NAlert
+      alert="border"
+      title="Border variant"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
+    />
+  </div>
+</template>
