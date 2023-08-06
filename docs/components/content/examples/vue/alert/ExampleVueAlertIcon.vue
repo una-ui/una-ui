1277c6c723a73f0b2b9a68da91de882a68483945
phojie rengel <phojrengel@gmail.com>
2023-08-06 19:10:33 +0800

    docs: update alert icon example

--- docs/components/content/examples/vue/alert/ExampleVueAlertIcon.vue
+++ docs/components/content/examples/vue/alert/ExampleVueAlertIcon.vue
@@ -1,63 +1,38 @@
-<script setup lang="ts">
-const description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.'
-</script>
-
 <template>
   <div flex="~ col" space-y-4>
     <NAlert
+      alert="outline"
+      title="Customize icon"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
+      icon="i-heroicons-beaker-20-solid"
+    />
+
+    <NAlert
       alert="outline-info"
-      title="Info color"
-      :description="description"
+      title="Info icon"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
       icon
     />
 
     <NAlert
       alert="outline-success"
-      title="Success color"
-      :description="description"
+      title="Success icon"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
       icon
     />
 
     <NAlert
       alert="outline-warning"
-      title="Warning color"
-      :description="description"
+      title="Warning icon"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
       icon
     />
 
     <NAlert
       alert="outline-error"
-      title="Error color"
-      :description="description"
+      title="Error icon"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
       icon
-    />
-
-    <NAlert
-      alert="soft-purple"
-      title="Purple color"
-      :description="description"
-      icon="i-heroicons-shield-check-20-solid"
-    />
-
-    <NAlert
-      alert="soft-lime"
-      title="Lime color"
-      :description="description"
-      icon="i-heroicons-banknotes-20-solid"
-    />
-
-    <NAlert
-      alert="soft-orange"
-      title="Orange color"
-      :description="description"
-      icon="i-heroicons-face-smile-20-solid"
-    />
-
-    <NAlert
-      alert="soft-sky"
-      title="Sky color"
-      :description="description"
-      icon="i-heroicons-bolt-20-solid"
     />
   </div>
 </template>
