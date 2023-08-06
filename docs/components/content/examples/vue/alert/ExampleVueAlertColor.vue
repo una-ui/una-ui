bb1375c132f32c4cf60a727564e22b848a2b27c1
phojie rengel <phojrengel@gmail.com>
2023-08-06 18:09:52 +0800

    docs: add alert color example

---
+++ docs/components/content/examples/vue/alert/ExampleVueAlertColor.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.'
+</script>
+
+<template>
+  <div flex="~ col" space-y-4>
+    <NAlert alert="outline-info" title="Info color" :description="description" />
+
+    <NAlert alert="outline-success" title="Success color" :description="description" />
+
+    <NAlert alert="outline-warning" title="Warning color" :description="description" />
+
+    <NAlert alert="outline-error" title="Error color" :description="description" />
+
+    <NAlert alert="soft-purple" title="Purple color" :description="description" />
+
+    <NAlert alert="soft-lime" title="Lime color" :description="description" />
+
+    <NAlert alert="soft-orange" title="Orange color" :description="description" />
+
+    <NAlert alert="soft-sky" title="Sky color" :description="description" />
+  </div>
+</template>
