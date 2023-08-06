90f4508412b988020b3ea14682262389b8b7170f
phojie rengel <phojrengel@gmail.com>
2023-08-06 18:30:56 +0800

    docs: update alert color example

--- docs/components/content/examples/vue/alert/ExampleVueAlertColor.vue
+++ docs/components/content/examples/vue/alert/ExampleVueAlertColor.vue
@@ -6,18 +6,18 @@ const description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Al
   <div flex="~ col" space-y-4>
     <NAlert alert="outline-info" title="Info color" :description="description" />
 
-    <NAlert alert="outline-success" title="Success color" :description="description" />
+    <NAlert alert="border-success" title="Success color" :description="description" />
 
     <NAlert alert="outline-warning" title="Warning color" :description="description" />
 
-    <NAlert alert="outline-error" title="Error color" :description="description" />
+    <NAlert alert="border-error" title="Error color" :description="description" />
 
     <NAlert alert="soft-purple" title="Purple color" :description="description" />
 
-    <NAlert alert="soft-lime" title="Lime color" :description="description" />
+    <NAlert alert="outline-lime" title="Lime color" :description="description" />
 
-    <NAlert alert="soft-orange" title="Orange color" :description="description" />
+    <NAlert alert="border-pink" title="Pink color" :description="description" />
 
-    <NAlert alert="soft-sky" title="Sky color" :description="description" />
+    <NAlert alert="outline-sky" title="Sky color" :description="description" />
   </div>
 </template>
