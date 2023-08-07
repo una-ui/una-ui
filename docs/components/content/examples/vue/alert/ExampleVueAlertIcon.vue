9eec6fd71260b1c3e3a2feb2c31a869d859f9c57
phojie rengel <phojrengel@gmail.com>
2023-08-07 22:06:50 +0800

    docs: update alert icon example

--- docs/components/content/examples/vue/alert/ExampleVueAlertIcon.vue
+++ docs/components/content/examples/vue/alert/ExampleVueAlertIcon.vue
@@ -1,7 +1,6 @@
 <template>
   <div flex="~ col" space-y-4>
     <NAlert
-      alert="outline"
       title="Customize icon"
       description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
       icon="i-heroicons-beaker-20-solid"
