0befcf9e05f8c994276d98ba21611072d605512f
phojie rengel <phojrengel@gmail.com>
2023-08-08 01:28:55 +0800

    docs: add initial badge section and examples

--- docs/components/content/examples/vue/badge/ExampleVueBadgeBasic.vue
+++ docs/components/content/examples/vue/badge/ExampleVueBadgeBasic.vue
@@ -1,5 +1,21 @@
 <template>
-  <NBadge>
-    Badge
-  </NBadge>
+  <div flex gap-4>
+    <NBadge label="Soft" />
+
+    <NBadge badge="solid" label="Solid" />
+
+    <NBadge badge="outline" label="Outline" />
+
+    <NBadge badge="soft-orange square">
+      Orange Square (slot)
+    </NBadge>
+
+    <NBadge badge="solid-lime" rounded-full>
+      Lime Rounded (slot)
+    </NBadge>
+
+    <NBadge badge="outline-pink" rounded-none>
+      Outline Pink (slot)
+    </NBadge>
+  </div>
 </template>
