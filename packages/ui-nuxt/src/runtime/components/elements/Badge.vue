69100d472995e4591c45d3afea010c902368de36
phojie rengel <phojrengel@gmail.com>
2023-08-08 01:28:10 +0800

    chore(NBadge): init

--- packages/ui-nuxt/src/runtime/components/elements/Badge.vue
+++ packages/ui-nuxt/src/runtime/components/elements/Badge.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import type { NBadgeProps } from '../../types'
 
+const props = defineProps<NBadgeProps>()
+
+const badgeVariants = ['solid', 'soft', 'outline', '~'] as const
+const hasVariant = computed(() => badgeVariants.some(badgeVariants => props.badge?.includes(badgeVariants)))
 </script>
 
 <template>
-  <div>
-    <slot />
-  </div>
+  <span
+    :badge="badge"
+    :class="{ 'badge-default-variant': !hasVariant }"
+  >
+    <slot>
+      {{ label }}
+    </slot>
+  </span>
 </template>
