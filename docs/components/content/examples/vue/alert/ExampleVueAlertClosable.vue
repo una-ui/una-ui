55bb9b701814866e5d901c8ac7ac6dd4d22e106a
phojie rengel <phojrengel@gmail.com>
2023-08-07 09:27:36 +0800

    docs: add alert closable section and example

---
+++ docs/components/content/examples/vue/alert/ExampleVueAlertClosable.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const close = ref(false)
+
+// auto close after 1 second
+watch(close, (value) => {
+  if (value) {
+    setTimeout(() => {
+      close.value = false
+    }, 1000)
+  }
+})
+</script>
+
+<template>
+  <NAlert
+    v-if="!close"
+    title="Attention needed"
+    description="dLorem ipsum dolor sit amet consectetur adipisicing elit. aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
+    closable
+    @close="close = true"
+  />
+</template>
