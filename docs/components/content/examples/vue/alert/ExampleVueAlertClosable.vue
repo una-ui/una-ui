37f7e04a74c5674af03bcf33aad7982c633355ca
phojie rengel <phojrengel@gmail.com>
2023-08-07 13:58:51 +0800

    docs: update alert closable example

--- docs/components/content/examples/vue/alert/ExampleVueAlertClosable.vue
+++ docs/components/content/examples/vue/alert/ExampleVueAlertClosable.vue
@@ -1,22 +1,33 @@
 <script setup lang="ts">
-const close = ref(false)
+const close1 = ref(false)
+const close2 = ref(false)
 
-// auto close after 1 second
-watch(close, (value) => {
-  if (value) {
+// if close1 and close2 are true, reset them to false after 1 second
+watch([close1, close2], () => {
+  if (close1.value && close2.value) {
     setTimeout(() => {
-      close.value = false
+      close1.value = false
+      close2.value = false
     }, 1000)
   }
 })
 </script>
 
 <template>
-  <NAlert
-    v-if="!close"
-    title="Attention needed"
-    description="dLorem ipsum dolor sit amet consectetur adipisicing elit. aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
-    closable
-    @close="close = true"
-  />
+  <div flex="~ col" space-y-4>
+    <NAlert
+      v-if="!close1"
+      title="Closable alert 1"
+      closable
+      @close="close1 = true"
+    />
+
+    <NAlert
+      v-if="!close2"
+      title="Closable alert 2"
+      closable
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
+      @close="close2 = true"
+    />
+  </div>
 </template>
