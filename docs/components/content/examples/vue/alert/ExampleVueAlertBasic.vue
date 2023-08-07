6f9ddbd0fe2f26102d37089ae058dfceb710bde0
phojie rengel <phojrengel@gmail.com>
2023-08-07 18:01:36 +0800

    docs: update alert basic example

--- docs/components/content/examples/vue/alert/ExampleVueAlertBasic.vue
+++ docs/components/content/examples/vue/alert/ExampleVueAlertBasic.vue
@@ -1,6 +1,6 @@
 <template>
   <NAlert
-    title="Attention needed"
-    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
+    title="WARNING"
+    description="This documentation is still a work in progress. The package itself is not yet officially released and some sections of this documentation may be incomplete or missing entirely until the first release. We are working hard to complete the documentation as soon as possible."
   />
 </template>
