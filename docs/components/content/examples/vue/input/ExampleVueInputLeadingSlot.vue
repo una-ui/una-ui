7a9ec18a6fb876d1e9dbb95888b3ec05ebf95180
phojie rengel <phojrengel@gmail.com>
2023-08-08 10:17:09 +0800

    docs: update input slot examples

--- docs/components/content/examples/vue/input/ExampleVueInputLeadingSlot.vue
+++ docs/components/content/examples/vue/input/ExampleVueInputLeadingSlot.vue
@@ -1,16 +1,18 @@
 <template>
-  <NInput
-    placeholder="Search"
-    trailing="i-heroicons-chat-bubble-left-right-20-solid"
-  >
-    <template #leading>
-      <!-- TODO convert to NAvatar soon -->
-      <div class="rounded-full bg-base">
-        <img
-          class="h-5.5"
-          src="https://avatars.githubusercontent.com/u/33350692?s=400&u=49395c835e8197ae2ee42ca02c95e828d8f64239&v=4"
-        >
-      </div>
-    </template>
-  </NInput>
+  <div flex>
+    <NInput
+      placeholder="Search"
+      trailing="i-heroicons-chat-bubble-left-right-20-solid"
+    >
+      <template #leading>
+        <!-- TODO convert to NAvatar soon -->
+        <div class="rounded-full bg-base">
+          <img
+            class="h-5"
+            src="https://avatars.githubusercontent.com/u/33350692?s=400&u=49395c835e8197ae2ee42ca02c95e828d8f64239&v=4"
+          >
+        </div>
+      </template>
+    </NInput>
+  </div>
 </template>
--- docs/components/content/examples/vue/input/ExampleVueInputTrailingSlot.vue
+++ docs/components/content/examples/vue/input/ExampleVueInputTrailingSlot.vue
@@ -1,13 +1,15 @@
 <template>
-  <NInput
-    leading="i-heroicons-currency-dollar-20-solid"
-    placeholder="Search"
-    class="pe-3em"
-  >
-    <template #trailing>
-      <span class="text-xs font-bold">
-        USD
-      </span>
-    </template>
-  </NInput>
+  <div flex>
+    <NInput
+      leading="i-heroicons-currency-dollar-20-solid"
+      placeholder="Search"
+      class="pe-3em"
+    >
+      <template #trailing>
+        <span class="text-xs font-semibold">
+          USD
+        </span>
+      </template>
+    </NInput>
+  </div>
 </template>
