8ec0f08f833c650ccf6adc84ae9b9f39bee2a82e
phojie rengel <phojrengel@gmail.com>
2023-08-09 03:50:27 +0800

    docs: add badge size example

---
+++ docs/components/content/examples/vue/badge/ExampleVueBadgeSize.vue
@@ -0,0 +1,9 @@
+<template>
+  <div flex-1 space-x-4 space-y-2>
+    <NBadge size="sm" badge="soft" label="Badge" />
+
+    <NBadge size="lg" badge="solid" label="Badge" />
+
+    <NBadge size="20px" badge="outline" label="Badge" />
+  </div>
+</template>
