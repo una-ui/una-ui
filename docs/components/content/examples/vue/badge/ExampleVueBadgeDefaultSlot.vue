150d95362e37809cc73b62ba39a21983af7fcb68
phojie rengel <phojrengel@gmail.com>
2023-08-09 02:52:29 +0800

    docs: add badge default slot example

---
+++ docs/components/content/examples/vue/badge/ExampleVueBadgeDefaultSlot.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const openSourser = [
+  {
+    name: 'Evan You',
+    avatar: 'https://avatars.githubusercontent.com/u/499550?v=4',
+  },
+  {
+    name: 'Anthony Fu',
+    avatar: 'https://avatars.githubusercontent.com/u/11247099?v=4',
+  },
+  {
+    name: 'Daniel Roe',
+    avatar: 'https://avatars.githubusercontent.com/u/28706372?v=4',
+  },
+  {
+    name: 'Sébastien Chopin',
+    avatar: 'https://avatars.githubusercontent.com/u/904724?v=4',
+  },
+  {
+    name: 'Pooya Parsa',
+    avatar: 'https://avatars.githubusercontent.com/u/5158436?v=4',
+  },
+  {
+    name: 'Eduardo San Martin Morote',
+    avatar: 'https://avatars.githubusercontent.com/u/664177?v=4',
+  },
+  {
+    name: 'Guillaume Chau',
+    avatar: 'https://avatars.githubusercontent.com/u/2798204?v=4',
+  },
+]
+</script>
+
+<template>
+  <div flex="~ wrap" gap-2>
+    <NBadge
+      v-for="{ name, avatar } in openSourser"
+      :key="name"
+      badge="solid-black"
+      closable
+    >
+      <!-- TODO convert to NAvatar soon -->
+      <img
+        class="h-5 rounded-full bg-base"
+        :src="avatar"
+      >
+
+      {{ name }}
+    </NBadge>
+  </div>
+</template>
