5e76b0b5eeea02d2595e5c5b06a9318fe3da5e2b
phojie rengel <phojrengel@gmail.com>
2023-08-08 19:12:34 +0800

    docs: update badge basic section and example

--- docs/components/content/examples/vue/badge/ExampleVueBadgeBasic.vue
+++ docs/components/content/examples/vue/badge/ExampleVueBadgeBasic.vue
@@ -1,3 +1,11 @@
 <template>
-  <NBadge label="Badge" />
+  <div flex space-x-4>
+    <!-- label syntax -->
+    <NBadge label="Badge 1" />
+
+    <!-- slot syntax -->
+    <NBadge>
+      Badge 2
+    </NBadge>
+  </div>
 </template>
