a849fb3ec48b87b1eb860c1658800a5a1c1cd8fe
phojie rengel <phojrengel@gmail.com>
2023-08-08 18:00:25 +0800

    docs: add badge basic example

--- docs/components/content/examples/vue/badge/ExampleVueBadgeBasic.vue
+++ docs/components/content/examples/vue/badge/ExampleVueBadgeBasic.vue
@@ -1,27 +1,3 @@
 <template>
-  <div flex flex-wrap gap-2>
-    <NBadge label="Soft" />
-
-    <NBadge badge="solid" label="Solid" />
-
-    <NBadge badge="solid-gray" label="Solid" />
-
-    <NBadge badge="outline" label="Outline" />
-
-    <NBadge badge="soft-orange square">
-      Orange Square (slot)
-    </NBadge>
-
-    <NBadge badge="solid-lime" rounded-full>
-      Lime Rounded (slot)
-    </NBadge>
-
-    <NBadge badge="outline-pink" rounded-none>
-      Outline Pink (slot)
-    </NBadge>
-
-    <NBadge badge="outline-pink" rounded-none>
-      Outline Pink (slot)
-    </NBadge>
-  </div>
+  <NBadge label="Badge" />
 </template>
