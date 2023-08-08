b8ef1e8711ce0fd4649da93370abc7f7b3413bb3
phojie rengel <phojrengel@gmail.com>
2023-08-08 10:37:21 +0800

    docs: add icon default section and example

---
+++ docs/components/content/examples/vue/icon/ExampleVueIconDefault.vue
@@ -0,0 +1,13 @@
+<template>
+  <div flex="~ wrap items-center" gap-4 size="2xl">
+    <NIcon name="i-info" text-info />
+
+    <NIcon name="i-success" text-success />
+
+    <NIcon name="i-warning" text-warning />
+
+    <NIcon name="i-error" text-error />
+
+    <NIcon name="i-loading" animate-spin />
+  </div>
+</template>
