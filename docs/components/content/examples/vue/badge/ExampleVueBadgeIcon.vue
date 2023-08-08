6f0378ad599886b2df9dd3bcf2de70125fb4886f
phojie rengel <phojrengel@gmail.com>
2023-08-09 02:53:52 +0800

    docs: add badge icon example

---
+++ docs/components/content/examples/vue/badge/ExampleVueBadgeIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <div flex="~ wrap" gap-2>
+    <NBadge label="v1.2.0" icon="i-heroicons-sparkles-solid text-primary" class="rounded-full" />
+
+    <NBadge badge="soft-gray" label="Offline" icon="i-tabler-circle-filled text-7px text-gray" />
+
+    <NBadge badge="soft-gray" label="Online" icon="i-tabler-circle-filled text-7px text-green" />
+  </div>
+</template>
