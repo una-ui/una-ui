f5a739e6cab97e99563c24e975b9e6e6c3ec79a2
phojie rengel <phojrengel@gmail.com>
2023-08-08 10:15:34 +0800

    docs: update input icon example

--- docs/components/content/examples/vue/input/ExampleVueInputIcon.vue
+++ docs/components/content/examples/vue/input/ExampleVueInputIcon.vue
@@ -1,20 +1,20 @@
 <template>
   <div grid="~ cols-1 sm:cols-2" gap-4>
-    <div col-span-1>
+    <div sm:col-span-1>
       <NInput
         leading="i-heroicons-magnifying-glass-20-solid"
         placeholder="This is leading icon"
       />
     </div>
 
-    <div col-span-1>
+    <div sm:col-span-1>
       <NInput
         trailing="i-heroicons-question-mark-circle-20-solid text-primary"
         placeholder="This is trailing icon with custom class"
       />
     </div>
 
-    <div col-span-2>
+    <div sm:col-span-2>
       <NInput
         input="outline-purple"
         size="1.3rem"
