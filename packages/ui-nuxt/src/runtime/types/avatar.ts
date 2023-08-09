2b46e0a42c24b34adcd52c3d7dc86c3dd8f0e2f3
phojie rengel <phojrengel@gmail.com>
2023-08-10 01:19:47 +0800

    chore(NAvatar): init

--- packages/ui-nuxt/src/runtime/types/avatar.ts
+++ packages/ui-nuxt/src/runtime/types/avatar.ts
@@ -1,2 +1,7 @@
 export interface NAvatarProps {
+  avatar?: string
+  src?: string
+  alt?: string
+  fallback?: string
+  delay?: number
 }
--- packages/ui-preset/src/_shortcuts/avatar.ts
+++ packages/ui-preset/src/_shortcuts/avatar.ts
@@ -1,12 +1,20 @@
 type AvatarPrefix = 'avatar'
 
 export const staticAvatar: Record<`${AvatarPrefix}-${string}` | AvatarPrefix, string> = {
+  // config
+  'avatar-default-variant': 'avatar-solid-gray',
+
   // base
-  'avatar': 'inline-flex items-center justify-center overflow-hidden',
-  'avatar-base': 'flex-none',
+  'avatar': 'relative font-medium leading-none h-2.5em w-2.5em text-brand inline-flex items-center justify-center rounded-full overflow-hidden',
+  'avatar-fallback': '',
+
+  'avatar-src': 'w-full h-full',
 }
 
-export const dynamicAvatar: [RegExp, (params: RegExpExecArray) => string][] = []
+export const dynamicAvatar: [RegExp, (params: RegExpExecArray) => string][] = [
+  [/^avatar-solid(-(\S+))?$/, ([, , c = 'primary']) => `avatar bg-${c}-600 dark:bg-${c}-500 text-inverted`],
+  // [/^avatar-soft(-(\S+))?$/, ([, , c = 'primary']) => `avatar bg-${c}-50 n-${c}-700 dark:n-${c}-400 ring-1 ring-${c}-700/10 ring-inset dark:bg-${c}-400/10 dark:ring-${c}-400/30`],
+]
 
 export const avatar = [
   ...dynamicAvatar,
