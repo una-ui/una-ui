1846eb2e57be1cdb18b75f3473b6e4523422e821
phojie rengel <phojrengel@gmail.com>
2023-08-10 01:20:48 +0800

    chore(NAvatar): init

---
+++ packages/ui-nuxt/src/runtime/components/elements/Avatar.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { useImage } from '@vueuse/core'
+import { computed } from 'vue'
+import type { NAvatarProps } from '../../types'
+
+const props = withDefaults(defineProps<NAvatarProps>(), {
+  delay: 0,
+})
+
+const { isLoading, error, isReady } = useImage({ src: props?.src ?? '' }, { delay: props.delay })
+
+const avatarVariants = ['solid', 'soft', 'outline', '~'] as const
+const hasVariant = computed(() => avatarVariants.some(avatarVariants => props.avatar?.includes(avatarVariants)))
+
+const placeholder = computed(() => {
+  if (props.fallback)
+    return props.fallback
+
+  return props.alt?.split(' ').map(word => word[0]).join('').slice(0, 2)
+})
+</script>
+
+<template>
+  <span
+    :avatar="avatar"
+    :class="{ 'avatar-default-variant': !hasVariant }"
+  >
+    <slot v-bind="{ isLoading, error, isReady }">
+      <!-- fallback -->
+      <slot
+        v-if="error || !isReady || isLoading && !placeholder"
+        name="fallback"
+      >
+        <span
+          avatar="fallback"
+        >
+          {{ placeholder }}
+        </span>
+      </slot>
+
+      <!-- image -->
+      <img
+        v-else
+        avatar="src"
+        :src="src"
+        :alt="alt"
+        loading="lazy"
+      >
+    </slot>
+  </span>
+</template>
