75dc5220b244b76b6a938dde360627c81afb66de
phojie rengel <phojrengel@gmail.com>
2023-08-10 01:36:47 +0800

    docs: add initial avatar colors and badge example

---
+++ docs/components/content/examples/vue/avatar/ExampleVueAvatarBadge.vue
@@ -0,0 +1,9 @@
+<template>
+  <div flex="~ items-center" gap-4>
+    <NAvatar>
+      PR
+    </NAvatar>
+
+    <NAvatar src="/images/avatar.png" alt="Phojie Rengel" />
+  </div>
+</template>
---
+++ docs/components/content/examples/vue/avatar/ExampleVueAvatarColor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div flex="~ col" gap-4>
+    <span class="text-sm font-medium">Dynamic colors:</span>
+    <div flex="~ items-center" gap-4>
+      <NAvatar avatar="solid-primary">
+        PR
+      </NAvatar>
+
+      <NAvatar avatar="solid-lime">
+        PR
+      </NAvatar>
+
+      <NAvatar avatar="solid-blue">
+        PR
+      </NAvatar>
+
+      <NAvatar avatar="solid-error">
+        PR
+      </NAvatar>
+
+      <NAvatar avatar="solid-purple">
+        PR
+      </NAvatar>
+
+      <NAvatar avatar="solid-pink">
+        PR
+      </NAvatar>
+    </div>
+
+    <hr border="base">
+
+    <span class="text-sm font-medium">Static colors:</span>
+
+    <div flex="~ items-center" gap-4>
+      <NAvatar avatar="solid-gray">
+        PR
+      </NAvatar>
+    </div>
+  </div>
+</template>
