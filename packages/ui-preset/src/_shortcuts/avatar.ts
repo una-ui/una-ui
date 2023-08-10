445281c1c3ec07f64957b805e94a9562bbc63b28
phojie rengel <phojrengel@gmail.com>
2023-08-10 14:08:28 +0800

    chore(avatar): update variants style

--- packages/ui-preset/src/_shortcuts/avatar.ts
+++ packages/ui-preset/src/_shortcuts/avatar.ts
@@ -6,7 +6,7 @@ export const staticAvatar: Record<`${AvatarPrefix}-${string}` | AvatarPrefix, st
 
   // base
   'avatar': 'relative font-medium leading-none h-2.5em w-2.5em inline-flex items-center justify-center rounded-full overflow-hidden',
-  'avatar-fallback': '',
+  'avatar-placeholder': '',
 
   'avatar-src': 'w-full h-full',
   'avatar-solid-white': 'avatar bg-base text-base ring-1 ring-base',
@@ -14,9 +14,9 @@ export const staticAvatar: Record<`${AvatarPrefix}-${string}` | AvatarPrefix, st
 }
 
 export const dynamicAvatar: [RegExp, (params: RegExpExecArray) => string][] = [
-  [/^avatar-solid(-(\S+))?$/, ([, , c = 'primary']) => `bg-${c}-600 dark:bg-${c}-500 text-inverted`],
-  [/^avatar-soft(-(\S+))?$/, ([, , c = 'primary']) => `text-brand bg-${c}-50 n-${c}-700 dark:n-${c}-400 ring-1 ring-${c}-700/10 ring-inset dark:bg-${c}-400/10 dark:ring-${c}-400/30`],
-  [/^avatar-outline(-(\S+))?$/, ([, , c = 'primary']) => `bg-transparent n-${c}-700 dark:n-${c}-400 ring-1 ring-${c}-700/10 dark:ring-${c}-400/30`],
+  [/^avatar-solid(-(\S+))?$/, ([, , c = 'gray']) => `bg-${c}-600 dark:bg-${c}-500 text-inverted`],
+  [/^avatar-soft(-(\S+))?$/, ([, , c = 'gray']) => `bg-${c}-50 text-${c}-700 dark:text-${c}-400 ring-1 ring-${c}-700/10 dark:bg-${c}-400/10 dark:ring-${c}-400/30`],
+  [/^avatar-outline(-(\S+))?$/, ([, , c = 'gray']) => `bg-transparent text-${c}-500 dark:text-${c}-400 ring-1 ring-current`],
 ]
 
 export const avatar = [
