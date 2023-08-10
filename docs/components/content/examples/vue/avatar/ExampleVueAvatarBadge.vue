670c3b24b41d9e7f056a6f8b56a82972f36be496
phojie rengel <phojrengel@gmail.com>
2023-08-10 15:38:02 +0800

    stlye: lint

--- docs/components/content/examples/vue/avatar/ExampleVueAvatarBadge.vue
+++ docs/components/content/examples/vue/avatar/ExampleVueAvatarBadge.vue
@@ -9,7 +9,6 @@
 
     <div class="relative">
       <NBadge badge="~" class="absolute bottom-1 right-0 z-1 h-3 w-3 rounded-full bg-success p-none ring-2 ring-$c-background" />
-
       <NAvatar src="/images/avatar.png" alt="Phojie Rengel" />
     </div>
   </div>
