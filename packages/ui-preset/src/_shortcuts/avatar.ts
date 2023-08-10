09892c3f8f56be72d373c7d9f32995e534513ad5
phojie rengel <phojrengel@gmail.com>
2023-08-10 15:02:42 +0800

    style: lint

--- packages/ui-preset/src/_shortcuts/avatar.ts
+++ packages/ui-preset/src/_shortcuts/avatar.ts
@@ -9,13 +9,15 @@ export const staticAvatar: Record<`${AvatarPrefix}-${string}` | AvatarPrefix, st
   'avatar': 'relative font-medium leading-none h-2.5em w-2.5em inline-flex items-center justify-center rounded-full overflow-hidden',
   'avatar-label': '',
   'avatar-icon-base': 'text-1.5em',
-
   'avatar-src': 'w-full h-full',
+
+  // variants
   'avatar-solid-white': 'avatar bg-base text-base ring-1 ring-base',
   'avatar-solid-black': 'avatar bg-inverted text-inverted',
 }
 
 export const dynamicAvatar: [RegExp, (params: RegExpExecArray) => string][] = [
+  // variants
   [/^avatar-solid(-(\S+))?$/, ([, , c = 'gray']) => `bg-${c}-600 dark:bg-${c}-500 text-inverted`],
   [/^avatar-soft(-(\S+))?$/, ([, , c = 'gray']) => `bg-${c}-50 text-${c}-700 dark:text-${c}-400 ring-1 ring-${c}-700/10 dark:bg-${c}-400/10 dark:ring-${c}-400/30`],
   [/^avatar-outline(-(\S+))?$/, ([, , c = 'gray']) => `bg-transparent text-${c}-500 dark:text-${c}-400 ring-1 ring-current`],
