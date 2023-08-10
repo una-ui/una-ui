1871c0acf8b237969d99fd14581779baee21d151
phojie rengel <phojrengel@gmail.com>
2023-08-10 14:17:57 +0800

    docs: add avatar fallback example

--- docs/components/content/examples/vue/avatar/ExampleVueAvatarFallback.vue
+++ docs/components/content/examples/vue/avatar/ExampleVueAvatarFallback.vue
@@ -1,5 +1,22 @@
 <template>
-  <NAvatar
-    fallback="PR"
-  />
+  <div flex="~ items-center" gap-4>
+    <NAvatar
+      src="https://test.com/dummy.png"
+      fallback="/images/avatar.png"
+    />
+
+    <NAvatar
+      src="https://test.com/dummy.png"
+      label="DS"
+    />
+
+    <NAvatar
+      src="https://test.com/dummy.png"
+      alt="Dummy Src"
+    />
+
+    <NAvatar
+      src="https://test.com/dummy.png"
+    />
+  </div>
 </template>
