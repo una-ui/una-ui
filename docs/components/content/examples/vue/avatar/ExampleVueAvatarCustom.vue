1204aae39944858e8cf62618949e32e297c381d8
phojie rengel <phojrengel@gmail.com>
2023-08-10 14:52:36 +0800

    docs: add customization section and example

---
+++ docs/components/content/examples/vue/avatar/ExampleVueAvatarCustom.vue
@@ -0,0 +1,11 @@
+<template>
+  <div flex="~ items-center" gap-4>
+    <NAvatar size="lg" avatar="~" style="background-color:#303" class="text-white">
+      PR
+    </NAvatar>
+
+    <NAvatar avatar="solid-white" label="PR" size="lg" class="rounded-md text-$nv-c-brand-next ring-$nv-c-brand-light" />
+
+    <NAvatar size="2xl" class="rounded-none" src="/images/avatar.png" />
+  </div>
+</template>
