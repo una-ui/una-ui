96a57e4c11a3bfee30450591275c1c51334163b4
phojie rengel <phojrengel@gmail.com>
2023-08-12 07:48:32 +0800

    docs: add avatar-group group-size example

---
+++ docs/components/content/examples/vue/avatar-group/ExampleVueAvatarGroupSize.vue
@@ -0,0 +1,11 @@
+<template>
+  <NAvatarGroup
+    size="xl"
+    :max="3"
+  >
+    <NAvatar src="https://avatars.githubusercontent.com/u/11247099?v=4" alt="Anthony Fu" />
+    <NAvatar src="https://avatars.githubusercontent.com/u/28706372?v=4" alt="Daniel Roe" />
+    <NAvatar src="/images/avatar.png" alt="Phojie Rengel" />
+    <NAvatar label="SR" />
+  </NAvatarGroup>
+</template>
