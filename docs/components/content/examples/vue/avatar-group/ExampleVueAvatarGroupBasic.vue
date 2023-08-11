27abe7b1db24bcc4c834f2e4496044f78a9505be
phojie rengel <phojrengel@gmail.com>
2023-08-12 07:47:58 +0800

    docs: add avatar-group basic example

---
+++ docs/components/content/examples/vue/avatar-group/ExampleVueAvatarGroupBasic.vue
@@ -0,0 +1,9 @@
+<template>
+  <NAvatarGroup
+    :max="2"
+  >
+    <NAvatar src="https://avatars.githubusercontent.com/u/11247099?v=4" alt="Anthony Fu" />
+    <NAvatar src="https://avatars.githubusercontent.com/u/28706372?v=4" alt="Daniel Roe" />
+    <NAvatar src="/images/avatar.png" alt="Phojie Rengel" />
+  </NAvatarGroup>
+</template>
