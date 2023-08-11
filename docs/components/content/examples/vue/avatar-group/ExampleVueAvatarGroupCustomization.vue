b6c07064c6a295263f74089d5e6abf7dd60be683
phojie rengel <phojrengel@gmail.com>
2023-08-12 07:48:15 +0800

    docs: add avatar-group custom example

---
+++ docs/components/content/examples/vue/avatar-group/ExampleVueAvatarGroupCustomization.vue
@@ -0,0 +1,13 @@
+<template>
+  <NAvatarGroup
+    :max="2"
+    avatar="solid-info"
+    :nv="{
+      avatar: 'text-white',
+    }"
+  >
+    <NAvatar label="AF" />
+    <NAvatar label="DR" />
+    <NAvatar label="PR" />
+  </NAvatarGroup>
+</template>
