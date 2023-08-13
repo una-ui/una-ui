9210187b6b9c4bf72de7f52a7b3a87df674d08bb
phojie rengel <phojrengel@gmail.com>
2023-08-13 23:31:59 +0800

    docs: set avatar indicator section and indicator initial page

---
+++ docs/components/content/examples/vue/avatar/ExampleVueAvatarIndicator.vue
@@ -0,0 +1,19 @@
+<template>
+  <div flex="~ items-center" gap-4>
+    <NIndicator indicator="solid-green" dot>
+      <NAvatar src="/images/avatar.png" alt="Phojie Rengel" />
+    </NIndicator>
+
+    <NIndicator indicator="solid-gray bottom-right" dot>
+      <NAvatar src="/images/avatar.png" alt="Phojie Rengel" />
+    </NIndicator>
+
+    <NIndicator label="1" indicator="solid-red top-right">
+      <NAvatar icon="i-heroicons-phone-arrow-down-left-20-solid" />
+    </NIndicator>
+
+    <NIndicator indicator="solid-info" label="New">
+      <NAvatar src="/images/avatar.png" alt="Phojie Rengel" />
+    </NIndicator>
+  </div>
+</template>
