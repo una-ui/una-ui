a806e62cf5f56dc48ec454ee2c72a6dd086ec9f6
phojie rengel <phojrengel@gmail.com>
2023-08-14 06:47:01 +0800

    docs: add indicator visibility section and example

---
+++ docs/components/content/examples/vue/indicator/ExampleVueIndicatorVisibility.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const isVisible = ref(true)
+</script>
+
+<template>
+  <div flex="~ nowrap" gap-4>
+    <NIndicator indicator="solid-red" label="1" :visible="isVisible">
+      <NButton label="i-heroicons-bell group-active:i-heroicons-bell-solid" size="lg" icon btn="soft-gray square" class="group rounded-full" />
+    </NIndicator>
+
+    <NSwitch
+      v-model="isVisible"
+    />
+  </div>
+</template>
