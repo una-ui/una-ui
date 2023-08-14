99a86d1416be22693d9edca841306ef0baed8b6d
phojie rengel <phojrengel@gmail.com>
2023-08-15 03:11:00 +0800

    chore(button): update base sizes

--- packages/ui-preset/src/_shortcuts/button.ts
+++ packages/ui-preset/src/_shortcuts/button.ts
@@ -4,28 +4,29 @@ export const staticButton: Record<`${ButtonPrefix}-${string}` | ButtonPrefix, st
   // config
   'btn-default-variant': 'btn-solid',
   'btn-loading-icon': 'i-loading',
+  'btn-rectangle': 'px-0.8571428571428571em py-0.5714285714285714em',
+  'btn-icon-size': 'h-1.4285714285714286em w-1.4285714285714286em',
 
   // base
-  'btn': 'bg-transparent size-sm leading-5 gap-1 btn-rectangle rounded-md inline-flex justify-center items-center btn-disabled font-semibold cursor-pointer',
+  'btn': 'bg-transparent text-0.875em leading-5 gap-0.42857142857142855em btn-rectangle rounded-md inline-flex justify-center items-center btn-disabled font-semibold cursor-pointer',
   'btn-disabled': 'disabled:n-disabled',
   'btn-label': '',
-  'btn-icon-label': 'text-1.1em',
-  'btn-leading': '-ml-0.2em mr-0.2em text-1.1em',
-  'btn-trailing': '-mr-0.2em ml-0.2em text-1.1em',
-  'btn-loading': 'animate-spin text-1em',
+  'btn-icon-label': 'btn-icon-size',
+  'btn-leading': '-ml-0.14285714285714285em btn-icon-size',
+  'btn-trailing': '-mr-0.14285714285714285em btn-icon-size',
+  'btn-loading': 'animate-spin btn-icon-size',
 
   // options
   'btn-block': 'w-full',
   'btn-reverse': 'flex-row-reverse',
-  'btn-rectangle': 'px-0.75em py-0.5em',
-  'btn-square': 'p-0.5em',
+  'btn-square': 'p-0.5714285714285714em',
 
   // variants
   'btn-solid-white': 'bg-base text-base ring-1 ring-base ring-inset shadow-sm btn-focus hover:bg-muted',
   'btn-ghost-white': 'text-base btn-focus hover:bg-$c-gray-50',
   'btn-solid-gray': 'bg-$c-gray-50 text-$c-gray-800 ring-1 ring-base ring-inset shadow-sm btn-focus hover:bg-$c-gray-100',
   'btn-ghost-gray': 'text-$c-gray-500 btn-focus hover:text-$c-gray-950 hover:bg-$c-gray-50',
-  'btn-soft-gray': 'text-$c-gray-500 bg-$c-gray-50 ring-1 ring-inset  ring-$c-gray-50 btn-focus hover:bg-$c-gray-100',
+  'btn-soft-gray': 'text-$c-gray-500 bg-$c-gray-50 ring-1 ring-inset ring-$c-gray-50 btn-focus hover:bg-$c-gray-100',
   'btn-link-gray': 'text-$c-gray-500 btn-focus hover:text-$c-gray-950 hover:underline underline-offset-4',
   'btn-solid-black': 'bg-inverted text-inverted ring-1 ring-inset ring-$c-gray-950 shadow-sm btn-focus hover:bg-$c-gray-800',
   'btn-link-black': 'text-$c-gray-950 btn-focus hover:underline underline-offset-4',
