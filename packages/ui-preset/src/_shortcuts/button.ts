da9d3ac6c77b66e29ad3c4bc5ecea414bf0d72c2
phojie rengel <phojrengel@gmail.com>
2023-08-14 09:14:52 +0800

    chore(button): set ring-inset

--- packages/ui-preset/src/_shortcuts/button.ts
+++ packages/ui-preset/src/_shortcuts/button.ts
@@ -21,13 +21,13 @@ export const staticButton: Record<`${ButtonPrefix}-${string}` | ButtonPrefix, st
   'btn-square': 'p-0.5em',
 
   // variants
-  'btn-solid-white': 'bg-base text-base ring-1 ring-base shadow-sm btn-focus hover:bg-muted',
+  'btn-solid-white': 'bg-base text-base ring-1 ring-base ring-inset shadow-sm btn-focus hover:bg-muted',
   'btn-ghost-white': 'text-base btn-focus hover:bg-$c-gray-50',
-  'btn-solid-gray': 'bg-$c-gray-50 text-$c-gray-800 ring-1 ring-base shadow-sm btn-focus hover:bg-$c-gray-100',
+  'btn-solid-gray': 'bg-$c-gray-50 text-$c-gray-800 ring-1 ring-base ring-inset shadow-sm btn-focus hover:bg-$c-gray-100',
   'btn-ghost-gray': 'text-$c-gray-500 btn-focus hover:text-$c-gray-950 hover:bg-$c-gray-50',
-  'btn-soft-gray': 'text-$c-gray-500 bg-$c-gray-50 ring-1 ring-$c-gray-50 btn-focus hover:bg-$c-gray-100',
+  'btn-soft-gray': 'text-$c-gray-500 bg-$c-gray-50 ring-1 ring-inset  ring-$c-gray-50 btn-focus hover:bg-$c-gray-100',
   'btn-link-gray': 'text-$c-gray-500 btn-focus hover:text-$c-gray-950 hover:underline underline-offset-4',
-  'btn-solid-black': 'bg-inverted text-inverted ring-1 ring-$c-gray-950 shadow-sm btn-focus hover:bg-$c-gray-800',
+  'btn-solid-black': 'bg-inverted text-inverted ring-1 ring-inset ring-$c-gray-950 shadow-sm btn-focus hover:bg-$c-gray-800',
   'btn-link-black': 'text-$c-gray-950 btn-focus hover:underline underline-offset-4',
 }
 
@@ -38,7 +38,7 @@ export const dynamicButton: [RegExp, (params: RegExpExecArray) => string][] = [
   // variants
   [/^btn-text(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-${c}-600 dark:text-${c}-500 hover:text-${c}-500 dark:hover:text-${c}-400`],
   [/^btn-solid(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-inverted shadow-sm bg-${c}-600 hover:bg-${c}-500 dark:bg-${c}-500 dark:hover:bg-${c}-400`],
-  [/^btn-outline(-(\S+))?$/, ([, , c = 'primary']) => `bg-transparent btn-focus-${c} text-${c}-500 dark:text-${c}-400 ring-1 ring-current hover:bg-${c}-50 dark:hover:bg-${c}-950`],
+  [/^btn-outline(-(\S+))?$/, ([, , c = 'primary']) => `bg-transparent btn-focus-${c} text-${c}-500 dark:text-${c}-400 ring-1 ring-inset ring-current hover:bg-${c}-50 dark:hover:bg-${c}-950`],
   [/^btn-soft(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-${c}-500 dark:text-${c}-400 bg-${c}-50 dark:bg-${c}-950 hover:bg-${c}-100 dark:hover:bg-${c}-900`],
   [/^btn-ghost(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-${c}-500 dark:text-${c}-400 hover:bg-${c}-50 dark:hover:bg-${c}-900`],
   [/^btn-link(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-${c}-500 dark:text-${c}-400 hover:underline underline-offset-4`],
