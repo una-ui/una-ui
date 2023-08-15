c9b2f369e0be72e94f0bf44f89e79d79b9184776
phojie rengel <phojrengel@gmail.com>
2023-08-15 09:40:31 +0800

    docs: update indicator visible prop example

--- docs/components/content/examples/vue/indicator/ExampleVueIndicatorVisibility.vue
+++ docs/components/content/examples/vue/indicator/ExampleVueIndicatorVisibility.vue
@@ -4,7 +4,7 @@ const isVisible = ref(true)
 
 <template>
   <div flex="~ nowrap" gap-4>
-    <NIndicator indicator="solid-red" label="1" :visible="isVisible">
+    <NIndicator indicator="solid-red" label="1" size="lg" :visible="isVisible">
       <NButton label="i-heroicons-bell group-active:i-heroicons-bell-solid" size="lg" icon btn="soft-gray square" class="group rounded-full" />
     </NIndicator>
 
