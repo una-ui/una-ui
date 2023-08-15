7fff8e1924d5b3227c88b54e0ea5f985dc7338c5
phojie rengel <phojrengel@gmail.com>
2023-08-16 01:56:34 +0800

    chore: init

--- packages/ui-preset/src/index.ts
+++ packages/ui-preset/src/index.ts
@@ -18,7 +18,7 @@ export default function presetNexvelt(options: nexveltUIOptions = {
     shortcuts,
     theme: mergeDeep<Theme>(unoTheme, {
       colors: {
-        brand: 'rgba(var(--nv-brand) / <alpha-value>)',
+        brand: 'rgba(var(--c-brand),%alpha)',
         primary: {
           DEFAULT: 'rgba(var(--nv-primary) / <alpha-value>)',
           active: 'rgba(var(--nv-primary-active) / <alpha-value>)',
@@ -60,7 +60,7 @@ export default function presetNexvelt(options: nexveltUIOptions = {
         const color = parseColor(body, theme)
         if ((color?.cssColor?.type === 'rgb' || color?.cssColor?.type === 'rgba') && color.cssColor.components) {
           return {
-            '--nv-brand': `${color.cssColor.components.join(',')}`,
+            '--c-brand': `${color.cssColor.components.join(',')}`,
           }
         }
       }],
