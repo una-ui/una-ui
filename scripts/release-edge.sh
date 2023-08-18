eb33b5c00a3b00ef8cda5ea098c3ebdbd4f82120
phojie rengel <phojrengel@gmail.com>
2023-08-19 00:21:52 +0800

    ci: test

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -1,3 +1,27 @@
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
-echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
-echo "always-auth=true" >> ~/.npmrc
+#!/bin/bash
+
+set -xe
+
+# Restore all git changes
+git restore -s@ -SW  -- packages
+
+# Bump versions to edge
+pnpm jiti ./scripts/bump-edge
+
+# Update token
+if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
+# Release packages
+for p in packages/* ; do
+  pushd $p
+  echo "Publishing $p"
+  cp ../../LICENSE .
+  cp ../../README.md .
+  pnpm publish --access public --no-git-checks
+  popd
+done
