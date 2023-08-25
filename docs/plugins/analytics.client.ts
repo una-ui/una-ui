0e5266f69bb79b595daee1ee89210a9e8ca22904
phojie rengel <phojrengel@gmail.com>
2023-08-25 12:31:45 +0800

    chore(analytics): disable in dev environment [skip-release]

--- docs/plugins/analytics.client.ts
+++ docs/plugins/analytics.client.ts
@@ -1,5 +1,8 @@
 import { inject } from '@vercel/analytics'
 
 export default defineNuxtPlugin(() => {
+  if (process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'development')
+    return
+
   inject()
 })
