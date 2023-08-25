221df8f66a682ba0904f61f6fae4c0ffa8588e54
phojie rengel <phojrengel@gmail.com>
2023-08-25 11:38:58 +0800

    chore: update kbd type tsdocs [skip-release]

--- packages/nuxt/src/runtime/types/kbd.ts
+++ packages/nuxt/src/runtime/types/kbd.ts
@@ -1,6 +1,6 @@
 export interface NKbdProps {
   /**
-   * Allows you to add `UnaUI` button preset properties,
+   * Allows you to add `UnaUI` kbd preset properties,
    * Think of it as a shortcut for adding options or variants to the preset if available.
    *
    * @see https://github.com/una-ui/una-ui/blob/main/packages/preset/src/_shortcuts/kbd.ts
