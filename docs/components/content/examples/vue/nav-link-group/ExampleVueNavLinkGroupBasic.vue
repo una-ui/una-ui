9844b543724a31f191fbc77721a73bd04494e90f
phojie rengel <phojrengel@gmail.com>
2023-09-10 05:51:05 +0800

    docs: update nav-link-group content and add basic example

---
+++ docs/components/content/examples/vue/nav-link-group/ExampleVueNavLinkGroupBasic.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const links = [
+  {
+    leading: 'i-heroicons-user-group',
+    label: 'Teams',
+    defaultOpen: true,
+    children: [
+      {
+        label: 'Team Members 1',
+        to: '/navigation/nav-link',
+      },
+      {
+        label: 'Team Members 2',
+      },
+      {
+        label: 'Team Members 3',
+      },
+    ],
+  },
+  {
+    leading: 'i-heroicons-user',
+    to: '/navigation/nav-link',
+    label: 'Users',
+    badge: {
+      label: '20+',
+    },
+  },
+  {
+    leading: 'i-heroicons-cog',
+    to: '/navigation/',
+    label: 'Settings',
+  },
+  {
+    leading: 'i-heroicons-document-text',
+    to: '/navigation/',
+    label: 'Documentation',
+
+  },
+  {
+    leading: 'i-heroicons-question-mark-circle',
+    to: '/navigation/',
+    label: 'Help',
+  },
+]
+</script>
+
+<template>
+  <NNavLinkGroup :links="links" />
+</template>
