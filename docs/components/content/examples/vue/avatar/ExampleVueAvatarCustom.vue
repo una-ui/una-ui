473aa405afd24041f7c861c4c7fc066dfa9b811b
phojie rengel <phojrengel@gmail.com>
2023-09-21 20:23:29 +0800

    style: update oudated variables

--- docs/components/content/examples/vue/avatar/ExampleVueAvatarCustom.vue
+++ docs/components/content/examples/vue/avatar/ExampleVueAvatarCustom.vue
@@ -2,7 +2,7 @@
   <div flex="~ items-center" gap-4>
     <NAvatar label="PR" size="lg" avatar="~" style="background-color:#303" class="text-white" />
 
-    <NAvatar avatar="solid-white" label="PR" size="lg" class="rounded-md text-$una-c-brand-next ring-$una-c-brand-light" />
+    <NAvatar avatar="solid-white" label="PR" size="lg" class="rounded-md text-$c-brand-next ring-$c-brand-light" />
 
     <NAvatar size="2xl" class="rounded-none" src="/images/avatar.png" />
   </div>
