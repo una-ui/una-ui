c0249725c4839ddfb0b57f7b8338aceb739b949e
phojie rengel <phojrengel@gmail.com>
2023-09-28 14:14:52 +0800

    chore(deps): revert content version

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,11 +2,16 @@ import pkg from '../package.json'
 import unaUI from '../packages/nuxt/src/module'
 
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus',
+  extends: ['@nuxt-themes/docus'],
   modules: [
     'nuxt-content-snippet',
     unaUI,
   ],
+
+  alias: {
+    'micromark/lib/preprocess.js': 'micromark',
+    'micromark/lib/postprocess.js': 'micromark',
+  },
 
   runtimeConfig: {
     public: {
