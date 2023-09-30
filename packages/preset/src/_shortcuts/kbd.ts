e98e748dd4879918499c56f79adfb0ed1c5fb33f
phojie rengel <phojrengel@gmail.com>
2023-09-30 21:40:21 +0800

    chore(kbd-preset): remove static variant

--- packages/preset/src/_shortcuts/kbd.ts
+++ packages/preset/src/_shortcuts/kbd.ts
@@ -6,10 +6,6 @@ export const staticKbd: Record<`${KbdPrefix}-${string}` | KbdPrefix, string> = {
 
   // base
   'kbd': 'inline-flex text-nowrap items-center h-1.8181818181818181em min-w-1.8181818181818181em font-sans justify-center rounded px-0.5454545454545454em py-0.18181818181818182em text-0.6875em font-normal',
-
-  // variant
-  'kbd-solid-gray': 'kbd-ring-gray bg-gray-50 text-gray-600 dark:bg-gray-900 dark:text-gray-300',
-  'kbd-outline-gray': 'kbd-ring-gray bg-transparent text-gray-600 dark:text-gray-300',
 }
 
 export const dynamicKbd: [RegExp, (params: RegExpExecArray) => string][] = [
