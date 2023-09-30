223a9eefaaea0640102110a20bb636d91b264562
phojie rengel <phojrengel@gmail.com>
2023-09-30 21:35:15 +0800

    docs: update avatar skeleton mode section example [skip]

--- docs/components/content/examples/vue/avatar/ExampleVueAvatarSkeleton.vue
+++ docs/components/content/examples/vue/avatar/ExampleVueAvatarSkeleton.vue
@@ -1,24 +1,6 @@
-<script setup lang="ts">
-const delay = ref(0)
-
-/**
- * This if to prevent pre-rendering,
- * you can directly set the delay value to attribute.
- *
- * @example
- * :delay="1000 * 120"
- */
-onMounted(() => {
-  setTimeout(() => {
-    delay.value = 1000 * 120
-  }, 3000)
-})
-</script>
-
 <template>
   <NAvatar
     src="/images/avatar.png"
     skeleton
-    :delay="delay"
   />
 </template>
