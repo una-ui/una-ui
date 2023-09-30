8b9334eef54fc816f4ea83df28f400c288641bd5
phojie rengel <phojrengel@gmail.com>
2023-09-30 21:30:44 +0800

    fix: prevent avatar skeleton example from pre-rendering

--- docs/components/content/examples/vue/avatar/ExampleVueAvatarSkeleton.vue
+++ docs/components/content/examples/vue/avatar/ExampleVueAvatarSkeleton.vue
@@ -1,7 +1,24 @@
+<script setup lang="ts">
+const delay = ref(0)
+
+/**
+ * This if to prevent pre-rendering,
+ * you can directly set the delay value to attribute.
+ *
+ * @example
+ * :delay="1000 * 120"
+ */
+onMounted(() => {
+  setTimeout(() => {
+    delay.value = 1000 * 120
+  }, 3000)
+})
+</script>
+
 <template>
   <NAvatar
     src="/images/avatar.png"
     skeleton
-    :delay="1000 * 60 * 60"
+    :delay="delay"
   />
 </template>
