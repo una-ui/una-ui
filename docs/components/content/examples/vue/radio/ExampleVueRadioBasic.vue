c524961a6ee25830f6d5a5a4a1506622d11ab8f7
phojie rengel <phojrengel@gmail.com>
2023-10-12 20:45:00 +0800

    docs: init

---
+++ docs/components/content/examples/vue/radio/ExampleVueRadioBasic.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const radio = ref('a')
+</script>
+
+<template>
+  <div class="flex gap-4">
+    <NRadio
+      v-model="radio"
+      value="a"
+    />
+
+    <NRadio
+      v-model="radio"
+      value="c"
+    />
+  </div>
+</template>
