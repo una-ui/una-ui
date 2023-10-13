a8e352c43b28097826dfb31885724e0ac02543dd
phojie <phojrengel@gmail.com>
2023-10-14 00:39:21 +0800

    docs(NRadio): add example

---
+++ docs/components/content/examples/vue/radio/ExampleVueRadioColor.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const radio = ref('')
+
+const colors = [
+  {
+    radio: 'primary',
+  },
+  {
+    radio: 'info',
+  },
+  {
+    radio: 'error',
+  },
+  {
+    radio: 'warning',
+  },
+  {
+    radio: 'success',
+  },
+  {
+    radio: 'purple',
+  },
+  {
+    radio: 'pink',
+  },
+  {
+    radio: 'violet',
+  },
+  {
+    radio: 'fuchsia',
+  },
+  {
+    radio: 'indigo',
+  },
+]
+</script>
+
+<template>
+  <div class="flex flex-wrap gap-4">
+    <NRadio
+      v-for="color in colors"
+      :key="color.radio"
+      v-model="radio"
+      :label="color.radio"
+      :radio="color.radio"
+      :value="color.radio"
+    />
+  </div>
+</template>
