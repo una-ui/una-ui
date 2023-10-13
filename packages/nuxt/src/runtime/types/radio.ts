5a9553bbd2359d9e07157fb1c57fb913bb88148a
phojie <phojrengel@gmail.com>
2023-10-14 01:51:22 +0800

    docs: rename radio label section to radio-group

--- packages/nuxt/src/runtime/types/radio.ts
+++ packages/nuxt/src/runtime/types/radio.ts
@@ -1,2 +1,16 @@
 export interface NRadioProps {
+  modelValue?: string
+  disabled?: boolean
+  name?: string
+  id?: string
+  value?: string
+  radio?: string
+  label?: string
+  size?: string
+  una?: {
+    radioDefaultColor?: string
+    radioLabel?: string
+    radioPeerFocus?: string
+    radioInner?: string
+  }
 }
