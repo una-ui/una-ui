60af79a50fe3188424df1ee26cdae064460371da
phojie <phojrengel@gmail.com>
2023-10-14 17:17:35 +0800

    docs(NRadio): add reverse and size section and example

---
+++ docs/components/content/examples/vue/radio/ExampleVueRadioSize.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const radio = ref('a')
+</script>
+
+<template>
+  <div class="flex gap-4">
+    <NRadio
+      v-model="radio"
+      value="a"
+      size="1cm"
+      label="1cm"
+    />
+
+    <NRadio
+      v-model="radio"
+      value="b"
+      size="sm:2xl"
+      label="sm:2xl"
+    />
+
+    <NRadio
+      v-model="radio"
+      value="c"
+      size="10px"
+      label="10px"
+    />
+  </div>
+</template>
