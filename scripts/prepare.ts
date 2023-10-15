99f8a25e204d854bb04e2f0d507453691e115b39
phojie <phojrengel@gmail.com>
2023-10-15 08:28:51 +0800

    chore(preset): add global preset to prefixes

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -53,5 +53,7 @@ const prefixFiles = await fg('packages/preset/src/_shortcuts/*.ts', {
 })
 
 const prefixes = prefixFiles.map(i => basename(i, extname(i))).filter(i => i !== 'index')
+const global = ['size']
+prefixes.push(...global)
 const formattedPrefixes = `export default [${prefixes.map(p => `'${p}'`).join(', ')}]\n`
 await fs.writeFile('./packages/preset/src/prefixes.ts', formattedPrefixes, { encoding: 'utf-8' })
