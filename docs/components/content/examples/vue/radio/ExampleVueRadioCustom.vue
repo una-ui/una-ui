f8fff382f71e4f5fd8a0821ce14e2aa7cb5444f1
phojie <phojrengel@gmail.com>
2023-10-15 22:33:24 +0800

    docs(n-radio): update section and add examples

---
+++ docs/components/content/examples/vue/radio/ExampleVueRadioCustom.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+const radio = ref()
+
+const options = [
+  {
+    value: 'vue',
+    class: 'n-checked:bg-green-500/30 n-checked:border-green-500',
+    radio: 'green',
+    una: {
+      radioIcon: 'i-logos-vue n-checked:scale-300 transition-transform duration-1000',
+    },
+  },
+  {
+    value: 'react',
+    class: 'n-checked:bg-blue-500/30 n-checked:border-blue-500',
+    radio: 'blue',
+    una: {
+      radioIcon: 'i-logos-react  n-checked:rotate-360 transition-transform duration-1000',
+    },
+  },
+  {
+    value: 'angular',
+    class: 'n-checked:bg-red-500/30 n-checked:border-red-500',
+    radio: 'red',
+    una: {
+      radioIcon: 'n-checked:i-logos-angular-icon n-checked:scale-200 transition-transform duration-1000',
+    },
+  },
+]
+</script>
+
+<template>
+  <div class="flex flex-col space-y-6">
+    <span>
+      Value: {{ radio }}
+    </span>
+
+    <div class="flex flex-wrap gap-4">
+      <NRadio
+        v-for="option in options"
+        :key="option.value"
+        v-model="radio"
+        :value="option.value"
+        :radio="option.radio"
+        :label="option.value"
+        :class="option.class"
+        size="15"
+        class="rounded-md"
+        :una="{
+          radioLabel: 'capitalize',
+          ...option.una,
+        }"
+      />
+    </div>
+  </div>
+</template>
---
+++ docs/components/content/examples/vue/radio/ExampleVueRadioDisabled.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const radio = ref()
+</script>
+
+<template>
+  <div class="flex gap-4">
+    <NRadio
+      v-model="radio"
+      value="a"
+      label="Disabled"
+      disabled
+    />
+
+    <NRadio
+      v-model="radio"
+      value="b"
+      label="Not disabled"
+    />
+  </div>
+</template>
