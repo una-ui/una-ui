af708d73319c5df0546a99fe3f5d54e898000867
Phojie Rengel <phojrengel@gmail.com>
2023-10-22 15:51:13 +0800

    docs(NCheckbox): add  section and example

---
+++ docs/components/content/examples/vue/checkbox/ExampleVueCheckboxDisabled.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const checkbox = ref()
+</script>
+
+<template>
+  <div class="flex gap-4">
+    <NCheckbox v-model="checkbox" label="Disabled" disabled />
+
+    <NCheckbox v-model="checkbox" label="Not disabled" />
+  </div>
+</template>
