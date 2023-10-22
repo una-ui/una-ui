4e48280c263a7a51a492139a78568e00c5f3a02a
Phojie Rengel <phojrengel@gmail.com>
2023-10-22 17:31:13 +0800

    docs(NCheckbox): add `customization` section and example

---
+++ docs/components/content/examples/vue/checkbox/ExampleVueCheckboxCustomIcon1.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const options = ref([
+  {
+    value: false,
+    label: '1',
+  },
+  {
+    value: false,
+    label: '2',
+  },
+  {
+    value: false,
+    label: '3',
+  },
+])
+</script>
+
+<template>
+  <div class="flex flex-wrap gap-4">
+    <NCheckbox
+      v-for="(option, i) in options"
+      :key="i"
+      v-model="option.value"
+      :label="option.label"
+      size="xl"
+      :una="{
+        checkboxIcon: 'i-tabler-checks',
+      }"
+    />
+  </div>
+</template>
---
+++ docs/components/content/examples/vue/checkbox/ExampleVueCheckboxCustomIcon2.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const options = ref([
+  {
+    value: false,
+    label: '1',
+    checkbox: 'green',
+    una: {
+      checkboxIcon: 'i-tabler-circle-1-filled',
+    },
+  },
+  {
+    value: false,
+    label: '2',
+    checkbox: 'react',
+    una: {
+      checkboxIcon: 'i-tabler-circle-2-filled',
+    },
+  },
+  {
+    value: false,
+    label: '3',
+    checkbox: 'angular',
+    una: {
+      checkboxIcon: 'i-tabler-circle-3-filled',
+    },
+  },
+])
+</script>
+
+<template>
+  <div class="flex flex-wrap gap-4">
+    <NCheckbox
+      v-for="(option, i) in options"
+      :key="i"
+      v-model="option.value"
+      :checkbox="option.checkbox"
+      :label="option.label"
+      size="3xl"
+      class="rounded-full"
+      :una="option.una"
+    />
+  </div>
+</template>
