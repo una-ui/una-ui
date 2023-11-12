02752a8c84a4b060566b775f075875c2546aefd7
Phojie Rengel <phojrengel@gmail.com>
2023-11-13 04:04:59 +0800

    docs(NTextarea): add cols and rows example

---
+++ docs/components/content/examples/vue/textarea/ExampleVueTextareaRowsCols.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const value = ref('')
+</script>
+
+<template>
+  <div class="max-w-lg">
+    <NInput
+      v-model="value"
+      type="textarea"
+      placeholder="Write your message here..."
+      :rows="10"
+      :cols="20"
+    />
+  </div>
+</template>
