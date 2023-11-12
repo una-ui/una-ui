6f91f6c8d6e54e3728920cb8912c1205c897fe9e
Phojie Rengel <phojrengel@gmail.com>
2023-11-12 10:00:31 +0800

    docs(NTextarea): add examples

---
+++ docs/components/content/examples/vue/textarea/ExampleVueTextareaBasic.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref('')
+</script>
+
+<template>
+  <div class="max-w-lg">
+    <NInput
+      v-model="value"
+      type="textarea"
+      placeholder="Write your message here..."
+    />
+  </div>
+</template>
