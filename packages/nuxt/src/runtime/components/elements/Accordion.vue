d9dd4c3147a1d8de620d9910a21c3c8ed40a976a
dimunyo <55717823+dimunyo@users.noreply.github.com>
2024-04-24 20:44:41 +0800

    feat: update accordion items to accept any

--- packages/nuxt/src/runtime/components/elements/Accordion.vue
+++ packages/nuxt/src/runtime/components/elements/Accordion.vue
@@ -53,7 +53,7 @@ function onLeave(element: Element, done: () => void) {
 }
 
 const [DefineTemplate, ReuseTemplate] = createReusableTemplate<{
-  item: NAccordionItemProps
+  item: NAccordionItemProps & any
   index: number
   open: boolean
   close: () => void
