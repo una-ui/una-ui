522babe9d78d43cbaff69a6af0da4fbc2ffb1324
Mekan Flores <mekan.flores@go.team>
2024-07-26 10:49:56 +0800

    feat(select): add boilerplate

---
+++ packages/nuxt/playground/components/Select.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+/**
+ * @name NSelect
+ *
+ * @todo
+ * - [] Add select props
+ * - [] Add select functionality
+ * - [] Add select presets
+ * - [] Add dynamic colors
+ * - [] Add dynamic sizes
+ * - [] NFormGroup integration
+ * - [] Add disabled state
+ * - [] Add select slots
+ * - [] Add select documentation
+ * - [] Add select markdown
+ * - [] Add select examples
+ */
+</script>
+
+<template>
+  <NSelect />
+</template>
---
+++ packages/nuxt/src/runtime/components/elements/Select.vue
@@ -0,0 +1,3 @@
+<template>
+  This is Select
+</template>
