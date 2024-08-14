2c29ad90df149ee553048351a0ee54e929d1e663
Phojie Rengel <phojrengel@gmail.com>
2024-08-14 18:29:25 +0800

    fix: add module config

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -128,6 +128,13 @@ export default defineNuxtModule<ModuleOptions>({
       watch: nuxt.options.dev,
     })
 
+    addComponentsDir({
+      path: resolve(runtimeDir, 'components/data', 'table'),
+      prefix: options.prefix,
+      global: options.global,
+      watch: nuxt.options.dev,
+    })
+
     // plugins
     if (options.themeable) {
       addPlugin(resolve(runtimeDir, 'plugins', 'theme.client'))
