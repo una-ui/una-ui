1cab77f9ffc8d678ea43ddba453bc17241a59353
mekan <mekan.flores@go.team>
2024-08-26 08:28:54 +0800

    feat(pagination): added scaffold types

---
+++ packages/nuxt/src/runtime/types/pagination.ts
@@ -0,0 +1,64 @@
+import type { HTMLAttributes } from 'vue'
+import type { PaginationEllipsisProps, PaginationFirstProps, PaginationLastProps, PaginationListItemProps, PaginationListProps, PaginationNextProps, PaginationPrevProps, PaginationRootProps } from 'radix-vue'
+
+interface BaseExtensions {
+  class?: HTMLAttributes['class']
+  size?: HTMLAttributes['class']
+}
+
+type RootExtensions = Omit<PaginationRootProps, 'modelValue'> & BaseExtensions
+type ListExtensions = Omit<PaginationListProps, 'value'> & BaseExtensions
+type ListItemExtensions = PaginationListItemProps & BaseExtensions
+type EllipsisExtensions = PaginationEllipsisProps & BaseExtensions
+type FirstExtensions = PaginationFirstProps & BaseExtensions
+type PreviousExtensions = PaginationPrevProps & BaseExtensions
+type NextExtensions = PaginationNextProps & BaseExtensions
+type LastExtensions = PaginationLastProps & BaseExtensions
+
+export interface NPaginationRootProps extends RootExtensions {
+  una?: {
+    paginationRoot?: HTMLAttributes['class']
+  }
+}
+
+export interface NPaginationListProps extends ListExtensions {
+  una?: {
+    paginationList?: HTMLAttributes['class']
+  }
+}
+
+export interface NPaginationListItemProps extends ListItemExtensions {
+  una?: {
+    paginationItemList?: HTMLAttributes['class']
+  }
+}
+
+export interface NPaginationEllipsisProps extends EllipsisExtensions {
+  una?: {
+    paginationEllipsis?: HTMLAttributes['class']
+  }
+}
+
+export interface NPaginationFirstProps extends FirstExtensions {
+  una?: {
+    paginationFirst?: HTMLAttributes['class']
+  }
+}
+
+export interface NPaginationPrevProps extends PreviousExtensions {
+  una?: {
+    paginationPrev?: HTMLAttributes['class']
+  }
+}
+
+export interface NPaginationNextProps extends NextExtensions {
+  una?: {
+    paginationNext?: HTMLAttributes['class']
+  }
+}
+
+export interface NPaginationLastProps extends LastExtensions {
+  una?: {
+    paginationLast?: HTMLAttributes['class']
+  }
+}
