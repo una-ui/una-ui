2acb024c016ceb301d55f1cc4c89ac470cc544f9
Mekan Flores <mekan.flores@go.team>
2024-08-29 14:07:52 +0800

    feat: wip pagination components

--- packages/nuxt/playground/app.vue
+++ packages/nuxt/playground/app.vue
@@ -3,7 +3,7 @@
     <div class="h-full w-full flex flex-col items-center justify-center gap-4">
       <ColorMode />
 
-      <Breadcrumb />
+      <Pagination />
 
       <NThemeSwitcher />
     </div>
