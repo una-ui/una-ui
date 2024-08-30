5be85f81407d539f24abdf2c75de8d66ab4d29fe
mekan <mekan.flores@go.team>
2024-08-31 01:12:59 +0800

    feat(pagination): add pagination last button

---
+++ packages/nuxt/src/runtime/components/data/pagination/PaginationLast.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { PaginationLast, useForwardProps } from 'radix-vue'
+import type { NPaginationLastProps } from '../../../types'
+import Button from '../../elements/Button.vue'
+import { cn } from '../../../utils'
+import Icon from '../../elements/Icon.vue'
+
+const props = withDefaults(defineProps<NPaginationLastProps>(), {
+})
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <PaginationLast v-bind="forwardedProps" as-child>
+    <Button :class="cn('pagination-last-button', props.class)">
+      <slot>
+        <Icon
+          :name="forwardedProps?.una?.paginationLastIcon || 'pagination-last-button-icon'"
+        />
+      </slot>
+    </Button>
+  </PaginationLast>
+</template>
