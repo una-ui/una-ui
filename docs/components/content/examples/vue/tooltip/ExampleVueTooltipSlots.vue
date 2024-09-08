3d35f98d598dbd7ae70f9e0a1dd65c3be7912dcd
mnenie <1alexpeshkov@gmail.com>
2024-09-08 19:49:03 +0300

    docs: fix styles in slots

--- docs/components/content/examples/vue/tooltip/ExampleVueTooltipSlots.vue
+++ docs/components/content/examples/vue/tooltip/ExampleVueTooltipSlots.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="grid h-28 place-items-center">
-    <NTooltip
-      :_tooltip-content="{
-        side: 'right',
-      }"
-    >
-      <template #trigger>
-        <NTooltipTrigger
-          class="rounded-full p-1"
-        >
-          <NAvatar
-            src="https://avatars.githubusercontent.com/u/499550?v=4"
-          />
-        </NTooltipTrigger>
-      </template>
-      <template #default>
-        Evan You
-      </template>
-    </NTooltip>
-  </div>
+  <NTooltip
+    :_tooltip-content="{
+      side: 'right',
+    }"
+  >
+    <template #trigger>
+      <NTooltipTrigger
+        class="rounded-full p-1"
+      >
+        <NAvatar
+          src="https://avatars.githubusercontent.com/u/499550?v=4"
+        />
+      </NTooltipTrigger>
+    </template>
+    <template #default>
+      Evan You
+    </template>
+  </NTooltip>
 </template>
