373a899c7ed8a2f73699b5a3e87a4018aff7ffa9
Phojie Rengel <phojrengel@gmail.com>
2024-09-08 10:30:52 +0800

    feat(Label): introduce new component and preset (#178)

---
+++ docs/components/content/examples/vue/label/ExampleVueLabelBasic.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="grid gap-2">
+    <div class="grid grid-cols-3 items-center gap-4">
+      <NLabel for="width">
+        Width
+      </NLabel>
+      <NInput
+        id="width"
+        type="text"
+        model-value="100%"
+        class="col-span-2 h-8"
+      />
+    </div>
+    <div class="grid grid-cols-3 items-center gap-4">
+      <NLabel for="maxWidth">
+        Max. width
+      </NLabel>
+      <NInput
+        id="maxWidth"
+        type="text"
+        model-value="300px"
+        class="col-span-2 h-8"
+      />
+    </div>
+    <div class="grid grid-cols-3 items-center gap-4">
+      <NLabel for="height">
+        Height
+      </NLabel>
+      <NInput
+        id="height"
+        type="text"
+        model-value="25px"
+        class="col-span-2 h-8"
+      />
+    </div>
+    <div class="grid grid-cols-3 items-center gap-4">
+      <NLabel for="maxHeight">
+        Max. height
+      </NLabel>
+      <NInput
+        id="maxHeight"
+        type="text"
+        model-value="none"
+        class="col-span-2 h-8"
+      />
+    </div>
+  </div>
+</template>
