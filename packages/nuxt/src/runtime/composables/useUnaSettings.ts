95151304673575db739cde615948214575e4d638
Phojie Rengel <phojrengel@gmail.com>
2024-09-10 22:36:10 +0800

    fix(ThemeSwitcher): add default value to font-size and radius

--- packages/nuxt/src/runtime/composables/useUnaSettings.ts
+++ packages/nuxt/src/runtime/composables/useUnaSettings.ts
@@ -22,6 +22,14 @@ export function useUnaSettings() {
   const settings = useStorage('una-settings', defaultSettings)
 
   watchEffect(() => {
+    if (!settings.value.radius)
+      settings.value.radius = defaultSettings.radius
+
+    if (!settings.value.fontSize)
+      settings.value.fontSize = defaultSettings.fontSize
+  })
+
+  watchEffect(() => {
     settings.value.primaryColors = getPrimaryColors(settings.value.primary)
     settings.value.grayColors = getGrayColors(settings.value.gray)
   })
