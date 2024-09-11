6f74528e8116ceaec0a7796db1cb48e015888a40
Phojie Rengel <phojrengel@gmail.com>
2024-09-11 22:24:54 +0800

    fix(Button): add bg-transparent to ghost preset

--- packages/preset/src/_shortcuts/btn.ts
+++ packages/preset/src/_shortcuts/btn.ts
@@ -22,11 +22,11 @@ export const staticBtn: Record<`${BtnPrefix}-${string}` | BtnPrefix, string> = {
 
   // variants
   'btn-solid-white': 'bg-base text-base ring-1 ring-base ring-inset shadow-sm btn-focus hover:bg-muted',
-  'btn-ghost-white': 'text-base btn-focus hover:bg-$c-gray-50',
+  'btn-ghost-white': 'bg-transparent text-base btn-focus hover:bg-$c-gray-50',
   'btn-outline-white': 'bg-transparent text-base ring-1 ring-base ring-inset btn-focus',
 
   'btn-solid-gray': 'bg-$c-gray-50 text-$c-gray-800 ring-1 ring-base ring-inset shadow-sm btn-focus hover:bg-$c-gray-100',
-  'btn-ghost-gray': 'text-$c-gray-600 btn-focus hover:bg-$c-gray-100',
+  'btn-ghost-gray': 'bg-transaprent text-$c-gray-600 btn-focus hover:bg-$c-gray-100',
   'btn-soft-gray': 'text-$c-gray-600 bg-$c-gray-50 btn-focus hover:bg-$c-gray-100',
   'btn-outline-gray': 'bg-transparent text-muted ring-1 ring-base ring-inset btn-focus',
   'btn-link-gray': 'text-muted btn-focus hover:text-base hover:underline underline-offset-4',
@@ -49,7 +49,7 @@ export const dynamicBtn: [RegExp, (params: RegExpExecArray) => string][] = [
   [/^btn-text(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-${c}-600 dark:text-${c}-500 hover:text-${c}-500 dark:hover:text-${c}-400`],
   [/^btn-outline(-(\S+))?$/, ([, , c = 'primary']) => `bg-transparent btn-focus-${c} text-${c}-500 dark:text-${c}-400 ring-1 ring-inset ring-${c}-500 dark:ring-${c}-400 hover:bg-${c}-50 dark:hover:bg-${c}-950`],
   [/^btn-soft(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-${c}-600 dark:text-${c}-400 bg-${c}-50 dark:bg-${c}-950 hover:bg-${c}-100 dark:hover:bg-${c}-900`],
-  [/^btn-ghost(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-${c}-600 dark:text-${c}-400 hover:bg-${c}-100 dark:hover:bg-${c}-900`],
+  [/^btn-ghost(-(\S+))?$/, ([, , c = 'primary']) => `bg-transparent btn-focus-${c} text-${c}-600 dark:text-${c}-400 hover:bg-${c}-100 dark:hover:bg-${c}-900`],
   [/^btn-link(-(\S+))?$/, ([, , c = 'primary']) => `btn-focus-${c} text-${c}-500 dark:text-${c}-400 hover:underline underline-offset-4`],
 ]
 
