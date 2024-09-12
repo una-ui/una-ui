50f2ad81789ff7f137f1583fae5b344e37452f88
Matthew Messinger <mattmess1221@gmail.com>
2024-09-12 16:52:51 -0400

    refactor(popover): use `content` slot instead of `popover-content` (#190)
    * Refactor Popover with a simpler interface
    
    * Update names of content prop and slot
    
    ---------
    
    Co-authored-by: Phojie Rengel <phojrengel@gmail.com>

--- packages/nuxt/src/runtime/types/popover.ts
+++ packages/nuxt/src/runtime/types/popover.ts
@@ -4,6 +4,8 @@ import type { HTMLAttributes } from 'vue'
 interface BaseExtensions { class?: HTMLAttributes['class'] }
 
 export interface NPopoverProps extends PopoverRootProps {
+  /** Props for the popover content */
+  _popoverContent?: NPopoverContentProps
 }
 
 export interface NPopoverContentProps extends PopoverContentProps, BaseExtensions {
