ccc4eac933b126ffd2e7ef05743d185750835e7e
mnenie <1alexpeshkov@gmail.com>
2024-09-17 22:39:44 +0300

    feat: add toast to module

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -113,6 +113,14 @@ export default defineNuxtModule<ModuleOptions>({
     })
 
     addComponentsDir({
+      path: resolve(runtimeDir, 'components/elements', 'toast'),
+      prefix: options.prefix,
+      global: options.global,
+      watch: nuxt.options.dev,
+      priority: 10,
+    })
+
+    addComponentsDir({
       path: resolve(runtimeDir, 'components/elements', 'tooltip'),
       prefix: options.prefix,
       global: options.global,
