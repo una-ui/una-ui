b0bff2096e8a5a5b81c10c54722c8703d1716e70
Phojie Rengel <phojrengel@gmail.com>
2024-09-24 12:41:49 +0800

    docs: update landing page buttons style

--- docs/components/content/docs/DocsHero.vue
+++ docs/components/content/docs/DocsHero.vue
@@ -40,18 +40,21 @@ const { copy, copied } = useClipboard({ source })
         <NButton
           to="/getting-started"
           btn="solid"
-          class="h-2.9em rounded-full px-5.5 py-3 font-bold"
+          class="p-(3 6) font-bold"
+          rounded="full"
           label="Getting Started"
         />
         <NButton
           to="/components/accordion"
           btn="solid-gray"
-          class="h-2.9em rounded-full px-5.5 py-3 font-bold"
+          class="p-(3 6) font-bold"
+          rounded="full"
           label="View Components"
         />
         <NButton
           btn="solid-gray"
-          class="h-2.9em rounded-full px-5.5 py-3 font-bold"
+          class="p-(3 6) font-bold"
+          rounded="full"
           :class="{ 'text-primary-active': copied }"
           :label="source"
           :trailing="!copied ? 'i-heroicons-clipboard-document' : 'i-heroicons-check'"
