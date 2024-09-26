59488645854113e810d7ed43bdfc1cf3cde20fd0
dimunyo <gitmekan13@gmail.com>
2024-09-26 12:32:34 +0800

    feat: add toggle label icon inside slot

--- packages/nuxt/src/runtime/components/elements/Toggle.vue
+++ packages/nuxt/src/runtime/components/elements/Toggle.vue
@@ -3,17 +3,19 @@ import type { NToggleProps } from '../../types'
 import { Toggle, type ToggleEmits, useForwardPropsEmits } from 'radix-vue'
 import { computed } from 'vue'
 import { cn } from '../../utils'
+import Icon from './Icon.vue'
 
 const props = withDefaults(defineProps<NToggleProps>(), {
-  variant: 'default',
-  size: 'default',
+  toggle: 'accent',
+  size: 'toggle-size',
+  rounded: 'toggle-corner',
   disabled: false,
 })
 
 const emits = defineEmits<ToggleEmits>()
 
 const delegatedProps = computed(() => {
-  const { class: _, size, variant, ...delegated } = props
+  const { class: _, size, ...delegated } = props
 
   return delegated
 })
@@ -24,8 +26,12 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
 <template>
   <Toggle
     v-bind="forwarded"
-    :class="cn('inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-base transition-colors hover:bg-muted hover:text-muted focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-base focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent', 'h-10 px-3', 'border border-base bg-transparent shadow-sm hover:bg-accent hover:text-accent', props.class)"
+    :class="cn('toggle', props.toggle, props.size, props.rounded, props.class)"
   >
-    <slot />
+    <slot>
+      <span v-if="forwarded.label">{{ forwarded.label }}</span>
+
+      <Icon v-else-if="forwarded.icon" :name="forwarded.icon" />
+    </slot>
   </Toggle>
 </template>
