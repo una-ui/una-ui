b6f870a633ee24b822bebcbc2d6d209b32772a12
Phojie Rengel <phojrengel@gmail.com>
2024-09-26 17:00:08 +0800

    docs: update unclosable example

--- docs/components/content/examples/vue/dialog/ExampleVueDialogUncloseable.vue
+++ docs/components/content/examples/vue/dialog/ExampleVueDialogUncloseable.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-const open = ref(false)
 </script>
 
 <template>
-  <div>
-    <NButton label="Open Dialog" @click="open = true" />
-  </div>
   <NDialog
     title="Uncloseable Dialog"
-    :open
-    :una="{ dialogClose: '!hidden' }"
+    :_dialog-content="{
+      trapFocus: true,
+    }"
   >
+    <template #trigger>
+      <NButton>
+        Open Dialog
+      </NButton>
+    </template>
+
     <div>
       This dialog will stay open and not close until the close button is pressed.
     </div>
-    <template #footer>
-      <NButton label="Close" @click="open = false" />
-    </template>
   </NDialog>
 </template>
