cecedd27e644ab6b636e11c7e3e78c9138101488
dimunyo <gitmekan13@gmail.com>
2024-09-26 15:14:32 +0800

    fix: remove redundant class

--- packages/preset/src/_shortcuts/toggle.ts
+++ packages/preset/src/_shortcuts/toggle.ts
@@ -4,7 +4,6 @@ export const staticToggle: Record<`${TogglePrefix}-${string}` | TogglePrefix, st
   // configurations
   'toggle': 'inline-flex items-center justify-center border border-base bg-transparent data-[state=on]:bg-accent shadow-sm data-[state=on]:text-accent hover:bg-muted hover:text-muted text-sm font-medium focus-visible:ring-base ring-offset-base transition-colors disabled:pointer-events-none disabled:opacity-50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2',
   'toggle-size': 'h-10 px-3',
-  'toggle-corner': 'rounded-xl',
 }
 
 export const dynamicToggle = [
