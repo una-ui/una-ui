5ebc070aa9d2771f4226e198ca8e73e92161f3d1
Phojie Rengel <phojrengel@gmail.com>
2024-09-30 07:36:41 +0800

    chore: update preset color

--- packages/preset/src/_shortcuts/switch.ts
+++ packages/preset/src/_shortcuts/switch.ts
@@ -1,3 +1,6 @@
+import type { RuleContext } from 'unocss'
+import { parseColor, type Theme } from '@unocss/preset-mini'
+
 type SwitchPrefix = 'switch'
 
 export const staticSwitch: Record<`${SwitchPrefix}-${string}` | SwitchPrefix, string> = {
@@ -7,7 +10,7 @@ export const staticSwitch: Record<`${SwitchPrefix}-${string}` | SwitchPrefix, st
   'switch-focus': 'focus-visible:ring-brand focus-visible:ring-offset-base transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2',
 
   // thumb
-  'switch-thumb': 'h-1em w-1em flex items-center justify-center absolute pointer-events-none block border border-base rounded-full bg-base shadow-lg ring-0 transition-transform',
+  'switch-thumb': 'square-1em flex items-center justify-center absolute pointer-events-none block border border-base rounded-full bg-base shadow-lg ring-0 transition-transform',
   'switch-thumb-checked': 'translate-x-1.01em',
   'switch-thumb-unchecked': 'translate-x-0.03em',
 
@@ -24,13 +27,20 @@ export const staticSwitch: Record<`${SwitchPrefix}-${string}` | SwitchPrefix, st
 export const dynamicSwitch = [
   [
     /^switch-checked(?:-([^-]+))?(?:-([^-]+))?$/,
-    ([, color = 'primary']) =>
-      `data-[state=checked]:n-${color}-600 dark:data-[state=checked]:n-${color}-500`,
+    ([, color = 'primary'], { theme }: RuleContext<Theme>) => {
+      const parsedColor = parseColor(color, theme)
+      if ((parsedColor?.cssColor?.type === 'rgb' || parsedColor?.cssColor?.type === 'rgba') && parsedColor.cssColor.components)
+        return `data-[state=checked]:n-${color}-600 dark:data-[state=checked]:n-${color}-500`
+    },
   ],
+
   [
     /^switch-unchecked(?:-([^-]+))?(?:-([^-]+))?$/,
-    ([, color = 'gray']) =>
-      `data-[state=unchecked]:n-${color}-200 dark:data-[state=unchecked]:n-${color}-700/58`,
+    ([, color = 'primary'], { theme }: RuleContext<Theme>) => {
+      const parsedColor = parseColor(color, theme)
+      if ((parsedColor?.cssColor?.type === 'rgb' || parsedColor?.cssColor?.type === 'rgba') && parsedColor.cssColor.components)
+        return `data-[state=unchecked]:n-${color}-200 dark:data-[state=unchecked]:n-${color}-700/58`
+    },
   ],
 ]
 
