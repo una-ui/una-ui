3899632503a1c197f61afc256998e7079c8ddeca
Phojie Rengel <phojrengel@gmail.com>
2024-09-30 08:28:56 +0800

    chore: init

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,12 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
+export NODE_OPTIONS=--max_old_space_size=16384
+
 # dont deploy if the branch starts with renovate/
 if [[ "$VERCEL_GIT_COMMIT_REF" == renovate/* ]]; then
+ 
+  
   # Don't build
   echo "🛑 - Build cancelled"
   exit 0;
