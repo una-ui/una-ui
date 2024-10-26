0bd86115914cc16fbdfec06c4e98cd46312c1a60
Phojie Rengel <phojrengel@gmail.com>
2024-10-26 08:55:05 +0800

    docs: improve hero section

--- docs/components/content/docs/DocsHero.vue
+++ docs/components/content/docs/DocsHero.vue
@@ -10,13 +10,13 @@ const { copy, copied } = useClipboard({ source })
 <template>
   <div grid="~ cols-1 lg:cols-5" class="my-15 gap-x-30 gap-y-5 lg:my-30 md:my-20 sm:my-15">
     <!-- left section -->
-    <div col="lg:span-3 span-1" class="flex flex-col items-center lg:items-start space-y-10 lg:px-0 sm:px-25">
+    <div col="lg:span-3 span-1" class="flex flex-col items-center gap-y-6 lg:items-start lg:px-0 sm:px-25">
       <NButton
         to="https://github.com/una-ui/una-ui/releases"
         target="blank"
         btn="soft-gray"
-        size="xs"
-        rounded-lg
+        size="10px sm:xs"
+        rounded="lg"
       >
         <NIcon name="i-heroicons-sparkles-solid" mr-1 text-sm text-primary />
         v{{ version }}
@@ -28,15 +28,15 @@ const { copy, copied } = useClipboard({ source })
             Una UI
           </span>
         </h1>
-        <h2 class="text-4xl text-muted font-bold lg:text-6xl">
-          The Atomic CSS Framework
+        <h2 class="text-4xl text-accent font-bold lg:text-6xl">
+          The Atomic UI Framework
         </h2>
-        <p class="pt-5 text-xl text-muted md:text-2xl">
-          Unlock fast, modern, and lightweight web applications with Una UI, powered by Unocss Engine, optimized for Vue and Nuxt.
+        <p class="text-xl text-muted md:text-2xl">
+          Unlock fast, modern, and lightweight web applications with Una UI, powered by Unocss Engine.
         </p>
       </div>
 
-      <div flex="~ wrap" class="justify-center gap-4 lg:justify-start">
+      <div flex="~ wrap" class="mt-4 justify-center gap-4 lg:justify-start">
         <NButton
           to="/getting-started"
           btn="solid"
@@ -64,10 +64,19 @@ const { copy, copied } = useClipboard({ source })
     </div>
 
     <!-- right section -->
-    <div col="span-1 lg:span-2" order="first lg:last" class="image-container relative grid mb-10 w-full place-items-center text-center lg:mb-0 lg:text-right">
-      <div class="image-bg absolute z-5 h-50 w-50 rotate-0 lg:h-85 lg:w-85" />
-      <div class="image-bg absolute z-5 h-50 w-50 rotate-45 lg:h-85 lg:w-85" />
-      <div class="h-50 w-50 -mt-10% lg:h-85 lg:w-85">
+    <div col="span-1 lg:span-2" order="first lg:last" class="image-container relative grid w-full place-items-center text-center lg:text-right">
+      <div
+        square="50 lg:85"
+        class="image-bg absolute z-5 rotate-0"
+      />
+      <div
+        square="50 lg:85"
+        class="image-bg absolute z-5 rotate-45"
+      />
+      <div
+        square="50 lg:85"
+        class="-mt-10%"
+      >
         <CommonLogo />
       </div>
     </div>
