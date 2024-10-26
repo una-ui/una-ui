1acb85da2b1e1a727aa25881e362cad9c7924b5f
Phojie Rengel <phojrengel@gmail.com>
2024-10-27 03:19:32 +0800

    docs: override default prose-components

---
+++ docs/components/content/ProseA.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  target: {
+    type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined>,
+    default: undefined,
+    required: false,
+  },
+  external: {
+    type: Boolean,
+    default: undefined,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <NuxtLink
+    :href="href"
+    :target="target"
+    :external="external"
+    class="font-semibold underline underline-offset-4 decoration-dotted hover:text-primary-active"
+  >
+    <slot />
+  </NuxtLink>
+</template>
