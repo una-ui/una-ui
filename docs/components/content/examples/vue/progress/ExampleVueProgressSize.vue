cfa64f8e2554c07d6e267b099f1c4ad2b72602d0
Phojie Rengel <phojrengel@gmail.com>
2024-11-10 02:01:09 +0800

    docs(progress): update examples and improve descriptions

--- docs/components/content/examples/vue/progress/ExampleVueProgressSize.vue
+++ docs/components/content/examples/vue/progress/ExampleVueProgressSize.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="flex flex-col space-y-4">
     <NProgress
-      :model-value="23"
-      size="xs"
-    />
-
-    <NProgress
       :model-value="54"
       progress="error"
       size="25px lg:3px"
     />
 
     <NProgress
+      :model-value="23"
+      progress="pink"
+      size="xs"
+    />
+
+    <NProgress
       :model-value="10"
       progress="lime"
-      size="2cm"
+      size="0.5cm"
     />
   </div>
 </template>
