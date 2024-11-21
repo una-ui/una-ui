09e08ec115b344dad8b7c555946f8d5827496e54
Phojie Rengel <phojrengel@gmail.com>
2024-11-21 21:27:26 +0800

    refactor(DropdownMenu): enhance components and styles

--- packages/preset/src/_shortcuts/dropdown-menu.ts
+++ packages/preset/src/_shortcuts/dropdown-menu.ts
@@ -11,12 +11,12 @@ export const staticDropdownMenu: Record<`${DropdownMenuPrefix}-${string}` | Drop
   'dropdown-menu-trigger-trailing': 'ml-auto',
 
   // dropdown-menu-content
-  'dropdown-menu-content': 'z-50 min-w-32 overflow-hidden rounded-md border border-base bg-popover p-1 text-popover shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+  'dropdown-menu-content': 'z-50 min-w-32 overflow-hidden rounded-md border border-base bg-popover p-1 text-popover shadow-md',
 
   // dropdown-menu-item
   'dropdown-menu-item-base': 'text-left transition-color focus-visible:outline-0',
-  'dropdown-menu-item-leading': 'opacity-75 text-1.1em',
-  'dropdown-menu-item-trailing': 'ml-auto opacity-75 text-1.1em',
+  'dropdown-menu-item-leading': 'text-1em',
+  'dropdown-menu-item-trailing': 'ml-auto opacity-75 text-1em',
 
   // dropdown-menu-label
   'dropdown-menu-label': 'px-2 py-1.5 text-1em font-semibold',
@@ -33,9 +33,10 @@ export const staticDropdownMenu: Record<`${DropdownMenuPrefix}-${string}` | Drop
 
   // dropdown-menu-sub
   'dropdown-menu-sub-trigger': 'transition-color focus-visible:outline-0',
-  'dropdown-menu-sub-trigger-leading': 'opacity-75 text-1.1em',
-  'dropdown-menu-sub-trigger-trailing': 'ml-auto opacity-75 text-1.1em',
-  'dropdown-menu-sub-content': 'z-50 min-w-32 overflow-hidden rounded-md border border-base bg-popover p-1 text-popover shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+  'dropdown-menu-sub-trigger-leading': 'text-1em',
+  'dropdown-menu-sub-trigger-trailing': 'ml-auto opacity-75 text-1em',
+  'dropdown-menu-sub-trigger-trailing-icon': 'i-lucide-chevron-right',
+  'dropdown-menu-sub-content': 'z-50 min-w-32 overflow-hidden rounded-md border border-base bg-popover p-1 text-popover shadow-lg',
 
 }
 
