81e54af6e2b5fb42cd1dabafd3d94fc3e3412f27
Phojie Rengel <phojrengel@gmail.com>
2024-11-21 23:33:43 +0800

    fix(Dialog): update animation configuration

--- packages/nuxt/src/runtime/components/elements/dialog/DialogDescription.vue
+++ packages/nuxt/src/runtime/components/elements/dialog/DialogDescription.vue
@@ -15,7 +15,7 @@ const forwardedProps = useForwardProps(delegatedProps)
   <DialogDescription
     v-bind="forwardedProps"
     :class="cn(
-      'dialog-description',
+      'dialog-description data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-48% data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-48%',
       props.una?.dialogDescription,
       props.class,
     )"
