53c6ee63f1fec0d5c3adfb07f01b88e5bbb75109
Phojie Rengel <phojrengel@gmail.com>
2025-01-09 13:54:31 +0800

    feat(forms): add new form component files and structure

---
+++ packages/nuxt/src/runtime/components/forms/form/FormLabel.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { NFormLabelProps } from '../../../types'
+import { useFormField } from '../../../composables/useFormField'
+import { cn } from '../../../utils'
+import Label from '../../elements/Label.vue'
+
+const props = defineProps<NFormLabelProps>()
+
+const { formItemId } = useFormField()
+</script>
+
+<template>
+  <Label
+    :class="cn(
+      'form-label',
+      props.una?.formLabel,
+      props.class,
+    )"
+    :for="formItemId"
+  >
+    <slot />
+  </Label>
+</template>
