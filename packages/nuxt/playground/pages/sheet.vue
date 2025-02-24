cb29f1daa05ae207a96dd9fb6344b7ac5196e911
Phojie Rengel <phojrengel@gmail.com>
2025-02-24 08:50:14 +0800

    feat(sheet): add dynamic sheet side selection

--- packages/nuxt/playground/pages/sheet.vue
+++ packages/nuxt/playground/pages/sheet.vue
@@ -1,42 +1,46 @@
 <script setup lang="ts">
-
+const SHEET_SIDES = ['top', 'right', 'bottom', 'left'] as const
+const username = ref('')
 </script>
 
 <template>
-  <NSheet>
-    <NSheetTrigger as-child>
-      <NButton variant="outline">
-        Open
-      </NButton>
-    </NSheetTrigger>
-    <NSheetContent>
-      <NSheetHeader>
-        <NSheetTitle>Edit profile</NSheetTitle>
-        <NSheetDescription>
-          Make changes to your profile here. Click save when you're done.
-        </NSheetDescription>
-      </NSheetHeader>
-      <div class="grid gap-4 py-4">
-        <div class="grid grid-cols-4 items-center gap-4">
-          <NLabel for="name" class="text-right">
-            Name
-          </NLabel>
-          <NInput id="name" value="Pedro Duarte" class="col-span-3" />
+  <div class="grid grid-cols-2 gap-2">
+    <NSheet v-for="side in SHEET_SIDES" :key="side">
+      <NSheetTrigger as-child>
+        <NButton variant="outline">
+          Open {{ side }}
+        </NButton>
+      </NSheetTrigger>
+
+      <NSheetContent :side="side">
+        <NSheetHeader>
+          <NSheetTitle>Edit profile</NSheetTitle>
+          <NSheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </NSheetDescription>
+        </NSheetHeader>
+        <div class="grid gap-4 py-4">
+          <div class="grid grid-cols-4 items-center gap-4">
+            <NLabel for="name" class="text-right">
+              Name
+            </NLabel>
+            <NInput id="name" v-model="username" :una="{ inputWrapper: 'col-span-3' }" />
+          </div>
+          <div class="grid grid-cols-4 items-center gap-4">
+            <NLabel for="username" class="text-right">
+              Username
+            </NLabel>
+            <NInput id="username" v-model="username" :una="{ inputWrapper: 'col-span-3' }" />
+          </div>
         </div>
-        <div class="grid grid-cols-4 items-center gap-4">
-          <NLabel for="username" class="text-right">
-            Username
-          </NLabel>
-          <NInput id="username" value="@peduarte" class="col-span-3" />
-        </div>
-      </div>
-      <NSheetFooter>
-        <NSheetClose as-child>
-          <NButton type="submit">
-            Save changes
-          </NButton>
-        </NSheetClose>
-      </NSheetFooter>
-    </NSheetContent>
-  </NSheet>
+        <NSheetFooter>
+          <NSheetClose as-child>
+            <NButton type="submit">
+              Save changes
+            </NButton>
+          </NSheetClose>
+        </NSheetFooter>
+      </NSheetContent>
+    </NSheet>
+  </div>
 </template>
