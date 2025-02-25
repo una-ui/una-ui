fba9e79365e67d0058576aa90588aa38c8f4b080
mnenie <1alexpeshkov@gmail.com>
2025-02-25 22:30:50 +0300

    refactor: shortcuts

--- packages/preset/src/_shortcuts/scroll-area.ts
+++ packages/preset/src/_shortcuts/scroll-area.ts
@@ -5,12 +5,12 @@ export const staticScrollArea: Record<`${ScrollAreaPrefix}-${string}` | ScrollAr
   'scroll-area': '',
   'scroll-area-scrollbar-vertical': 'h-full w-2.5 border-l border-l-transparent p-px',
   'scroll-area-scrollbar-horizontal': 'h-2.5 flex-col border-t border-t-transparent p-px',
-  'scroll-area-scrollbar-thumb': 'relative flex-1 rounded-full bg-border',
 
   // components
   'scroll-area-root': 'relative overflow-hidden',
   'scroll-area-scrollbar': 'flex touch-none select-none transition-colors',
   'scroll-area-viewport': 'h-full w-full rounded-inherit',
+  'scroll-area-scrollbar-thumb': 'relative flex-1 rounded-full bg-border',
 }
 
 export const dynamicScrollArea: [RegExp, (params: RegExpExecArray) => string][] = [
