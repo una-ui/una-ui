cd2627d813726f59b654ffab19a25e83356097cd
Phojie Rengel <phojrengel@gmail.com>
2025-03-03 14:28:11 +0800

    refactor(Avatar): update default presets and component configuration

--- packages/nuxt/src/runtime/components/elements/avatar/AvatarFallback.vue
+++ packages/nuxt/src/runtime/components/elements/avatar/AvatarFallback.vue
@@ -4,43 +4,36 @@ import { AvatarFallback } from 'reka-ui'
 import { cn, omitProps } from '../../../utils'
 import Icon from '../Icon.vue'
 
-const props = withDefaults(defineProps<NAvatarAvatarFallbackProps>(), {
-  as: 'span',
-})
+const props = defineProps<NAvatarAvatarFallbackProps>()
 </script>
 
 <template>
   <AvatarFallback
-    as-child
+    v-bind="omitProps(props, ['label', 'una', 'icon'])"
+    :class="cn(
+      'avatar-fallback',
+      una?.avatarFallback,
+      props.class,
+    )"
   >
-    <component
-      :is="as"
-      v-bind="omitProps(props, ['label', 'una', 'icon'])"
-      :class="cn(
-        'avatar-fallback',
-        una?.avatarFallback,
-        props.class,
-      )"
-    >
-      <slot>
-        <span
-          v-if="label && !icon"
-          :class="cn(
-            'avatar-label',
-            una?.avatarLabel,
-          )"
-        >
-          {{ label }}
-        </span>
-        <Icon
-          v-else-if="icon"
-          :class="cn(
-            'avatar-icon',
-            una?.avatarIcon,
-          )"
-          :name="label"
-        />
-      </slot>
-    </component>
+    <slot>
+      <span
+        v-if="label && !icon"
+        :class="cn(
+          'avatar-label',
+          una?.avatarLabel,
+        )"
+      >
+        {{ label }}
+      </span>
+      <Icon
+        v-else-if="icon"
+        :class="cn(
+          'avatar-icon',
+          una?.avatarIcon,
+        )"
+        :name="label"
+      />
+    </slot>
   </AvatarFallback>
 </template>
