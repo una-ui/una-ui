fb9215cd8be4afb6773b95ecf7ed3cd47d3dce66
Phojie Rengel <phojrengel@gmail.com>
2025-03-09 07:04:31 +0800

    feat(Nuxt): automatically install default collection icons

--- packages/nuxt/src/una.config.ts
+++ packages/nuxt/src/una.config.ts
@@ -1,4 +1,6 @@
 import type { UnocssNuxtOptions } from '@unocss/nuxt'
+import type { UserConfig } from 'unocss'
+import { createExternalPackageIconLoader } from '@iconify/utils/lib/loader/external-pkg'
 import extratorUna from '@una-ui/extractor-vue-script'
 import presetUna from '@una-ui/preset'
 import prefixes from '@una-ui/preset/prefixes'
@@ -8,30 +10,40 @@ import {
   presetWind3,
   transformerDirectives,
   transformerVariantGroup,
+
 } from 'unocss'
 import { presetAnimations } from 'unocss-preset-animations'
 
-export const defaultConfig = { preflight: true, presets: [
-  presetWind3(),
-  presetAttributify(),
-  presetIcons({
-    extraProperties: {
-      'display': 'inline-block',
-      'height': '1em',
-      'width': '1em',
-      'vertical-align': 'middle',
-    },
-  }),
-  presetUna(),
-  presetAnimations(),
-], extractors: [
-  extratorUna({
-    prefixes,
-  }),
-], transformers: [
-  transformerDirectives(),
-  transformerVariantGroup(),
-] }
+export const defaultConfig: UserConfig<object> = {
+  presets: [
+    presetWind3(),
+    presetAttributify(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'height': '1em',
+        'width': '1em',
+        'vertical-align': 'middle',
+      },
+      collections: {
+        ...createExternalPackageIconLoader('@iconify-json/lucide', true),
+        ...createExternalPackageIconLoader('@iconify-json/radix-icons', true),
+        ...createExternalPackageIconLoader('@iconify-json/tabler', true),
+      },
+    }),
+    presetUna(),
+    presetAnimations(),
+  ],
+  extractors: [
+    extratorUna({
+      prefixes,
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
+}
 
 function extendUnocssOptions(options: UnocssNuxtOptions = {}): UnocssNuxtOptions {
   return {
