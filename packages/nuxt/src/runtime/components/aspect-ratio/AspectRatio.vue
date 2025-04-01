5f0b79f665313fd0bcb09391ab0eee86f308173b
mnenie <1alexpeshkov@gmail.com>
2025-04-01 18:48:56 +0300

    feat: add aspect ratio component

---
+++ packages/nuxt/src/runtime/components/aspect-ratio/AspectRatio.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { NAspectRatioProps } from '../../types/aspect-ratio'
+import { reactiveOmit } from '@vueuse/core'
+import { AspectRatio } from 'reka-ui'
+import { cn } from '../../utils'
+
+const props = withDefaults(defineProps<NAspectRatioProps>(), {
+  aspectRatio: 'soft',
+})
+
+const rootProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <AspectRatio
+    v-slot="{ aspect }"
+    v-bind="rootProps"
+    :aspect-ratio
+    :class="cn(
+      'aspect-ratio',
+      props.una?.aspectRatio,
+      props.class,
+    )"
+  >
+    <slot :aspect />
+  </AspectRatio>
+</template>
