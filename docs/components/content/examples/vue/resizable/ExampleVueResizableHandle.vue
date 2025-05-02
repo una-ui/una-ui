36ce060032033a11b01c85921781953e196565ea
mnenie <1alexpeshkov@gmail.com>
2025-05-02 18:58:05 +0300

    docs: add handle and panel examples

---
+++ docs/components/content/examples/vue/resizable/ExampleVueResizableHandle.vue
@@ -0,0 +1,39 @@
+<template>
+  <NResizable
+    id="group-1"
+    direction="horizontal"
+    class="mx-auto max-w-lg min-h-260px border rounded-lg"
+  >
+    <NResizablePanel id="group-1-panel-1" :default-size="30">
+      <div class="h-full flex items-center justify-center p-6">
+        <span class="font-semibold">1</span>
+      </div>
+    </NResizablePanel>
+    <NResizableHandle id="group-1-handle" with-handle />
+    <NResizablePanel id="group-1-panel-2" :default-size="70">
+      <NResizable id="group-2" direction="vertical">
+        <NResizablePanel id="group-2-panel-1" :default-size="20">
+          <div class="h-full flex items-center justify-center p-4">
+            <span class="font-semibold">2</span>
+          </div>
+        </NResizablePanel>
+        <NResizableHandle id="group-2-handle" with-handle resizable-handle="solid-red" />
+        <NResizablePanel id="group-2-panel-2" :default-size="80">
+          <NResizable id="group-3" direction="horizontal">
+            <NResizablePanel id="group-3-panel-1" :default-size="50">
+              <div class="h-full flex items-center justify-center p-4">
+                <span class="font-semibold">3</span>
+              </div>
+            </NResizablePanel>
+            <NResizableHandle id="group-3-handle" with-handle icon="i-lucide-arrow-left-right" resizable-handle="solid-primary" />
+            <NResizablePanel id="group-3-panel-2" :default-size="50">
+              <div class="h-full flex items-center justify-center p-6">
+                <span class="font-semibold">4</span>
+              </div>
+            </NResizablePanel>
+          </NResizable>
+        </NResizablePanel>
+      </NResizable>
+    </NResizablePanel>
+  </NResizable>
+</template>
---
+++ docs/components/content/examples/vue/resizable/ExampleVueResizablePanel.vue
@@ -0,0 +1,29 @@
+<template>
+  <NResizable
+    id="group"
+    direction="horizontal"
+    class="mx-auto max-w-lg min-h-260px border rounded-lg"
+  >
+    <NResizablePanel id="group-panel-1" v-slot="{ isCollapsed }" :collapsed-size="6" :min-size="20" :collapsible="true" :default-size="30">
+      <div class="h-full flex flex-col gap-4" :class="[isCollapsed ? 'px-1 py-1.5' : 'p-1.5']">
+        <div class="w-full flex items-center gap-2">
+          <NSkeleton class="h-5 min-w-5.5 w-6" rounded="sm" />
+          <div v-show="!isCollapsed" class="w-full flex flex-col gap-1">
+            <NSkeleton class="h-2 w-full" rounded="sm" />
+            <NSkeleton class="h-1.5 w-60%" rounded="sm" />
+          </div>
+        </div>
+        <div class="flex flex-col gap-2">
+          <NSkeleton v-for="i in 3" :key="i" rounded="sm" class="h-5 w-full" />
+        </div>
+        <NSkeleton class="mt-auto h-5 w-full" rounded="sm" />
+      </div>
+    </NResizablePanel>
+    <NResizableHandle id="group-handle" with-handle />
+    <NResizablePanel id="group-panel-2" :default-size="70" :min-size="60">
+      <div class="h-full flex items-center justify-center p-6">
+        <span class="font-semibold">Main</span>
+      </div>
+    </NResizablePanel>
+  </NResizable>
+</template>
