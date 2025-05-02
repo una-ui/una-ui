e8763d9435a7623ac8b5fa593557702a9c28eb5a
Phojie Rengel <phojrengel@gmail.com>
2025-05-02 12:10:00 +0800

    fix: types

--- packages/nuxt/src/runtime/components/drawer/Drawer.vue
+++ packages/nuxt/src/runtime/components/drawer/Drawer.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import type { DrawerRootEmits, DrawerRootProps } from 'vaul-vue'
+import { reactivePick } from '@vueuse/core'
 import { useForwardPropsEmits } from 'reka-ui'
 import { DrawerRoot } from 'vaul-vue'
 
@@ -8,14 +9,13 @@ const props = withDefaults(defineProps<DrawerRootProps>(), {
 })
 
 const emits = defineEmits<DrawerRootEmits>()
-
-const forwarded = useForwardPropsEmits(props, emits)
+const rootProps = useForwardPropsEmits(reactivePick(props, 'activeSnapPoint', 'closeThreshold', 'shouldScaleBackground', 'setBackgroundColorOnScale', 'scrollLockTimeout', 'fixed', 'dismissible', 'modal', 'open', 'defaultOpen', 'nested', 'direction', 'noBodyStyles', 'handleOnly', 'preventScrollRestoration', 'snapPoints'), emits)
 </script>
 
 <template>
   <DrawerRoot
     data-slot="drawer"
-    v-bind="forwarded"
+    v-bind="rootProps"
   >
     <slot />
   </DrawerRoot>
