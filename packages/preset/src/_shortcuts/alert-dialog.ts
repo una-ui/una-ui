3151a322ad117a22961f65aefcdb83c1bd7f9a4b
Phojie Rengel <phojrengel@gmail.com>
2025-05-04 09:25:47 +0800

    feat(AlertDialog): improve dialog content & footer style presets

--- packages/preset/src/_shortcuts/alert-dialog.ts
+++ packages/preset/src/_shortcuts/alert-dialog.ts
@@ -7,12 +7,12 @@ export const staticAlertDialog: Record<`${AlertDialogPrefix}-${string}` | AlertD
   // sub-components
   'alert-dialog-cancel': 'mt-2 sm:mt-0',
   'alert-dialog-overlay': 'fixed inset-0 z-50 bg-black/80',
-  'alert-dialog-content': 'fixed left-1/2 top-1/2 z-50 grid w-full max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 border bg-background p-6 shadow-lg duration-200 sm:rounded-lg',
+  'alert-dialog-content': 'bg-background fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg',
 
   'alert-dialog-title': 'text-lg font-semibold',
   'alert-dialog-description': 'text-muted text-sm',
   'alert-dialog-header': 'flex flex-col gap-2 text-center sm:text-left',
-  'alert-dialog-footer': 'flex flex-col gap-2 sm:flex-row sm:justify-end',
+  'alert-dialog-footer': 'flex flex-col-reverse gap-2 sm:flex-row sm:justify-end',
 }
 
 export const dynamicAlertDialog: [RegExp, (params: RegExpExecArray) => string][] = [
