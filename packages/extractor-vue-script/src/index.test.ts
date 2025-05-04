26c28dccaec8638572cc8b10c79a70127dcb469e
Matthew Messinger <mattmess1221@gmail.com>
2025-05-04 19:27:25 -0400

    tests(extractor): add tests for extractor regexp

---
+++ packages/extractor-vue-script/src/index.test.ts
@@ -0,0 +1,37 @@
+import { expect, it } from 'vitest'
+import extractorVueScript from './index'
+
+const prefixes = [
+  'size',
+]
+
+const extractor = extractorVueScript({ prefixes })
+
+async function extract(code: string): Promise<string[]> {
+  /* @ts-expect-error extract accepts {code} and returns string[] */
+  return await extractor.extract!({ code })
+}
+
+it('test extractor typescript with defaults', async () => {
+  const code = `
+const props = withDefaults(defineProps<{size?: string}>(), {
+    size: 'md'
+})
+`
+  const result = await extract(code)
+  expect(result).toStrictEqual(['[size~=\"md\"]'])
+})
+
+it('test extractor vue object prop default', async () => {
+  const code = `
+const props = defineProps({
+    size: { type: String, default: 'md' }
+})
+`
+  const result = await extract(code)
+  expect(result).toStrictEqual(['[size~=\"md\"]'])
+})
+
+// TODO: Support double quotes
+
+// TODO: Support prop destructure
