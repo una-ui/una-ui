db483372f5ef671ebeca056f806c5dbc0fc94f24
dimunyo <gitmekan13@gmail.com>
2025-05-16 10:50:08 +0800

    refactor: remove unused code

--- packages/nuxt/src/runtime/components/drawer/DrawerContent.vue
+++ packages/nuxt/src/runtime/components/drawer/DrawerContent.vue
@@ -58,7 +58,6 @@ const contentEvents = computed(() => {
       v-on="contentEvents"
     >
       <div :class="cn('drawer-content-handle')" />
-      <!-- <div class="group-data-[vaul-drawer-direction=bottom]/drawer-content:block mx-auto mt-4 hidden h-2 w-[100px] shrink-0 rounded-full bg-muted" /> -->
       <slot />
     </DrawerContent>
   </DrawerPortal>
