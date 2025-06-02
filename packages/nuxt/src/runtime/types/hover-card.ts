f50067c10ad2e93bde927a82a0b21b2e58e814d7
mnenie <1alexpeshkov@gmail.com>
2025-06-03 00:37:57 +0300

    feat: add types

---
+++ packages/nuxt/src/runtime/types/hover-card.ts
@@ -0,0 +1,64 @@
+import type { HoverCardArrowProps, HoverCardContentProps, HoverCardRootProps, HoverCardTriggerProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import type { NButtonProps } from './button'
+
+interface BaseExtensions {
+  /** CSS class for the component */
+  class?: HTMLAttributes['class']
+  /** Size of the component */
+  size?: HTMLAttributes['class']
+}
+
+export interface NHoverCardProps extends HoverCardRootProps, BaseExtensions {
+  /**
+   * Allows you to add `UnaUI` hover-card preset properties,
+   * Think of it as a shortcut for adding options or variants to the preset if available.
+   *
+   * @see https://github.com/una-ui/una-ui/blob/main/packages/preset/src/_shortcuts/hover-card.ts
+   * @example
+   * hover-card="gray"
+   */
+  hoverCard?: HTMLAttributes['class']
+  /** Whether to show the arrow or not */
+  indicator?: boolean
+
+  /** Props for the hover card content */
+  _hoverCardContent?: Partial<NHoverCardContentProps>
+  /** Props for the hover card trigger */
+  _hoverCardTrigger?: Partial<NHoverCardTriggerProps>
+  /** Props for the hover card arrow */
+  _hoverCardArrow?: Partial<NHoverCardArrowProps>
+
+  /**
+   * `UnaUI` preset configuration
+   *
+   * @see https://github.com/una-ui/una-ui/blob/main/packages/preset/src/_shortcuts/hover-card.ts
+   */
+  una?: NHoverCardUnaProps
+}
+
+export interface NHoverCardContentProps extends HoverCardContentProps, BaseExtensions {
+  /** Additional properties for the una component */
+  una?: Pick<NHoverCardUnaProps, 'hoverCardContent'>
+}
+
+export interface NHoverCardTriggerProps extends HoverCardTriggerProps, Omit<NButtonProps, 'una'> {
+  /** Additional properties for the una component */
+  una?: Pick<NHoverCardUnaProps, 'hoverCardTrigger'> & NButtonProps['una']
+}
+
+export interface NHoverCardArrowProps extends HoverCardArrowProps, BaseExtensions {
+  /** Additional properties for the una component */
+  una?: Pick<NHoverCardUnaProps, 'hoverCardArrow'>
+}
+
+interface NHoverCardUnaProps {
+  /** CSS class for the hover card */
+  hoverCard?: HTMLAttributes['class']
+  /** CSS class for the hover card content */
+  hoverCardContent?: HTMLAttributes['class']
+  /** CSS class for the hover card trigger */
+  hoverCardTrigger?: HTMLAttributes['class']
+  /** CSS class for the hover card arrow */
+  hoverCardArrow?: HTMLAttributes['class']
+}
