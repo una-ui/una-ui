7b23ff2cf8e374fbd7f8fd31b5aef14c872c0980
Phojie Rengel <phojrengel@gmail.com>
2025-06-09 13:38:46 +0800

    feat: add support for model modifiers (#423)

--- packages/nuxt/src/runtime/components/forms/Input.vue
+++ packages/nuxt/src/runtime/components/forms/Input.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { NInputProps } from '../../types'
 import { computed, onMounted, ref } from 'vue'
-import { cn, randomId } from '../../utils'
+import { cn, looseToNumber, randomId } from '../../utils'
 import NIcon from '../elements/Icon.vue'
 
 defineOptions({
@@ -13,14 +13,23 @@ const props = withDefaults(defineProps<NInputProps>(), {
   type: 'text',
   resize: 'none',
   rows: 0,
+  autofocusDelay: 0,
 })
 
-const emit = defineEmits(['leading', 'trailing', 'update:modelValue'])
+const emit = defineEmits([
+  'leading',
+  'trailing',
+  'update:modelValue',
+  'change',
+  'blur',
+])
 
 const slots = defineSlots<{
   leading?: any
   trailing?: any
 }>()
+
+const [modelValue, modelModifiers] = defineModel<string | number | null | undefined>()
 
 const id = props.id ?? randomId('input')
 
@@ -101,15 +110,63 @@ function resizeTextarea(): void {
     inputRef.value.rows = maxAutoresizeRows
 }
 
+function updateInput(value: string | null) {
+  if (modelModifiers.trim) {
+    value = value?.trim() ?? null
+  }
+
+  if (modelModifiers.number || props.type === 'number') {
+    value = looseToNumber(value)
+  }
+
+  if (modelModifiers.nullify) {
+    value ||= null
+  }
+
+  modelValue.value = value
+  emit('update:modelValue', value)
+}
+
 function onInput(event: Event): void {
-  emit('update:modelValue', (event.target as HTMLInputElement).value)
+  if (!modelModifiers.lazy) {
+    updateInput((event.target as HTMLInputElement).value)
+  }
 
   resizeTextarea()
 }
 
+function onChange(event: Event) {
+  const value = (event.target as HTMLInputElement).value
+
+  if (modelModifiers.lazy) {
+    updateInput(value)
+  }
+
+  // Update trimmed input so that it has same behavior as native input https://github.com/vuejs/core/blob/5ea8a8a4fab4e19a71e123e4d27d051f5e927172/packages/runtime-dom/src/directives/vModel.ts#L63
+  if (modelModifiers.trim) {
+    (event.target as HTMLInputElement).value = value.trim()
+  }
+
+  emit('change', event)
+}
+
+function onBlur(event: FocusEvent) {
+  emit('blur', event)
+}
+
 onMounted(() => {
+  setTimeout(() => {
+    autoFocus()
+  }, props.autofocusDelay)
+
   resizeTextarea()
 })
+
+function autoFocus() {
+  if (props.autofocus) {
+    inputRef.value?.focus()
+  }
+}
 
 defineExpose({
   focus: () => inputRef.value?.focus(),
@@ -168,6 +225,8 @@ defineExpose({
       :cols="type === 'textarea' ? cols : undefined"
       v-bind="$attrs"
       @input="onInput"
+      @blur="onBlur"
+      @change="onChange"
     />
 
     <div
