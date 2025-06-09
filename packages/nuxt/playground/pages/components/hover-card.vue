d873278f43e3a68ef2bb49f59f8c3d33872af698
mnenie <1alexpeshkov@gmail.com>
2025-06-10 00:03:10 +0300

    playground: add examples and sidebar path

---
+++ packages/nuxt/playground/pages/components/hover-card.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const [DefineContent, ReuseContent] = createReusableTemplate()
+</script>
+
+<template>
+  <div class="flex items-center space-x-8">
+    <NHoverCard indicator>
+      <template #trigger>
+        <NAvatar src="https://github.com/vuejs.png" class="cursor-pointer" />
+      </template>
+      <ReuseContent />
+    </NHoverCard>
+    <NHoverCard hovercard="soft-primary" indicator>
+      <template #trigger>
+        <NButton btn="soft-primary" class="cursor-pointer">
+          With color
+        </NButton>
+      </template>
+      <ReuseContent />
+    </NHoverCard>
+  </div>
+
+  <DefineContent>
+    <div class="flex justify-between space-x-4">
+      <NAvatar src="https://github.com/vuejs.png" />
+      <div class="space-y-1">
+        <h4 class="text-sm font-semibold">
+          @vuejs
+        </h4>
+        <p class="text-sm">
+          Progressive JavaScript framework for building modern web interfaces.
+        </p>
+        <div class="flex items-center pt-2">
+          <span class="text-sm text-muted">
+            Joined January 2014
+          </span>
+        </div>
+      </div>
+    </div>
+  </DefineContent>
+</template>
