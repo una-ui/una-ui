be17640eca61f6ff64c8498de2d9791a2f867035
Alex Peshkov <1alexpeshkov@gmail.com>
2025-06-12 20:41:44 +0300

    feat(HoverCard): introduces new components and presets  (#424)
    * feat: add types
    
    * chore: update types
    
    * feat: add shortcuts and prefixes
    
    * chore: update general variants
    
    * feat: add components
    
    * playground: add examples and sidebar path
    
    * chore: update indicator prop name to arrow
    
    * chore: update slots
    
    * docs: add examples and page
    
    * [autofix.ci] apply automated fixes
    
    * chore: update components with hover-card prop
    
    * chore: update shortcuts and types
    
    * chore: update examples
    
    * style(hover-card): reorder class props for consistency
    
    * style(hover-card): update class syntax for consistency
    
    * docs: add missing sections
    
    * docs: update custom rendering example
    
    * docs: update hover-card-arrow example
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Phojie Rengel <phojrengel@gmail.com>

---
+++ packages/nuxt/playground/pages/components/hover-card.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const [DefineContent, ReuseContent] = createReusableTemplate()
+</script>
+
+<template>
+  <div class="flex items-center space-x-8">
+    <NHoverCard arrow>
+      <template #trigger>
+        <NAvatar src="https://github.com/vuejs.png" class="cursor-pointer" />
+      </template>
+      <template #content>
+        <ReuseContent />
+      </template>
+    </NHoverCard>
+    <NHoverCard hover-card="soft-primary" arrow>
+      <template #trigger>
+        <NButton btn="soft-primary" class="cursor-pointer">
+          With color
+        </NButton>
+      </template>
+      <template #content>
+        <ReuseContent />
+      </template>
+    </NHoverCard>
+  </div>
+
+  <DefineContent>
+    <div class="flex justify-between space-x-4">
+      <NAvatar src="https://github.com/vuejs.png" />
+      <div class="space-y-1">
+        <h4 class="text-sm font-semibold">
+          @vuejs
+        </h4>
+        <p class="text-sm">
+          Progressive JavaScript framework for building modern web interfaces.
+        </p>
+        <div class="flex items-center pt-2">
+          <span class="text-sm text-muted">
+            Joined January 2014
+          </span>
+        </div>
+      </div>
+    </div>
+  </DefineContent>
+</template>
