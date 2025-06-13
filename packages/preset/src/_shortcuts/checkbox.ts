d92b23e08df042d97353856356942e14aa9be9e7
Phojie Rengel <phojrengel@gmail.com>
2025-06-14 02:00:56 +0800

    refactor: update checkbox focus styling

--- packages/preset/src/_shortcuts/checkbox.ts
+++ packages/preset/src/_shortcuts/checkbox.ts
@@ -1,12 +1,8 @@
-import type { RuleContext } from '@unocss/core'
-import type { Theme } from '@unocss/preset-uno'
-import { parseColor } from '@unocss/preset-mini/utils'
-
 type CheckboxPrefix = 'checkbox'
 
 export const staticCheckbox: Record<`${CheckboxPrefix}-${string}` | CheckboxPrefix, string> = {
   // base
-  'checkbox': 'checkbox-primary text-md w-1em h-1em shrink-0 rounded-sm ring-offset-background focus-visible:outline-none disabled:n-disabled border border-brand bg-brand text-background focus-visible:(ring-2 ring-brand ring-offset-2) data-[state=unchecked]:(bg-background text-foreground)',
+  'checkbox': 'square-1em shrink-0 rounded-4px shadow-xs transition-shadow outline-none disabled:n-disabled border border-input dark:border-input/30',
   'checkbox-label': 'block',
   'checkbox-reverse': 'flex-row-reverse',
 
@@ -14,19 +10,19 @@ export const staticCheckbox: Record<`${CheckboxPrefix}-${string}` | CheckboxPref
   'checkbox-wrapper': 'gap-x-3 relative inline-flex items-center hover:cursor-pointer',
 
   // icon
-  'checkbox-indicator': 'flex items-center justify-center h-full w-full data-[state=unchecked]:opacity-0 transition-base opacity-100 text-background',
-  'checkbox-icon-base': 'w-1em h-1em',
+  'checkbox-indicator': 'flex items-center justify-center text-current transition-none',
+  'checkbox-icon-base': 'square-0.875rem',
   'checkbox-checked-icon': 'i-check',
   'checkbox-unchecked-icon': '',
   'checkbox-indeterminate-icon': 'i-lucide-minus',
 }
 
 export const dynamicCheckbox = [
-  [/^checkbox-(.*)$/, ([, body]: string[], { theme }: RuleContext<Theme>) => {
-    const color = parseColor(body, theme)
-    if ((color?.cssColor?.type === 'rgb' || color?.cssColor?.type === 'rgba') && color.cssColor.components)
-      return `n-${body}-600 dark:n-${body}-500`
-  }],
+  [/^checkbox(-(\S+))?$/, ([, , c = 'primary']) => `checkbox-focus-${c} checkbox-checked-${c} checkbox-indeterminate-${c} checkbox-unchecked-gray`],
+  [/^checkbox-focus(-(\S+))?$/, ([, , c = 'primary']) => `focus-visible:ring-3px focus-visible:border-${c}-200 dark:focus-visible:border-${c}-800 focus-visible:ring-${c}-200/50 dark:focus-visible:ring-${c}-800/50`],
+  [/^checkbox-indeterminate(-(\S+))?$/, ([, , c = 'gray']) => `data-[state=indeterminate]:(bg-${c}-600 text-${c}-50) dark:data-[state=indeterminate]:(bg-${c}-500 text-${c}-900)`],
+  [/^checkbox-checked(-(\S+))?$/, ([, , c = 'primary']) => `data-[state=checked]:(bg-${c}-600 text-${c}-50) dark:data-[state=checked]:(bg-${c}-500 text-${c}-900)`],
+  [/^checkbox-unchecked(-(\S+))?$/, ([, , c = 'gray']) => `data-[state=unchecked]:(bg-${c}-200 text-${c}-50) dark:data-[state=unchecked]:(bg-${c}-800/30 text-${c}-900)`],
 ]
 
 export const checkbox = [
