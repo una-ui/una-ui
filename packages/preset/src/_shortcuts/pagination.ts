a72c9dacc0f4d769f1c9a181135034b6ae170926
Phojie Rengel <phojrengel@gmail.com>
2025-06-21 01:49:48 +0800

    feat(Pagination): update and improve default pagination list variant

--- packages/preset/src/_shortcuts/pagination.ts
+++ packages/preset/src/_shortcuts/pagination.ts
@@ -40,7 +40,7 @@ export const dynamicPagination = [
   ],
   [
     /^pagination-unselected(?:-([^-]+))?(?:-([^-]+))?$/,
-    ([, variant = 'solid', color = 'gray']) =>
+    ([, variant = 'outline', color = 'gray']) =>
       `data-[selected=false]:btn-${variant}-${color}`,
   ],
 
