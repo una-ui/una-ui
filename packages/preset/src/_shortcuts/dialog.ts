6ffa7968d9638f5e3e6a2b97961b5d0cfb8b9b07
Phojie Rengel <phojrengel@gmail.com>
2025-06-22 04:57:31 +0800

    feat(Dialog): improve dialog-overlay style preset

--- packages/preset/src/_shortcuts/dialog.ts
+++ packages/preset/src/_shortcuts/dialog.ts
@@ -5,7 +5,7 @@ export const staticDialog: Record<`${KbdPrefix}-${string}` | KbdPrefix, string> 
   'dialog': '',
 
   // sub-components
-  'dialog-overlay': 'fixed inset-0 z-50 bg-black/80',
+  'dialog-overlay': 'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80',
   'dialog-content': 'bg-background fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg',
 
   'dialog-scroll-overlay': 'fixed inset-0 z-50 grid place-items-center overflow-y-auto bg-black/80',
