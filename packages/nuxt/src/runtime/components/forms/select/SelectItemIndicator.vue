4641f13ab8711538f882bbd456b73f816ed201a3
Matthew Messinger <mattmess1221@gmail.com>
2025-10-02 12:40:53 -0400

    fix(SelectItemIndicator): use forwardProps for improved prop handling

--- packages/nuxt/src/runtime/components/forms/select/SelectItemIndicator.vue
+++ packages/nuxt/src/runtime/components/forms/select/SelectItemIndicator.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import type { NSelectItemIndicatorProps } from '../../../types'
-import { SelectItemIndicator } from 'reka-ui'
+import { reactiveOmit } from '@vueuse/core'
+import { SelectItemIndicator, useForwardProps } from 'reka-ui'
 import { cn } from '../../../utils'
 import Icon from '../../elements/Icon.vue'
 
 const props = defineProps<NSelectItemIndicatorProps>()
+
+const forwardProps = useForwardProps(reactiveOmit(props, ['icon', 'una']))
 </script>
 
 <template>
   <SelectItemIndicator
-    v-bind="props"
+    v-bind="forwardProps"
     :class="cn(
       'select-item-indicator',
       props.una?.selectItemIndicator,
