a36f7d845ca55cbdbbda3b0aef67920fa56f8970
Phojie Rengel <phojrengel@gmail.com>
2025-11-12 19:21:32 +0800

    ci: resolve release configuration [skip]

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -13,7 +13,7 @@ if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
   echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
   echo "always-auth=true" >> ~/.npmrc
-  npm whoami
+  pnpm whoami
 fi
 
 # List package directories and store them in an array
@@ -29,6 +29,6 @@ for ((i=${#package_dirs[@]}-1; i>=0; i--)) ; do
   echo "Publishing $p"
   cp ../../LICENSE .
   cp ../../README.md .
-  pnpm publish --access public --no-git-checks
+  pnpm publish --access public --no-git-checks --tag edge
   popd
 done
