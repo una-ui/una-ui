7f0d3e09876bf4d2c9206e5909621ead98e58d21
Matthew Messinger <mattmess1221@gmail.com>
2026-01-19 12:15:37 -0500

    feat(popover): Add close, arrow, and anchor components
    PopoverTrigger and PopoverAnchor are now conditionally rendered depending on the presense of $slots.trigger and $slots.anchor

--- packages/nuxt/src/runtime/components/elements/popover/Popover.vue
+++ packages/nuxt/src/runtime/components/elements/popover/Popover.vue
@@ -2,24 +2,44 @@
 import type { PopoverRootEmits } from 'reka-ui'
 import type { NPopoverProps } from '../../../types'
 import { reactiveOmit } from '@vueuse/core'
-import { PopoverRoot, PopoverTrigger, useForwardPropsEmits } from 'reka-ui'
+import { PopoverAnchor, PopoverArrow, PopoverClose, PopoverRoot, PopoverTrigger, useForwardPropsEmits } from 'reka-ui'
+import { cn } from '../../../utils'
 import NPopoverContent from './PopoverContent.vue'
 
 const props = defineProps<NPopoverProps>()
 const emits = defineEmits<PopoverRootEmits>()
 
-const delegatedProps = reactiveOmit(props, ['_popoverContent'])
+const delegatedProps = reactiveOmit(props, ['una', '_popoverContent', '_popoverAnchor', '_popoverArrow', '_popoverClose'])
 
 const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
 
 <template>
-  <PopoverRoot v-slot="{ open }" v-bind="forwarded">
-    <PopoverTrigger as-child>
-      <slot name="trigger" :open />
+  <PopoverRoot v-slot="{ open, close }" v-bind="forwarded" :class="una?.popoverRoot">
+    <PopoverTrigger v-if="$slots.trigger" as-child>
+      <slot name="trigger" :open :close />
     </PopoverTrigger>
-    <NPopoverContent v-bind="_popoverContent">
-      <slot />
+    <PopoverAnchor
+      v-if="$slots.anchor" as-child
+      v-bind="_popoverAnchor"
+      :class="cn('popover-anchor', una?.popoverAnchor)"
+    >
+      <slot name="anchor" :open :close />
+    </PopoverAnchor>
+    <NPopoverContent v-bind="_popoverContent" :una>
+      <slot :close />
+      <PopoverClose
+        v-bind="_popoverClose"
+        :class="cn('popover-close', una?.popoverClose)"
+      >
+        <slot name="close" :close>
+          <NIcon :name="cn('popover-close-icon', una?.popoverCloseIcon)" />
+        </slot>
+      </PopoverClose>
+      <PopoverArrow
+        v-bind="_popoverArrow"
+        :class="cn('popover-arrow', una?.popoverArrow)"
+      />
     </NPopoverContent>
   </PopoverRoot>
 </template>
--- packages/nuxt/src/runtime/types/popover.ts
+++ packages/nuxt/src/runtime/types/popover.ts
@@ -1,4 +1,4 @@
-import type { PopoverContentProps, PopoverRootProps } from 'reka-ui'
+import type { PopoverAnchorProps, PopoverArrowProps, PopoverCloseProps, PopoverContentProps, PopoverRootProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 
 interface BaseExtensions { class?: HTMLAttributes['class'] }
@@ -6,7 +6,23 @@ interface BaseExtensions { class?: HTMLAttributes['class'] }
 export interface NPopoverProps extends PopoverRootProps {
   /** Props for the popover content */
   _popoverContent?: NPopoverContentProps
+  _popoverAnchor?: PopoverAnchorProps
+  _popoverArrow?: PopoverArrowProps
+  _popoverClose?: PopoverCloseProps
+
+  una?: NPopoverUnaProps
 }
 
 export interface NPopoverContentProps extends PopoverContentProps, BaseExtensions {
+  una?: Pick<NPopoverUnaProps, 'popoverContent'>
+}
+
+interface NPopoverUnaProps {
+  popoverRoot?: string
+  popoverTrigger?: string
+  popoverContent?: string
+  popoverClose?: string
+  popoverCloseIcon?: string
+  popoverArrow?: string
+  popoverAnchor?: string
 }
