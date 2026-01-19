61ad03f5b86c6d3b86dbc01e94c629f6aeec46b0
Phojie Rengel <phojrengel@gmail.com>
2026-01-19 08:40:58 +0800

    chore:init

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -10,8 +10,9 @@ pnpm jiti ./scripts/bump-edge
 
 # Update token
 if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
-  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" > .npmrc.auth
-  export NPM_CONFIG_USERCONFIG=.npmrc.auth
+  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
   npm whoami
 fi
 
